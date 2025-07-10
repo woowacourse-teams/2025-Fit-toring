68d82861a01f4c1a7e8d120df25dc152e320ec76
ppparkta <pppppark2@gmail.com>
2025-07-10 14:55:30 +0900

    build: 백엔드 프로젝트 초기 세팅 #1

---
+++ backend/fittoring/src/main/java/com/fittoring/fittoring/FittoringApplication.java
@@ -0,0 +1,13 @@
+package com.fittoring.fittoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittoringApplication.class, args);
+	}
+
+}
---
+++ backend/fittoring/src/test/java/com/fittoring/fittoring/FittoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fittoring.fittoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
