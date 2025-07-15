8447950b4861e490b11bfbdb24ef645d88190dcf
Lee Dong Yeop <s01054830455@gmail.com>
2025-07-15 17:54:40 +0900

    feat: 멘토링 엔티티 구현 #22
    Co-authored-by: ppparkta <pppppark2@gmail.com>
    Co-authored-by: jye326 <juyongeun@gmail.com>
    Co-authored-by: kimdevv <kim__dev@hufs.ac.kr>

---
+++ backend/fittoring/src/main/java/com/fittoring/fittoring/Mentoring.java
@@ -0,0 +1,36 @@
+package com.fittoring.fittoring;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "mentoring")
+@Entity
+public class Mentoring {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String mentorName;
+
+    @Column(nullable = false, unique = true)
+    private String mentorPhone;
+
+    @Column(nullable = false)
+    private int price;
+
+    private Integer career;
+
+    private String content;
+
+    @Column(nullable = false)
+    private String introduction;
+}
