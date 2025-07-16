e3013838b1381868b3d2bc88b30b86992018ee78
Lee Dong Yeop <s01054830455@gmail.com>
2025-07-16 11:06:29 +0900

    feat: 자격증 엔티티 구현 #22
    Co-authored-by: ppparkta <pppppark2@gmail.com>
    Co-authored-by: jye326 <juyongeun@gmail.com>
    Co-authored-by: kimdevv <kim__dev@hufs.ac.kr>

---
+++ backend/fittoring/src/main/java/com/fittoring/fittoring/Certificate.java
@@ -0,0 +1,23 @@
+package com.fittoring.fittoring;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "certificate")
+@Entity
+public class Certificate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+}
