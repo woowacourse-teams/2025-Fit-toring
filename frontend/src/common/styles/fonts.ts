d810cee9b63927022be476e53d1dbe70c04baf87
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-16 17:32:11 +0900

    build: Pretendard 폰트 추가 및 적용 #52
    Co-authored-by: Beomtae <Beomtae@users.noreply.github.com>
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

---
+++ frontend/src/common/styles/fonts.ts
@@ -0,0 +1,22 @@
+import { css } from '@emotion/react';
+
+import PretendardBold from '../assets/fonts/Pretendard-Bold.woff2';
+import PretendardRegular from '../assets/fonts/Pretendard-Regular.woff2';
+
+export const fonts = css`
+  @font-face {
+    font-family: Pretendard;
+
+    font-weight: 400;
+    src: url(${PretendardRegular}) format('woff2');
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: Pretendard;
+
+    font-weight: 700;
+    src: url(${PretendardBold}) format('woff2');
+    font-display: swap;
+  }
+`;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,14 @@ import { ThemeProvider, Global } from '@emotion/react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { fonts } from './common/styles/fonts';
 import { resetCss } from './common/styles/reset';
 import { THEME } from './common/styles/theme';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={THEME}>
-      <Global styles={resetCss} />
+      <Global styles={[resetCss, fonts]} />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
