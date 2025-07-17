6dae750eecf895f0b7de3b9abdc1969d73144eff
ppparkta <pppppark2@gmail.com>
2025-07-17 17:55:11 +0900

    refactor: Jpa 관련 설정 파일 분리 #11
    Co-authored-by: kimdevv <kim__dev@hufs.ac.kr>

--- backend/fittoring/src/main/java/fittoring/FittoringApplication.java
+++ backend/fittoring/src/main/java/fittoring/FittoringApplication.java
@@ -2,14 +2,11 @@ package fittoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class FittoringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FittoringApplication.class, args);
     }
-
 }
