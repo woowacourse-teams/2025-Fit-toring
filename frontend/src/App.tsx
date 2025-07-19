9e4e7c98dc07e963c973eb40634538ef3e3be048
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-07-19 12:02:01 +0900

    feat: detail 페이지 라우팅 추가 #30
    현재 api 가 나오지 않아서 path를 /detail 로 설정 추후 api 가 나온다면 /detail/{id} 와 같이 변경 필요

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import MobileLayout from './common/components/MobileLayout/MobileLayout';
+import Detail from './domain/mentoring/pages/Detail';
 import Home from './domain/mentoring/pages/Home';
 
-const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/detail', element: <Detail /> }, // TODO: API 연결 시 /detail/{id} 와 같이 변경 필요
+]);
 
 function App() {
   return (
