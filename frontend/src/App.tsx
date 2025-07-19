c71ddbda2a20622ff78aec30bdb68e023a5b0ed2
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-07-19 15:12:59 +0900

    refactor: 주석 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Detail from './domain/mentoring/pages/Detail';
 import Home from './domain/mentoring/pages/Home';
 
 const router = createBrowserRouter([
+  // TODO: 데모데이 전 path 상수화
   { path: '/', element: <Home /> },
   { path: '/detail', element: <Detail /> }, // TODO: API 연결 시 /detail/{id} 와 같이 변경 필요
 ]);
