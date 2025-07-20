89bf4e0f7d5adf5a58252a2ed99033dc6a993a9d
kimyou1102 <yoosk5485@naver.com>
2025-07-20 17:19:38 +0900

    feat: TextWithIcon 컴포넌트 구현 #77

---
+++ frontend/src/domain/booking/components/TextWithIcon/TextWithIcon.tsx
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+interface TextWithIconProps {
+  text: string;
+  iconSrc: string;
+  iconName: string;
+}
+
+function TextWithIcon({ text, iconSrc, iconName }: TextWithIconProps) {
+  return (
+    <StyledContainer>
+      <StyledImg alt={`${iconName} 아이콘`} src={iconSrc} />
+      <StyledSpan>{text}</StyledSpan>
+    </StyledContainer>
+  );
+}
+
+export default TextWithIcon;
+
+const StyledContainer = styled.div`
+  display: flex;
+  gap: 0.3rem;
+  align-items: center;
+`;
+
+const StyledImg = styled.img`
+  width: 1.4rem;
+  height: 1.4rem;
+`;
+
+const StyledSpan = styled.span`
+  color: ${({ theme }) => theme.FONT.B03};
+  font-size: 1.2rem;
+  display: flex;
+`;
