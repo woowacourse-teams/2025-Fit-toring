0eb14010468f1eb350ee51f157161b59d11846e8
kimyou1102 <yoosk5485@naver.com>
2025-07-20 18:16:45 +0900

    feat: CategoryTag 스토리 추가 #77

---
+++ frontend/src/domain/booking/components/CategoryTag/CategoryTag.stories.tsx
@@ -0,0 +1,36 @@
+import { ThemeProvider } from '@emotion/react';
+
+import { THEME } from '../../../../common/styles/theme';
+
+import CategoryTag from './CategoryTag';
+
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+const meta = {
+  title: 'booking/CategoryTag',
+  component: CategoryTag,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={THEME}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+} satisfies Meta<typeof CategoryTag>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    tagName: '카테고리 태그',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: '기본 카테고리 태그입니다.',
+      },
+    },
+  },
+};
