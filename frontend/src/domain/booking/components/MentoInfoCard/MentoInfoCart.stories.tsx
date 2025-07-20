333069a52f2e49091bcfa5981414f049bb126fa6
kimyou1102 <yoosk5485@naver.com>
2025-07-20 18:21:43 +0900

    feat: MentoInfoCard 스토리 추가 #77

---
+++ frontend/src/domain/booking/components/MentoInfoCard/MentoInfoCart.stories.tsx
@@ -0,0 +1,33 @@
+import { ThemeProvider } from '@emotion/react';
+
+import { THEME } from '../../../../common/styles/theme';
+
+import MentoInfoCard from './MentoInfoCard';
+
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+const meta = {
+  title: 'booking/MentoInfoCard',
+  component: MentoInfoCard,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={THEME}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+} satisfies Meta<typeof MentoInfoCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: '기본 예약페이지의 멘토 정보 카드입니다.',
+      },
+    },
+  },
+};
