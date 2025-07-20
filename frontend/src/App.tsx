28e259e6963dd5cc3cc1c54dd10cca3a0b8f2299
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-20 22:36:52 +0900

    refactor: PAGE_URL 상수를 사용하여 라우터 경로 수정 #34

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import MobileLayout from './common/components/MobileLayout/MobileLayout';
+import { PAGE_URL } from './domain/mentoring/constants/url';
 import Home from './domain/mentoring/pages/Home';
 
-const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: PAGE_URL.HOME, element: <Home /> },
+]);
 
 function App() {
   return (
