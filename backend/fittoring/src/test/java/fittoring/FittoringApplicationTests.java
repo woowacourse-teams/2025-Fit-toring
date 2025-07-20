e90b5955f0933915942ebeedeae8e0d864c942f1
ppparkta <pppppark2@gmail.com>
2025-07-20 21:09:19 +0900

    test: Application 테스트 DB 설정 추가 #65
    - 테스트 시 값 초기화 문제로 인해 수정했습니다.

--- backend/fittoring/src/test/java/fittoring/FittoringApplicationTests.java
+++ backend/fittoring/src/test/java/fittoring/FittoringApplicationTests.java
@@ -1,8 +1,13 @@
 package fittoring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @SpringBootTest
 class FittoringApplicationTests {
 
