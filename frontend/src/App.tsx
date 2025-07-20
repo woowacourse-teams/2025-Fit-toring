19282bca08718db9fc5b7a7758b9c32893576794
kimyou1102 <yoosk5485@naver.com>
2025-07-20 18:05:37 +0900

    feat: 예약페이지 라우팅 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import MobileLayout from './common/components/MobileLayout/MobileLayout';
+import MentoringBooking from './domain/booking/pages/MentoringBooking';
 import Home from './domain/mentoring/pages/Home';
 
-const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/booking', element: <MentoringBooking /> },
+]);
 
 function App() {
   return (
