b6ed7a5a4823044431cedfe85c5bb5d3be94fb6a
ppparkta <pppppark2@gmail.com>
2025-07-20 21:08:52 +0900

    refactor: 외부 API 타임아웃 전역 설정 #65
    - 테스트 시 값 초기화 문제로 인해 수정했습니다.

--- backend/fittoring/src/main/java/fittoring/config/RestClientConfiguration.java
+++ backend/fittoring/src/main/java/fittoring/config/RestClientConfiguration.java
@@ -1,30 +1,41 @@
 package fittoring.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfiguration {
 
+    @Value("${sms.base-url}")
+    private String baseUrl;
+
+    @Value("${sms.timeout.connect}")
+    private int connectTimeout;
+
+    @Value("${sms.timeout.read}")
+    private int readTimeout;
+
     @Bean
-    public RestClient smsRestClient(@Autowired RestClient.Builder restClientBuilder) {
-        return restClientBuilder.baseUrl("https://api.solapi.com")
+    public RestClient smsRestClient(RestClient.Builder builder) {
+        return builder.baseUrl(baseUrl)
                 .defaultHeader("Content-Type", "application/json")
                 .build();
     }
 
-    private RestClientCustomizer restClientCustomizer() {
+    @Bean
+    public RestClientCustomizer timeoutCustomizer() {
         return restClientBuilder -> restClientBuilder.requestFactory(simpleClientHttpRequestFactory());
     }
 
-    private SimpleClientHttpRequestFactory simpleClientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
-        simpleClientHttpRequestFactory.setConnectTimeout(1_200);
-        simpleClientHttpRequestFactory.setReadTimeout(5_000);
-        return simpleClientHttpRequestFactory;
+    private ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(connectTimeout);
+        factory.setReadTimeout(readTimeout);
+        return factory;
     }
 }
