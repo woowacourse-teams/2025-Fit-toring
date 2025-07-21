3f467d874d60563f0174efdf895c42f52d200cb3
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-07-21 23:16:10 +0900

    feat: Summary 컴포넌트 생성 #33

--- frontend/src/pages/detail/Detail.tsx
+++ frontend/src/pages/detail/Detail.tsx
@@ -1,13 +1,26 @@
+import styled from '@emotion/styled';
+
 import DetailHeader from './components/DetailHeader/DetailHeader';
 import Profile from './components/Profile/Profile';
+import Summary from './components/Summary/Summary';
 
 function Detail() {
   return (
     <>
       <DetailHeader />
       <Profile />
+      <Summary />
+      <StyledH4>멘토 소개</StyledH4>
     </>
   );
 }
 
 export default Detail;
+
+const StyledH4 = styled.h4`
+  display: flex;
+  justify-content: center;
+
+  ${({ theme }) => theme.TYPOGRAPHY.H4_R}
+  color: ${({ theme }) => theme.FONT.B01};
+`;
---
+++ frontend/src/pages/detail/components/Summary/Summary.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+
+function Summary() {
+  return (
+    <StyledContainer>
+      <StyledBoldText>
+        5년차 전문 트레이너로 개인 맞춤 운동 및 식단 코칭을 제공합니다.
+      </StyledBoldText>
+      <StyledSummary>
+        <p>경력: 전문 트레이너 5년 </p>
+        <p>자격증: 생활스포츠지도사, 운동처방사</p>
+      </StyledSummary>
+      <StyledHr />
+    </StyledContainer>
+  );
+}
+
+export default Summary;
+
+const StyledContainer = styled.section`
+  display: flex;
+  width: 100%;
+  margin-top: 3rem;
+  padding: 1rem;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+`;
+const StyledBoldText = styled.p`
+  ${({ theme }) => theme.TYPOGRAPHY.B2_B}
+  color: ${({ theme }) => theme.FONT.B02}
+`;
+
+const StyledSummary = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.1rem;
+
+  ${({ theme }) => theme.TYPOGRAPHY.B4_R}
+  color: ${({ theme }) => theme.FONT.B02}
+`;
+
+const StyledHr = styled.hr`
+  width: 100%;
+  height: 0.1rem;
+  border: 1px solid ${({ theme }) => theme.LINE.REGULAR};
+`;
