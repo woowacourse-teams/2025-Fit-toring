e16e3c5b43d87366a2a71579dd5c4acd9381f8cc
kimyou1102 <yoosk5485@naver.com>
2025-07-21 20:44:24 +0900

    style: Header컴포넌트 최상위 요소 position: fixed -> sticky로 변경 #23
    - fixed일 경우 다른 컴포넌트에서 margin-top을 줘야함
    - sticky는 레이아웃 흐름에 포함되기 때문에 따로 margin-top을 주지 않아도 됨

--- frontend/src/common/components/Header/Header.tsx
+++ frontend/src/common/components/Header/Header.tsx
@@ -9,12 +9,13 @@ function Header({ children }: PropsWithChildren) {
 export default Header;
 
 const StyledContainer = styled.header`
-  position: fixed;
+  position: sticky;
+  top: 0px;
   width: 48rem;
   height: 5.7rem;
   border-bottom: 1px solid ${({ theme }) => theme.LINE.REGULAR};
 
-  background: rgb(${({ theme }) => theme.BG.LIGHT} / 80%);
+  background: rgb(from ${({ theme }) => theme.BG.WHITE} r g b / 80%);
   box-shadow:
     0 1px 3px 0 rgb(0 0 0 / 10%),
     0 1px 2px -1px rgb(0 0 0 / 10%);
