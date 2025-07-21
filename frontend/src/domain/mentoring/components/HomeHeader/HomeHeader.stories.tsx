c9d6ad4a6138f51b228ee18ba9a4f56ab25d5192
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-21 14:58:09 +0900

    test: HomeHeader 스토리 추가 #34

---
+++ frontend/src/domain/mentoring/components/HomeHeader/HomeHeader.stories.tsx
@@ -0,0 +1,40 @@
+import { ThemeProvider } from '@emotion/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { THEME } from '../../../../common/styles/theme';
+import { PAGE_URL } from '../../../common/constants/url';
+
+import HomeHeader from './HomeHeader';
+
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+const meta = {
+  title: 'Home/HomeHeader',
+  component: HomeHeader,
+
+  tags: ['autodocs'],
+
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={THEME}>
+        <MemoryRouter initialEntries={[PAGE_URL.HOME]}>
+          <Story />
+        </MemoryRouter>
+      </ThemeProvider>
+    ),
+  ],
+} satisfies Meta<typeof HomeHeader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'HomeHeader 컴포넌트는 홈 페이지의 헤더를 구성합니다. 로고와 제목을 포함하고 있습니다. 로고 클릭 시 메인 홈 페이지로 이동합니다.',
+      },
+    },
+  },
+};
