1baee11476eca756aeb7d2fcad4f14401baa7067
kimyou1102 <yoosk5485@naver.com>
2025-07-21 22:08:11 +0900

    feat: BookingHeader 컴포넌트 스토리 파일 추가 #23

---
+++ frontend/src/pages/booking/components/BookingHeader/BookingHeader.stories.tsx
@@ -0,0 +1,38 @@
+import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { THEME } from '../../../../common/styles/theme';
+
+import BookingHeader from './BookingHeader';
+
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+const meta = {
+  title: 'Booking/BookingHeader',
+  component: BookingHeader,
+  parameters: {},
+
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <ThemeProvider theme={THEME}>
+          <Story />
+        </ThemeProvider>
+      </BrowserRouter>
+    ),
+  ],
+} satisfies Meta<typeof BookingHeader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: '헤더입니다.',
+      },
+    },
+  },
+};
