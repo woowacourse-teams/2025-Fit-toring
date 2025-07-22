6f3556bc5dc40ff03d9731bde3aef69574fd19c3
kimyou1102 <yoosk5485@naver.com>
2025-07-22 15:51:15 +0900

    feat: 예약페이지에 BookingForm 컴포넌트 적용 #42

--- frontend/src/pages/booking/Booking.tsx
+++ frontend/src/pages/booking/Booking.tsx
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled';
 
+import BookingForm from './components/BookingForm/BookingForm';
 import MentoInfoCard from './components/MentorInfoCard/MentorInfoCard';
 
 function Booking() {
   return (
     <StyledContainer>
+      <BookingForm />
       <MentoInfoCard />
     </StyledContainer>
   );
