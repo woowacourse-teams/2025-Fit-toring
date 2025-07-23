5a5095a2f5f7eedd4da867c32e8e252950b6455b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-24 01:11:49 +0900

    feat: Home 컴포넌트에서 MentorCardList 추가 및 불필요한 스타일 제거 #37

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -4,18 +4,19 @@ import Footer from '../../common/components/Footer/Footer';
 
 import HomeHeader from './components/HomeHeader/HomeHeader';
 import MentorCardItem from './components/MentorCardItem/MentorCardItem';
+import MentorCardList from './components/MentorCardList/MentorCardList';
 import Slogan from './components/Slogan/Slogan';
 
 function Home() {
   return (
     <StyledContainer>
       <HomeHeader />
-      <Slogan />
-      <StyledSection>
-        <StyledUl>
+      <StyledContents>
+        <Slogan />
+        <MentorCardList>
           <MentorCardItem />
-        </StyledUl>
-      </StyledSection>
+        </MentorCardList>
+      </StyledContents>
       <Footer>문의: fitoring7@gmail.com</Footer>
     </StyledContainer>
   );
@@ -30,12 +31,8 @@ const StyledContainer = styled.div`
   height: 100%;
 `;
 
-const StyledSection = styled.div`
-  flex-grow: 1;
-`;
-
-const StyledUl = styled.ul`
+const StyledContents = styled.div`
   display: flex;
-  justify-content: center;
-  padding: 1rem 1.4rem;
+  flex-direction: column;
+  gap: 2rem;
 `;
