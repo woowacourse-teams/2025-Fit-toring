9c9c8f551d78c56f4b938adb906eb52f874ed4c2
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-23 21:15:37 +0900

    feat: 홈 페이지에 멘토 수 및 서비스 소개 개요 컴포넌트 추가 #39

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -3,14 +3,17 @@ import styled from '@emotion/styled';
 import Footer from '../../common/components/Footer/Footer';
 
 import HomeHeader from './components/HomeHeader/HomeHeader';
+import MentorOverview from './components/MentorOverview/MentorOverview';
 import Slogan from './components/Slogan/Slogan';
 
 function Home() {
   return (
     <StyledContainer>
       <HomeHeader />
-      <Slogan />
-      <StyledSection>section</StyledSection>
+      <StyledContents>
+        <Slogan />
+        <MentorOverview mentorCount={6} />
+      </StyledContents>
       <Footer>문의: fitoring7@gmail.com</Footer>
     </StyledContainer>
   );
@@ -25,6 +28,8 @@ const StyledContainer = styled.div`
   height: 100%;
 `;
 
-const StyledSection = styled.div`
-  flex-grow: 1;
+const StyledContents = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 `;
