554f1267d806bb856f0515a0d6b8bf0751f0f876
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-07-24 00:18:34 +0900

    feat: api 연결 로직 추가 #41

--- frontend/src/pages/detail/Detail.tsx
+++ frontend/src/pages/detail/Detail.tsx
@@ -1,4 +1,9 @@
+import { useEffect, useState } from 'react';
+
 import styled from '@emotion/styled';
+import { useParams } from 'react-router-dom';
+
+import { apiClient } from '../../common/apis/apiClient';
 
 import ApplySection from './components/ApplySection/ApplySection';
 import DetailHeader from './components/DetailHeader/DetailHeader';
@@ -6,14 +11,51 @@ import Introduction from './components/Introduction/Introduction';
 import MentorSummary from './components/MentorSummary/MentorSummary';
 import Profile from './components/Profile/Profile';
 
+interface MentoringResponse {
+  id: number;
+  mentorName: string;
+  categories: string[];
+  price: number;
+  career: number;
+  imageUrl: string;
+  introduction: string;
+}
+
 function Detail() {
+  const { mentoringId } = useParams();
+  const [data, setData] = useState<MentoringResponse | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await apiClient.get<MentoringResponse>({
+        endpoint: `/mentorings/${mentoringId}`,
+      });
+
+      setData(response);
+    } catch (error) {
+      console.error('fetchData 실패', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [mentoringId]);
+
+  if (!data) {
+    return <div>로딩 중...</div>;
+  }
+
   return (
     <StyledContainer>
       <DetailHeader />
-      <Profile />
-      <MentorSummary />
+      <Profile
+        profileImg={data.imageUrl}
+        mentorName={data.mentorName}
+        categories={data.categories}
+      />
+      <MentorSummary introduction={data.introduction} career={data.career} />
       <Introduction />
-      <ApplySection />
+      <ApplySection price={data.price} />
     </StyledContainer>
   );
 }
