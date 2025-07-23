5a01cde4fe9239019af14f3fd30e246bc417bfec
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-24 03:38:04 +0900

    feat: SpecialtyModal에 SpecialtyCheckbox 추가 및 선택 기능 구현 #39

--- frontend/src/pages/home/components/SpecialtyFilterModal/SpecialtyFilterModal.tsx
+++ frontend/src/pages/home/components/SpecialtyFilterModal/SpecialtyFilterModal.tsx
@@ -1,6 +1,23 @@
+import { useState } from 'react';
+
 import styled from '@emotion/styled';
 
 import Modal from '../../../../common/components/Modal/Modal';
+import SpecialtyCheckbox from '../SpecialtyCheckbox/SpecialtyCheckbox';
+
+const ALL_SPECIALTIES = [
+  { id: 'weightLoss', label: '체중 감량' },
+  { id: 'muscleGain', label: '근력 증진' },
+  { id: 'bodyCorrection', label: '체형 교정' },
+  { id: 'muscleExercise', label: '근력 운동' },
+  { id: 'bulkUp', label: '벌크업' },
+  { id: 'rehabExercise', label: '재활 운동' },
+  { id: 'nutritionCounseling', label: '영양 상담' },
+  { id: 'postureCorrection', label: '자세 교정' },
+  { id: 'stretching', label: '스트레칭' },
+  { id: 'diet', label: '다이어트' },
+  { id: 'competitionPrep', label: '대회 준비' },
+] as const;
 
 interface SpecialtyModalProps {
   opened: boolean;
@@ -19,11 +36,35 @@ function SpecialtyModal({
   handleReset,
   handleApply,
 }: SpecialtyModalProps) {
+  const [temporarySelectedSpecialties, setTemporarySelectedSpecialties] =
+    useState<string[]>(selectedSpecialties);
+
+  if (!opened) return null;
+
+  const handleTemporarySpecialtyToggle = (specialtyId: string) => {
+    setTemporarySelectedSpecialties((prev) =>
+      prev.includes(specialtyId)
+        ? prev.filter((id) => id !== specialtyId)
+        : [...prev, specialtyId],
+    );
+  };
+
   return (
     <Modal opened={opened} onCloseClick={handleCloseModal}>
       <StyledContainer>
         <StyledTitle>전문 분야</StyledTitle>
         <StyledLine />
+
+        <StyledSpecialtyWrapper>
+          {ALL_SPECIALTIES.map((specialty) => (
+            <SpecialtyCheckbox
+              key={specialty.id}
+              specialty={specialty.label}
+              checked={temporarySelectedSpecialties.includes(specialty.id)}
+              onChange={() => handleTemporarySpecialtyToggle(specialty.id)}
+            />
+          ))}
+        </StyledSpecialtyWrapper>
       </StyledContainer>
     </Modal>
   );
@@ -51,4 +92,15 @@ const StyledLine = styled.hr`
   margin: 0;
   border: none;
   border-top: 1px solid ${({ theme }) => theme.BORDER.GRAY300};
+`;
+
+const StyledSpecialtyWrapper = styled.div`
+  display: flex;
+  padding: 0.4rem;
+  overflow-y: auto;
+  flex-wrap: wrap;
+  gap: 0.8rem;
+  justify-content: center;
+
+  max-height: 15rem;
 `;
