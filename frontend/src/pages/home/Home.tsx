a5fb17cbbd72a8cc3bd13ec334ed70360d3c6c9d
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-24 22:33:10 +0900

    feat: 멘토 목록 가져오기 기능 추가 및 관련 상태 관리 개선 #26

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,16 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styled from '@emotion/styled';
 
 import Footer from '../../common/components/Footer/Footer';
 
+import { getMentorList } from './apis/getMentorList';
 import HomeHeader from './components/HomeHeader/HomeHeader';
-import MentorOverview from './components/MentorOverview/MentorOverview';
 import MentorCardItem from './components/MentorCardItem/MentorCardItem';
 import MentorCardList from './components/MentorCardList/MentorCardList';
+import MentorOverview from './components/MentorOverview/MentorOverview';
 import Slogan from './components/Slogan/Slogan';
 import SpecialtyFilterModal from './components/SpecialtyFilterModal/SpecialtyFilterModal';
 import SpecialtyFilterModalButton from './components/SpecialtyFilterModalButton/SpecialtyFilterModalButton';
+
+import type { MentorInformation } from './types/MentorInformation';
+
+const convertSelectedSpecialtiesToParams = (
+  selectedSpecialties: string[],
+): Record<string, string> => {
+  const params: Record<string, string> = {};
+  selectedSpecialties.forEach((specialty, index) => {
+    params[`categoryTitle${index + 1}`] = specialty;
+  });
+
+  return params;
+};
 
 function Home() {
   const [modalOpened, setModalOpened] = useState(false);
@@ -27,12 +41,31 @@ function Home() {
     setSelectedSpecialties(specialties);
     handleCloseModal();
   };
+
+  const [mentorList, setMentorList] = useState<MentorInformation[]>([]);
+
+  useEffect(() => {
+    const fetchMentorData = async () => {
+      try {
+        const data = await getMentorList({
+          params: convertSelectedSpecialtiesToParams(selectedSpecialties),
+        });
+
+        setMentorList(data);
+      } catch (error) {
+        console.error('멘토 데이터 가져오기 실패:', error);
+      }
+    };
+
+    fetchMentorData();
+  }, [selectedSpecialties]);
+
   return (
     <StyledContainer>
       <HomeHeader />
       <StyledContents>
         <Slogan />
-        <MentorOverview mentorCount={6} />
+        <MentorOverview mentorCount={mentorList.length} />
         <SpecialtyFilterModalButton handleOpenModal={handleOpenModal} />
         <SpecialtyFilterModal
           opened={modalOpened}
@@ -41,7 +74,9 @@ function Home() {
           handleApply={handleApply}
         />
         <MentorCardList>
-          <MentorCardItem />
+          {mentorList.map((mentor) => (
+            <MentorCardItem key={mentor.id} mentor={mentor} />
+          ))}
         </MentorCardList>
       </StyledContents>
       <Footer>문의: fitoring7@gmail.com</Footer>
@@ -64,5 +99,4 @@ const StyledContents = styled.main`
   align-items: center;
   gap: 2rem;
   padding: 0 1.4rem;
-
 `;
