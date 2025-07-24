9037df88fad9039db96c10cd9dacea2ba35a8cd4
kimyou1102 <yoosk5485@naver.com>
2025-07-24 22:46:15 +0900

    refactor: 컨벤션에 맞게 if 중괄호 추가 #49

--- frontend/src/pages/booking/utils/getFormattedPhoneNumber .ts
+++ frontend/src/pages/booking/utils/getFormattedPhoneNumber .ts
@@ -1,8 +1,11 @@
 export const getFormattedPhoneNumber = (input: string) => {
   const onlyNums = input.replace(/\D/g, ''); // 숫자만 남기고 나머지 문자 제거
 
-  if (onlyNums.length < 4) return onlyNums;
-  if (onlyNums.length < 8)
+  if (onlyNums.length < 4) {
+    return onlyNums;
+  }
+  if (onlyNums.length < 8) {
     return `${onlyNums.slice(0, 3)}-${onlyNums.slice(3)}`;
+  }
   return `${onlyNums.slice(0, 3)}-${onlyNums.slice(3, 7)}-${onlyNums.slice(7, 11)}`;
 };
