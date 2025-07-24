eee842ad9e5c13ff0551d348738a00ea407589f3
kimyou1102 <yoosk5485@naver.com>
2025-07-24 22:49:00 +0900

    test: 테스트 설명 수정 및 해당하는 테스트블럭으로 테스트 검증 이동 # 49

--- frontend/__tests__/getFormatPhoneNumber.test.ts
+++ frontend/__tests__/getFormatPhoneNumber.test.ts
@@ -3,7 +3,7 @@ import { describe, it, expect } from 'vitest';
 import { getFormattedPhoneNumber } from '../src/pages/booking/utils/getFormattedPhoneNumber ';
 
 describe('getFormattedPhoneNumber 유틸 함수 테스트', () => {
-  it('숫자 3자리 미만 입력 시 그대로 반환', () => {
+  it('숫자 3자리 이하 입력 시 그대로 반환', () => {
     expect(getFormattedPhoneNumber('')).toBe('');
     expect(getFormattedPhoneNumber('0')).toBe('0');
     expect(getFormattedPhoneNumber('01')).toBe('01');
@@ -14,10 +14,10 @@ describe('getFormattedPhoneNumber 유틸 함수 테스트', () => {
     expect(getFormattedPhoneNumber('0123')).toBe('012-3');
     expect(getFormattedPhoneNumber('01234')).toBe('012-34');
     expect(getFormattedPhoneNumber('0123456')).toBe('012-3456');
-    expect(getFormattedPhoneNumber('01234567')).toBe('012-3456-7');
   });
 
   it('8자리 이상 숫자 입력 시 하이픈 두 개 추가', () => {
+    expect(getFormattedPhoneNumber('01234567')).toBe('012-3456-7');
     expect(getFormattedPhoneNumber('012345678')).toBe('012-3456-78');
     expect(getFormattedPhoneNumber('0123456789')).toBe('012-3456-789');
     expect(getFormattedPhoneNumber('01234567890')).toBe('012-3456-7890');
