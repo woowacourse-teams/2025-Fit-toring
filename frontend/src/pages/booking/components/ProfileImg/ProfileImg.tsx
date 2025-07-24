95b96e2e0128aa0e7661e5c6bda8c6205a245fc9
kimyou1102 <yoosk5485@naver.com>
2025-07-24 15:38:19 +0900

    feat: 프로필 이미지 오류시 기본 이미지 지정 #119

--- frontend/src/pages/booking/components/ProfileImg/ProfileImg.tsx
+++ frontend/src/pages/booking/components/ProfileImg/ProfileImg.tsx
@@ -1,11 +1,19 @@
 import styled from '@emotion/styled';
 
+import profileImg from '../../../../common/assets/images/profileImg.svg';
+
 interface ProfileImgProps {
   src: string;
 }
 
 function ProfileImg({ src }: ProfileImgProps) {
-  return <StyledContainer src={src} alt="프로필 이미지" />;
+  const handleImgError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    const target = e.target as HTMLImageElement;
+    target.src = profileImg; // TODO: 임시 기본이미지 추후에 변경해야함
+  };
+  return (
+    <StyledContainer src={src} alt="프로필 이미지" onError={handleImgError} />
+  );
 }
 
 export default ProfileImg;
