c947efbf765ab8d83687e8d9b51f1b73a9ac5d55
kimyou1102 <yoosk5485@naver.com>
2025-07-27 16:05:13 +0900

    feat: 전화번호 길이 검증 및 에러메시지 리턴 유틸함수 생성 #153

---
+++ frontend/src/pages/booking/utils/phoneNumberValidator.ts
@@ -0,0 +1,16 @@
+import { ERROR_MESSAGE } from '../constants/errorMessage';
+
+const validatePhoneNumberLength = (phoneNumber: string) => {
+  if (phoneNumber === '') return '';
+
+  const digits = phoneNumber.replace(/\D/g, '');
+  return digits.length !== 11;
+};
+
+export const getPhoneNumberErrorMessage = (phoneNumber: string) => {
+  if (validatePhoneNumberLength(phoneNumber)) {
+    return ERROR_MESSAGE.INVALID_PHONE_NUMBER_LENGTH;
+  }
+
+  return '';
+};
