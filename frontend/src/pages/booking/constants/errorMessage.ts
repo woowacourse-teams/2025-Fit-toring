34b723dce67890a8164a47ebad0d484d4acfaacb
kimyou1102 <yoosk5485@naver.com>
2025-07-27 16:04:35 +0900

    feat: 에러메시지 상수 파일 생성 #153

---
+++ frontend/src/pages/booking/constants/errorMessage.ts
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGE = {
+  INVALID_MENTEE_NAME_LENGTH: '이름은 1자 이상 5자 미만으로 입력해주세요.',
+  INVALID_MENTEE_NAME_CHARACTERS: '이름은 한글만 입력할 수 있습니다.',
+  INVALID_PHONE_NUMBER_LENGTH: '전화번호 11자리를 입력해주세요.',
+};
