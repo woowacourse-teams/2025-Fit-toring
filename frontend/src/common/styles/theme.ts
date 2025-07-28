f1d04d5d9e0ad95acd3a9f4417707ed971ba1044
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-28 14:34:17 +0900

    style: theme.ts에서 Pretendard 폰트 속성 제거 #78
    Co-authored-by: Beomtae <Beomtae@users.noreply.github.com>
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

--- frontend/src/common/styles/theme.ts
+++ frontend/src/common/styles/theme.ts
@@ -51,197 +51,141 @@ export const THEME = {
   },
   TYPOGRAPHY: {
     H1_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 23px;
       line-height: normal;
     `,
     H1_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 23px;
       line-height: normal;
     `,
     H2_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 20px;
       line-height: normal;
     `,
     H2_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 20px;
       line-height: normal;
     `,
     H3_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 18px;
       line-height: normal;
     `,
     H3_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 18px;
       line-height: normal;
     `,
     H4_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 16px;
       line-height: normal;
     `,
     H4_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 16px;
       line-height: normal;
     `,
     B1_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 15px;
       line-height: normal;
     `,
     B1_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 15px;
       line-height: 160%; /* LH행간: 160% */
     `,
     B2_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 14px;
       line-height: normal;
     `,
     B2_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 14px;
       line-height: 150%; /* LH행간: 150% */
     `,
     B3_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 13px;
       line-height: normal;
     `,
     B3_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 13px;
       line-height: normal;
     `,
     B4_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 12px;
       line-height: normal;
     `,
     B4_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 12px;
       line-height: 125%; /* LH행간: 125% */
     `,
     C1_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 13px;
       line-height: normal;
     `,
     C2_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 12px;
       line-height: normal;
     `,
     C3_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 11px;
       line-height: normal;
     `,
     C4_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 10px;
       line-height: normal;
     `,
     BTN1_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 16px;
       line-height: normal;
     `,
     BTN1_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 16px;
       line-height: normal;
     `,
     BTN2_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 14px;
       line-height: normal;
     `,
     BTN2_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 14px;
       line-height: normal;
     `,
     BTN3_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 13px;
       line-height: normal;
     `,
     BTN3_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 13px;
       line-height: normal;
     `,
     BTN4_B: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 700;
       font-size: 12px;
       line-height: normal;
     `,
     BTN4_R: css`
-      font-family: Pretendard, sans-serif;
-
       font-weight: 400;
       font-size: 12px;
       line-height: normal;
