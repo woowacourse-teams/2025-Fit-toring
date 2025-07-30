2a4be0987dbd83fefe9beed04b968ecdf12cd255
kimyou1102 <yoosk5485@naver.com>
2025-07-30 15:49:26 +0900

    refactor: 변경될 수 있는 이름관련 숫자 상수화 #153

--- frontend/src/pages/booking/constants/errorMessage.ts
+++ frontend/src/pages/booking/constants/errorMessage.ts
@@ -1,5 +1,7 @@
+import { NAME } from './name';
+
 export const ERROR_MESSAGE = {
-  INVALID_MENTEE_NAME_LENGTH: '이름은 1자 이상 5자 미만으로 입력해주세요.',
+  INVALID_MENTEE_NAME_LENGTH: `이름은 ${NAME.MIN_LENGTH}자 이상 ${NAME.MAX_LENGTH}자 미만으로 입력해주세요.`,
   INVALID_MENTEE_NAME_CHARACTERS: '이름은 한글만 입력할 수 있습니다.',
   INVALID_PHONE_NUMBER_LENGTH: '전화번호 11자리를 입력해주세요.',
 };
