a5d970af4a6768a65c20d14eacef9bb7bd935745
kimyou1102 <yoosk5485@naver.com>
2025-07-31 00:31:47 +0900

    feat: 회원가입 페이지에 SignupIntro 컴포넌트 적용 #210

--- frontend/src/pages/signup/Signup.tsx
+++ frontend/src/pages/signup/Signup.tsx
@@ -1,9 +1,11 @@
 import SignupHeader from './components/SignupHeader/SignupHeader';
+import SignupIntro from './components/SignupIntro/SignupIntro';
 
 function Signup() {
   return (
     <div>
       <SignupHeader />
+      <SignupIntro />
     </div>
   );
 }
