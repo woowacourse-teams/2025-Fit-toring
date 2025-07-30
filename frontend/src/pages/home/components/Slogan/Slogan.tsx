93cdeec6516845d3eeac814c5bfbc8c5cc301cc1
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-30 19:43:40 +0900

    refactor: Slogan에서 작은 설명 문구 제거 #199

--- frontend/src/pages/home/components/Slogan/Slogan.tsx
+++ frontend/src/pages/home/components/Slogan/Slogan.tsx
@@ -7,10 +7,6 @@ function Slogan() {
         내가 알고 싶은 운동 & 식단, <StyledStrong>핏토링</StyledStrong>에서
         물어봐요!
       </StyledTitle>
-      <StyledDescription>
-        전문 트레이너와 운동 애호가들로부터 15분 단위로 합리적인 비용의 1회성
-        멘토링을 받아보세요
-      </StyledDescription>
     </StyledContainer>
   );
 }
@@ -46,13 +42,4 @@ const StyledTitle = styled.h1`
 
 const StyledStrong = styled.strong`
   color: ${({ theme }) => theme.SYSTEM.MAIN600};
-`;
-
-const StyledDescription = styled.p`
-  width: 28.2rem;
-
-  color: ${({ theme }) => theme.FONT.B03};
-  text-align: center;
-
-  ${({ theme }) => theme.TYPOGRAPHY.B2_R}
 `;
