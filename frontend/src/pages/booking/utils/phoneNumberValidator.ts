2198fa45868a08d736540560173c6ce9b17665a8
kimyou1102 <yoosk5485@naver.com>
2025-07-30 15:42:55 +0900

    refactor: 매직넘버 상수화 #153

--- frontend/src/pages/booking/utils/phoneNumberValidator.ts
+++ frontend/src/pages/booking/utils/phoneNumberValidator.ts
@@ -1,12 +1,14 @@
 import { ERROR_MESSAGE } from '../constants/errorMessage';
 
+const VALID_PHONE_NUMBER_LENGTH = 11;
+
 const validatePhoneNumberLength = (phoneNumber: string) => {
   if (phoneNumber === '') {
     return false;
   }
 
   const digits = phoneNumber.replace(/\D/g, '');
-  return digits.length !== 11;
+  return digits.length !== VALID_PHONE_NUMBER_LENGTH;
 };
 
 export const getPhoneNumberErrorMessage = (phoneNumber: string) => {
