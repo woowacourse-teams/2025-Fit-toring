ec4aa3d8c4caa76bdedbf6e53e5160f79a698a6a
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-31 16:25:50 +0900

    test: MyPageHeader 스토리 추가 #189

---
+++ frontend/src/pages/myPage/components/MyPageHeader/MyPageHeader.stories.tsx
@@ -0,0 +1,38 @@
+import { ThemeProvider } from '@emotion/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { PAGE_URL } from '../../../../common/constants/url';
+import { THEME } from '../../../../common/styles/theme';
+
+import MyPageHeader from './MyPageHeader';
+
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+const meta = {
+  title: 'MyPage/MyPageHeader',
+  component: MyPageHeader,
+
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={THEME}>
+        <MemoryRouter initialEntries={[PAGE_URL.MY_PAGE]}>
+          <Story />
+        </MemoryRouter>
+      </ThemeProvider>
+    ),
+  ],
+} satisfies Meta<typeof MyPageHeader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'MyPageHeader 컴포넌트는 마이 페이지의 헤더를 구성합니다. 뒤로가기와 제목, 그리고 메뉴 버튼을 포함하고 있습니다.',
+      },
+    },
+  },
+};
