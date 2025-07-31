561d947d3476c7e8bdb69e6c0da44ca259b75fcc
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-07-31 20:49:22 +0900

    fix: story 변경 #181

--- frontend/src/pages/mentoringCreate/components/BaseInfo/BaseInfo.stories.tsx
+++ frontend/src/pages/mentoringCreate/components/BaseInfo/BaseInfo.stories.tsx
@@ -17,7 +17,7 @@ export const DefaultBaseInfo: Story = {
     docs: {
       description: {
         story:
-          'BaseInfo 컴포넌트는 멘토링 개설 페이지의 기본 정보 섹션을 구성합니다. 멘토링 제목, 상담료, 전화번호를 입력할 수 있는 폼을 포함하고 있습니다.',
+          'BaseInfo 컴포넌트는 멘토링 개설 페이지의 기본 정보 섹션을 구성합니다. 멘토의 이름, 상담료, 전화번호를 입력할 수 있는 폼을 포함하고 있습니다.',
       },
     },
   },
