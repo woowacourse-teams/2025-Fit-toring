67a2fedf5b4fa97635a0c30250006a33675eabaa
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-07-31 16:26:25 +0900

    feat: TitleSeparator 컴포넌트 구현 #181

---
+++ frontend/src/pages/mentoringCreate/components/TitleSeparator/TitleSeparator.tsx
@@ -0,0 +1,42 @@
+import type { PropsWithChildren } from 'react';
+
+import styled from '@emotion/styled';
+
+function TitleSeparator({ children }: PropsWithChildren) {
+  return (
+    <>
+      <StyledTitle>{children}</StyledTitle>
+      <StyledWrapper>
+        <StyledLeftBar />
+        <StyledRightBar />
+      </StyledWrapper>
+    </>
+  );
+}
+
+export default TitleSeparator;
+
+const StyledTitle = styled.h2`
+  margin-bottom: 1.7rem;
+
+  ${({ theme }) => theme.TYPOGRAPHY.H2_R};
+  color: ${({ theme }) => theme.FONT.B01};
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
+`;
+
+const StyledLeftBar = styled.div`
+  width: 6rem;
+  height: 0.2rem;
+
+  background-color: ${({ theme }) => theme.SYSTEM.MAIN600};
+`;
+
+const StyledRightBar = styled.div`
+  width: 100%;
+  height: 0.2rem;
+
+  background-color: ${({ theme }) => theme.SYSTEM.MAIN100};
+`;
