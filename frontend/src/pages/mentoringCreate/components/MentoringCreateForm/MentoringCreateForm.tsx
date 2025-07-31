226bee9e9f42bc1a0621ef79364821725c017dd0
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-07-31 16:28:07 +0900

    feat: MentoringCreateForm 에 BaseInfo 적용 #181

---
+++ frontend/src/pages/mentoringCreate/components/MentoringCreateForm/MentoringCreateForm.tsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+
+import BaseInfo from '../BaseInfo/BaseInfo';
+
+function MentoringCreateForm() {
+  return (
+    <StyledContainer>
+      <BaseInfo></BaseInfo>
+    </StyledContainer>
+  );
+}
+
+export default MentoringCreateForm;
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100%;
+  padding: 3.3rem;
+  border: 1px solid #e2e8f0;
+  border-radius: 16px;
+  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 10%);
+
+  background: #fff;
+
+  color: white;
+`;
