8183a4e7a11954ad512658ac4e1dd1256dab3845
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-31 15:28:50 +0900

    feat: MyPage 컴포넌트 추가 및 Layout 설정 #189

---
+++ frontend/src/pages/myPage/MyPage.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+function MyPage() {
+  return <StyledContainer></StyledContainer>;
+}
+
+export default MyPage;
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  height: 100%;
+`;
