c106239ed507ca98308c6e85ac9487ae38da9cc7
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-07-31 17:11:04 +0900

    refactor: lint 적용 #181

--- frontend/src/pages/mentoringCreate/components/mentoringCreateHeader/MentoringCreateHeader.tsx
+++ frontend/src/pages/mentoringCreate/components/mentoringCreateHeader/MentoringCreateHeader.tsx
@@ -1,8 +1,8 @@
-import Header from '../../../../common/components/Header/Header';
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 
 import backIcon from '../../../../common/assets/images/backIcon.svg';
+import Header from '../../../../common/components/Header/Header';
 
 function MentoringCreateHeader() {
   const navigate = useNavigate();
