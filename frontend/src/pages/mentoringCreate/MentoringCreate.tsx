f33faff03708e3983afd0bf0595d7adb511fbc20
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-07-31 16:28:32 +0900

    feat: MentoringCreate 에 MentoringCreateForm 적용 #181

--- frontend/src/pages/mentoringCreate/MentoringCreate.tsx
+++ frontend/src/pages/mentoringCreate/MentoringCreate.tsx
@@ -1,7 +1,21 @@
-import MentoringCreateHeader from './components/mentoringCreateHeader/MentoringCreateHeader';
+import styled from '@emotion/styled';
+
+import MentoringCreateForm from './components/MentoringCreateForm/MentoringCreateForm';
+import MentoringCreateHeader from './components/MentoringCreateHeader/MentoringCreateHeader';
 
 function MentoringCreate() {
-  return <MentoringCreateHeader />;
+  return (
+    <>
+      <MentoringCreateHeader />
+      <StyledWrapper>
+        <MentoringCreateForm />
+      </StyledWrapper>
+    </>
+  );
 }
 
 export default MentoringCreate;
+
+const StyledWrapper = styled.div`
+  padding: 3.2rem 1.6rem;
+`;
