073a837071952c43c386ed0a59038a7242f96c8b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-31 15:32:05 +0900

    feat: MyPageHeader에 뒤로가기 버튼 및 아이콘 추가 #189

--- frontend/src/pages/myPage/components/MyPageHeader/MyPageHeader.tsx
+++ frontend/src/pages/myPage/components/MyPageHeader/MyPageHeader.tsx
@@ -1,11 +1,16 @@
 import styled from '@emotion/styled';
 
+import backIcon from '../../../../common/assets/images/backIcon.svg';
 import Header from '../../../../common/components/Header/Header';
 
 function MyPageHeader() {
   return (
     <Header>
-      <StyledWrapper></StyledWrapper>
+      <StyledWrapper>
+        <StyledBackButton onClick={() => {}} type="button">
+          <StyledBackIcon src={backIcon} alt="뒤로가기 아이콘" />
+        </StyledBackButton>
+      </StyledWrapper>
     </Header>
   );
 }
@@ -17,4 +22,19 @@ const StyledWrapper = styled.div`
   align-items: center;
 
   height: 100%;
+`;
+
+const StyledBackButton = styled.button`
+  position: absolute;
+
+  margin-left: 1rem;
+  padding: 0;
+  border: none;
+
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const StyledBackIcon = styled.img`
+  width: 3.4rem;
 `;
