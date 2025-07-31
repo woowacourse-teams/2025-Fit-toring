d777560c1c42a65db92d0b53b19eacb7a09b281b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-31 21:48:44 +0900

    feat: 드롭다운 담는 StyledMenuList 추가 #226

--- frontend/src/pages/myPage/components/MenuDropDown/MenuDropDown.tsx
+++ frontend/src/pages/myPage/components/MenuDropDown/MenuDropDown.tsx
@@ -4,15 +4,26 @@ import menuIcon from '../../../../common/assets/images/menuBar.svg';
 
 function MenuDropDown() {
   return (
-    <>
+    <StyledContainer>
       <StyledMenuButton>
         <StyledMenuIcon src={menuIcon} alt="메뉴 열기 아이콘" />
       </StyledMenuButton>
-    </>
+
+      <StyledMenuList opened={true}>
+        <li>개설한 멘토링</li>
+        <li>참여한 멘토링</li>
+        <li>회원 정보</li>
+        <li>로그아웃</li>
+      </StyledMenuList>
+    </StyledContainer>
   );
 }
 
 export default MenuDropDown;
+
+const StyledContainer = styled.div`
+  position: relative;
+`;
 
 const StyledMenuButton = styled.button`
   display: flex;
@@ -43,4 +54,26 @@ const StyledMenuButton = styled.button`
 
 const StyledMenuIcon = styled.img`
   width: 2.4rem;
+`;
+
+const StyledMenuList = styled.ul<{ opened: boolean }>`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  position: absolute;
+  top: 100%;
+  right: 2rem;
+  z-index: 50;
+
+  width: 20rem;
+  margin-top: 0.4rem;
+  border: 1px solid ${({ theme }) => theme.OUTLINE.REGULAR};
+  border-radius: 16px;
+  box-shadow: 0 0.4rem 1.6rem rgb(0 0 0 / 10%);
+
+  background-color: ${({ theme }) => theme.BG.WHITE};
+  opacity: ${({ opened }) => (opened ? 1 : 0)};
+  transform: ${({ opened }) =>
+    opened ? 'translateY(0)' : 'translateY(-10px)'};
+  transition: all 0.2s ease;
 `;
