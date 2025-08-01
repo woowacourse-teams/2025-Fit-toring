bffbaa919f0906d01c4708ecf07d6bc9113a6780
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-01 15:50:48 +0900

    feat: MyProfile 컴포넌트에 내 정보를 담는 StyledWrapper 추가 #190

--- frontend/src/pages/myPage/components/MyProfile/MyProfile.tsx
+++ frontend/src/pages/myPage/components/MyProfile/MyProfile.tsx
@@ -16,13 +16,14 @@ function MyProfile() {
       <StyledIntro>
         멘토링 활동 내역을 확인하고 개인정보를 관리하세요.
       </StyledIntro>
-
-      <div>
-        <img src={img} alt="Profile" />
-        <p>이름: {name}</p>
-        <p>전화번호: {phone}</p>
-        <p>아이디: {id}</p>
-      </div>
+      <StyledWrapper>
+        <div>
+          <img src={img} alt="Profile" />
+          <p>이름: {name}</p>
+          <p>전화번호: {phone}</p>
+          <p>아이디: {id}</p>
+        </div>
+      </StyledWrapper>
     </StyledContainer>
   );
 }
@@ -42,4 +43,18 @@ const StyledContainer = styled.section`
 const StyledIntro = styled.h2`
   color: ${({ theme }) => theme.FONT.B03};
   ${({ theme }) => theme.TYPOGRAPHY.B2_R}
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  height: auto;
+  padding: 2rem;
+  border-radius: 16px;
+  box-shadow: rgb(0 0 0 / 10%) 0 0.4rem 1.2rem;
+
+  background-color: ${({ theme }) => theme.BG.WHITE};
 `;
