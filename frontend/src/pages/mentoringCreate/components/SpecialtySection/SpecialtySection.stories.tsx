a8b7cb15a9349c966ec7668127d2058af7301815
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-08-01 17:26:14 +0900

    test: SpecialtySection 스토리북 추가 #183

---
+++ frontend/src/pages/mentoringCreate/components/SpecialtySection/SpecialtySection.stories.tsx
@@ -0,0 +1,24 @@
+import SpecialtySection from './SpecialtySection';
+
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+const meta = {
+  title: 'mentoringCreate/SpecialtySection',
+  component: SpecialtySection,
+
+  decorators: [(Story) => <Story />],
+} satisfies Meta<typeof SpecialtySection>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DefaultSpecialtySection: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          '멘토링 생성 페이지의 전문 분야 선택 섹션입니다. 최대 3개의 전문 분야를 선택할 수 있습니다.',
+      },
+    },
+  },
+};
