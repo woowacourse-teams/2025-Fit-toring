4934433fc0ea0f9e2a0d08edcb84f10d40ab7ae9
kimdev <kim__dev@hufs.ac.kr>
2025-08-01 15:59:35 +0900

    feat: 리뷰 엔티티 구현 #229

---
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/model/Review.java
@@ -0,0 +1,45 @@
+package fittoring.mentoring.business.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "review")
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private byte rating;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, unique = true)
+    private Mentoring mentoring;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, unique = true)
+    private Member reviewer;
+}
