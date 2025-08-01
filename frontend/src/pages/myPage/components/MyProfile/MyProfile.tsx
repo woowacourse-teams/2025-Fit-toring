cae8fb25dfdd2135f8380f69322db8b876f9ee73
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-01 16:13:47 +0900

    feat: MyProfile 컴포넌트 내 정보에 스타일 적용 #190

--- frontend/src/pages/myPage/components/MyProfile/MyProfile.tsx
+++ frontend/src/pages/myPage/components/MyProfile/MyProfile.tsx
@@ -17,12 +17,10 @@ function MyProfile() {
         멘토링 활동 내역을 확인하고 개인정보를 관리하세요.
       </StyledIntro>
       <StyledWrapper>
-        <div>
-          <img src={img} alt="Profile" />
-          <p>이름: {name}</p>
-          <p>전화번호: {phone}</p>
-          <p>아이디: {id}</p>
-        </div>
+        <StyledImage src={img} alt="Profile" />
+        <StyledName>{name}</StyledName>
+        <StyledId>아이디: {id}</StyledId>
+        <StyledPhone>전화번호: {phone}</StyledPhone>
       </StyledWrapper>
     </StyledContainer>
   );
@@ -49,12 +47,36 @@ const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 0.8rem;
 
   width: 100%;
   height: auto;
+  max-height: 24rem;
   padding: 2rem;
   border-radius: 16px;
   box-shadow: rgb(0 0 0 / 10%) 0 0.4rem 1.2rem;
 
   background-color: ${({ theme }) => theme.BG.WHITE};
+`;
+
+const StyledImage = styled.img`
+  width: 6rem;
+  height: 6rem;
+  border: 1px solid ${({ theme }) => theme.SYSTEM.MAIN300};
+  border-radius: 50%;
+`;
+
+const StyledName = styled.p`
+  color: ${({ theme }) => theme.FONT.B01};
+  ${({ theme }) => theme.TYPOGRAPHY.B1_B}
+`;
+
+const StyledId = styled.p`
+  color: ${({ theme }) => theme.FONT.B01};
+  ${({ theme }) => theme.TYPOGRAPHY.B4_R}
+`;
+
+const StyledPhone = styled.p`
+  color: ${({ theme }) => theme.FONT.B01};
+  ${({ theme }) => theme.TYPOGRAPHY.B4_R}
 `;
