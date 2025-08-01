4bc639c20f0617cf54fcf67b4b72935d29292ed5
kimyou1102 <yoosk5485@naver.com>
2025-08-01 12:54:47 +0900

    feat: 이름 검증 훅 이용해서 구현 #202

--- frontend/src/pages/signup/components/SignupForm/SignupForm.tsx
+++ frontend/src/pages/signup/components/SignupForm/SignupForm.tsx
@@ -2,16 +2,27 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import Button from '../../../../common/components/Button/Button';
+import useNameInput from '../../../../common/hooks/useNameInput';
 import PasswordFields from '../PasswordFields/PasswordFields';
 import PhoneFields from '../PhoneFields/PhoneFields';
 import UserIdField from '../UserIdField/UserIdField';
 import UserInfoFields from '../UserInfoFields/UserInfoFields';
 
 function SignupForm() {
+  const {
+    name,
+    handleNameChange,
+    errorMessage: nameErrorMessage,
+  } = useNameInput();
+
   return (
     <StyledContainer>
       <StyledFormFields>
-        <UserInfoFields />
+        <UserInfoFields
+          name={name}
+          nameErrorMessage={nameErrorMessage}
+          handleNameChange={handleNameChange}
+        />
         <UserIdField />
         <PasswordFields />
         <PhoneFields />
@@ -22,7 +33,6 @@ function SignupForm() {
         customStyle={css`
           height: 4.3rem;
           box-shadow: 0 4px 12px 0 rgb(0 120 111 / 30%);
-
           font-size: 1.6rem;
         `}
       >
