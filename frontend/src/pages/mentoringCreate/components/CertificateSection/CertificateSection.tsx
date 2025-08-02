3a554dfed12ce6998819c8afb8d41c8ea5c97edb
Beomtae <region9@naver.com>
2025-08-02 21:30:24 +0900

    feat: CertificateSection 가이드 글 추가 #185

--- frontend/src/pages/mentoringCreate/components/CertificateSection/CertificateSection.tsx
+++ frontend/src/pages/mentoringCreate/components/CertificateSection/CertificateSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import styled from '@emotion/styled';
 
 import TitleSeparator from '../TitleSeparator/TitleSeparator';
 
@@ -6,8 +6,17 @@ function CertificateSection() {
   return (
     <section>
       <TitleSeparator>검증된 자격 사항</TitleSeparator>
+      <StyledGuideText>최대 3개까지 등록 가능합니다.</StyledGuideText>
     </section>
   );
 }
 
 export default CertificateSection;
+
+const StyledGuideText = styled.p`
+  margin-bottom: 2rem;
+  padding-left: 0.5rem;
+
+  ${({ theme }) => theme.TYPOGRAPHY.B4_R}
+  color: ${({ theme }) => theme.FONT.B04}
+`;
