2e4e714b8f55cd1d2bec8df3b07705dddd362c93
Lee Dong Yeop <s01054830455@gmail.com>
2025-08-02 19:47:06 +0900

    feat: refreshToken 엔티티와 repository 구현

---
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/model/RefreshToken.java
@@ -0,0 +1,39 @@
+package fittoring.mentoring.business.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "refresh_token")
+@Entity
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Getter
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+
+    public RefreshToken(Long memberId, String token, LocalDateTime createAt) {
+        this(null, memberId, token, createAt);
+    }
+}
