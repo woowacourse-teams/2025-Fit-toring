19071479a49f52ffdff974b5c697cb87bcbb5f0f
Beomtae <region9@naver.com>
2025-08-02 22:04:35 +0900

    feat: 자격 항목 추가 버튼 생성 #185

--- frontend/src/pages/mentoringCreate/components/CertificateSection/CertificateSection.tsx
+++ frontend/src/pages/mentoringCreate/components/CertificateSection/CertificateSection.tsx
@@ -16,6 +16,7 @@ function CertificateSection() {
         </p>
         <p>멘토 페이지에는 항목 형식에 따라 순서대로 보여집니다.</p>
       </StyledDescriptionWrapper>
+      <StyledAddButton type="button">+ 자격 항목 추가하기</StyledAddButton>
     </section>
   );
 }
@@ -39,14 +40,32 @@ const StyledDescriptionWrapper = styled.div`
 
   width: 100%;
   height: 100%;
+  margin-bottom: 3.5rem;
   padding: 2rem;
   border: 1px solid ${({ theme }) => theme.OUTLINE.REGULAR};
-  border-radius: 1.2rem;
+  border-radius: 12px;
 
   background-color: ${({ theme }) => theme.BG.LIGHT};
 
   & > p {
     ${({ theme }) => theme.TYPOGRAPHY.B2_R}
     color: ${({ theme }) => theme.FONT.B04};
+  }
+`;
+
+const StyledAddButton = styled.button`
+  width: 100%;
+  height: 6.8rem;
+  border: 1px dashed ${({ theme }) => theme.SYSTEM.MAIN600};
+  ${({ theme }) => theme.TYPOGRAPHY.BTN1_R}
+  border-radius: 12px;
+
+  background-color: ${({ theme }) => theme.BG.WHITE};
+
+  color: ${({ theme }) => theme.SYSTEM.MAIN700};
+  cursor: pointer;
+
+  &:active {
+    transform: scale(0.98);
   }
 `;
