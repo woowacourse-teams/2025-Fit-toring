d20a2fbb3099cd62019315fec1ec3745e65af4c0
Beomtae <region9@naver.com>
2025-08-03 00:19:59 +0900

    refactor: 라벨 타입 정의 #183

--- frontend/src/pages/mentoringCreate/components/SpecialtyTag/SpecialtyTag.tsx
+++ frontend/src/pages/mentoringCreate/components/SpecialtyTag/SpecialtyTag.tsx
@@ -6,6 +6,9 @@ interface SpecialtyTagProps {
   checked: boolean;
   onChange: () => void;
 }
+
+type LabelType = Pick<SpecialtyTagProps, 'disabled' | 'checked'>;
+
 function SpecialtyTag({
   title,
   disabled,
@@ -26,7 +29,7 @@ function SpecialtyTag({
 
 export default SpecialtyTag;
 
-const StyledContainer = styled.label<{ checked: boolean; disabled: boolean }>`
+const StyledContainer = styled.label<LabelType>`
   display: flex;
   align-items: center;
   justify-content: center;
