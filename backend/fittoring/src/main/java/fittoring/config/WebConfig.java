ac99f178145fffd8c345ac2c2aa0a69994a77a2d
Lee Dong Yeop <s01054830455@gmail.com>
2025-08-03 17:04:10 +0900

    feat: 인터셉터 검증에서 엔드포인트 설정
    - 검증에서 제외될 엔드포인트는 excludePathPatterns 메서드에 명시하였음.

--- backend/fittoring/src/main/java/fittoring/config/WebConfig.java
+++ backend/fittoring/src/main/java/fittoring/config/WebConfig.java
@@ -28,6 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authenticationInterceptor);
+        registry.addInterceptor(authenticationInterceptor)
+                .excludePathPatterns("/**");
     }
 }
