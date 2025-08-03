25b8ce85ff81dc822ff2e0ea550050ca9936fb51
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-03 21:26:25 +0900

    feat: CreatedMentoring 컴포넌트의 정보 래퍼 추가 및 스타일 수정 #192

--- frontend/src/pages/myPage/components/CreatedMentoring/CreatedMentoring.tsx
+++ frontend/src/pages/myPage/components/CreatedMentoring/CreatedMentoring.tsx
@@ -45,12 +45,15 @@ function CreatedMentoring() {
     <StyledContainer>
       <StyledTitle>개설한 멘토링</StyledTitle>
       <StyledWrapper>
-        <StyledSubTitle>
-          멘토링 신청 목록 ({MENTORING_APPLICATIONS.length}건)
-        </StyledSubTitle>
-        <StyledDescription>
-          사용자들이 신청한 멘토링을 승인하거나 거절할 수 있습니다.
-        </StyledDescription>
+        <StyledInfoWrapper>
+          <StyledSubTitle>
+            멘토링 신청 목록 ({MENTORING_APPLICATIONS.length}건)
+          </StyledSubTitle>
+          <StyledDescription>
+            사용자들이 신청한 멘토링을 승인하거나 거절할 수 있습니다.
+          </StyledDescription>
+        </StyledInfoWrapper>
+        <StyledLine />
       </StyledWrapper>
     </StyledContainer>
   );
@@ -76,14 +79,20 @@ const StyledTitle = styled.h2`
 const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1rem;
 
   width: 100%;
   height: 100%;
-  padding: 2.5rem 2rem;
   border: 1px solid ${({ theme }) => theme.OUTLINE.REGULAR};
   border-radius: 16px;
   box-shadow: 0 0.4rem 1.6rem rgb(0 0 0 / 10%);
+`;
+
+const StyledInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  padding: 2.5rem 2rem;
 `;
 
 const StyledSubTitle = styled.h3`
@@ -94,4 +103,12 @@ const StyledSubTitle = styled.h3`
 const StyledDescription = styled.p`
   color: ${({ theme }) => theme.FONT.B04};
   ${({ theme }) => theme.TYPOGRAPHY.B1_R}
+`;
+
+const StyledLine = styled.hr`
+  width: 100%;
+  height: 1px;
+  margin: 0;
+  border: none;
+  border-top: 1px solid ${({ theme }) => theme.OUTLINE.REGULAR};
 `;
