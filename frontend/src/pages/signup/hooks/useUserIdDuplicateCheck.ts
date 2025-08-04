081df23b8e0ad2d7aab4d3a32db352e50ad7f0f0
kimyou1102 <yoosk5485@naver.com>
2025-08-04 17:01:52 +0900

    feat: useUserIdDuplicateCheck 훅 생성 #204
    - 중복확인 관련 로직을 훅에서만 책임지게 하기 위해 훅으로 분리

---
+++ frontend/src/pages/signup/hooks/useUserIdDuplicateCheck.ts
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+import { posValidateId } from '../apis/postValidateId';
+
+import { useConfirmState } from './useConfirmStatus';
+
+interface useDuplicateCheckParams {
+  userId: string;
+  userIdErrorMessage: string;
+}
+
+const useUserIdDuplicateCheck = ({
+  userId,
+  userIdErrorMessage,
+}: useDuplicateCheckParams) => {
+  const [duplicateError, setDuplicateError] = useState(false);
+
+  const {
+    confirm: userIdConfirm,
+    isCheckNeeded: isUserIdCheck,
+    shouldBlockSubmit: shouldBlockSubmitByUserId,
+  } = useConfirmState(userId);
+
+  const handleDuplicateConfrimClick = async () => {
+    setDuplicateError(false);
+
+    try {
+      const response = await posValidateId(userId);
+
+      if (response.status === 200) {
+        userIdConfirm();
+        alert('사용 가능한 아이디입니다.');
+      }
+    } catch (error) {
+      console.error('아이디 중복 확인 에러:', error);
+      setDuplicateError(true);
+    }
+  };
+
+  const getFinalUserIdErrorMessage = () => {
+    if (duplicateError) {
+      return '이미 사용중인 아이디입니다.';
+    }
+
+    if (!isUserIdCheck) {
+      return '중복확인을 해주세요';
+    }
+
+    return userIdErrorMessage;
+  };
+
+  return {
+    duplicateError,
+    handleDuplicateConfrimClick,
+    shouldBlockSubmitByUserId,
+    getFinalUserIdErrorMessage,
+  };
+};
+
+export default useUserIdDuplicateCheck;
