79f7617689c8d865a01c8541ba1bd2367e5024fb
kimyou1102 <yoosk5485@naver.com>
2025-08-04 21:25:30 +0900

    refactor: postSignup 매개변수 타입 파일로 분리 #204

--- frontend/src/pages/signup/apis/postSignup.ts
+++ frontend/src/pages/signup/apis/postSignup.ts
@@ -1,15 +1,9 @@
 import { apiClient } from '../../../common/apis/apiClient';
 import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
 
-interface SignupInfoParams {
-  loginId: string;
-  name: string;
-  gender: '남' | '여';
-  phone: string;
-  password: string;
-}
+import type { SignupInfo } from '../types/signupInfo';
 
-export const postSignup = async (signupInfo: SignupInfoParams) => {
+export const postSignup = async (signupInfo: SignupInfo) => {
   return await apiClient.post({
     endpoint: API_ENDPOINTS.SIGNUP,
     searchParams: { ...signupInfo },
---
+++ frontend/src/pages/signup/types/signupInfo.ts
@@ -0,0 +1,7 @@
+export interface SignupInfo {
+  loginId: string;
+  name: string;
+  gender: '남' | '여';
+  phone: string;
+  password: string;
+}
