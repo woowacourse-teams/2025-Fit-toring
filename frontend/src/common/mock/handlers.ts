af2870fa3d001de03d48ee642d1ae89d7762f8f5
kimyou1102 <yoosk5485@naver.com>
2025-08-04 10:01:07 +0900

    feat: validateId handler 작성 및 추가 #204

--- frontend/src/common/mock/handlers.ts
+++ frontend/src/common/mock/handlers.ts
@@ -1,3 +1,4 @@
 import { mentoringHandler } from './mentoring/handlers';
+import { validateIdHandler } from './validateId/validateId';
 
-export const handlers = [...mentoringHandler];
+export const handlers = [...mentoringHandler, ...validateIdHandler];
---
+++ frontend/src/common/mock/validateId/validateId.ts
@@ -0,0 +1,36 @@
+import { http, HttpResponse } from 'msw';
+
+import { API_ENDPOINTS } from '../../constants/apiEndpoints';
+
+interface AuthCodeBody {
+  loginId: string;
+}
+
+const BASE_URL = process.env.BASE_URL;
+const AUTH_CODE_URL = `${BASE_URL}${API_ENDPOINTS.VALIDATE_ID}`;
+
+const DUMMY_DUPLICATE_USER_ID = 'test1234';
+
+const postValidateId = http.post(AUTH_CODE_URL, async ({ request }) => {
+  const body = await request.json();
+
+  if (!body) {
+    return;
+  }
+
+  const { loginId } = body as AuthCodeBody;
+
+  if (loginId === DUMMY_DUPLICATE_USER_ID) {
+    return HttpResponse.json(
+      { message: '중복된 아이디입니다.' },
+      { status: 400 },
+    );
+  }
+
+  return HttpResponse.json(
+    { message: '사용 가능한 아이디입니다.' },
+    { status: 200 },
+  );
+});
+
+export const validateIdHandler = [postValidateId];
