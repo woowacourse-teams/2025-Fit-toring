fc0448921005064f4bbe84ccb9a1447476342a62
kimyou1102 <yoosk5485@naver.com>
2025-08-04 21:23:41 +0900

    feat: useVerificationCodeConfirm 훅 생성 #204
    - 인증번호 확인 관련 로직을 훅에서만 책임지게 하기 위해 훅으로 분리

---
+++ frontend/src/pages/signup/hooks/useVerificationCodeConfirm.ts
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+
+import { postAuthCodeVerify } from '../apis/postAuthCodeVerify';
+
+import { useConfirmState } from './useConfirmStatus';
+
+interface useVerificationCodeConfirmParams {
+  verificationCode: string;
+  verificationCodeErrorMessage: string;
+}
+
+const useVerificationCodeConfirm = ({
+  verificationCode,
+  verificationCodeErrorMessage,
+}: useVerificationCodeConfirmParams) => {
+  const {
+    confirm: verificationCodeConfirm,
+    isCheckNeeded: isVerificationCodeCheck,
+    shouldBlockSubmit: shouldBlockSubmitByVerificationCode,
+  } = useConfirmState(verificationCode);
+
+  const [verificationCodeError, setVerificationCodeError] = useState(false);
+
+  const handleAuthCodeVerifyClick = async (phoneNumber: string) => {
+    setVerificationCodeError(false);
+    try {
+      const response = await postAuthCodeVerify(phoneNumber, verificationCode);
+      if (response.status === 200) {
+        alert('인증 성공');
+        verificationCodeConfirm();
+      }
+    } catch (error) {
+      setVerificationCodeError(true);
+      console.error('인증 실패', error);
+    }
+  };
+
+  const getFinalVerificationCodeErrorMessage = () => {
+    if (!isVerificationCodeCheck) {
+      return '인증을 해주세요';
+    }
+    if (verificationCodeError) {
+      return '인증 실패';
+    }
+
+    return verificationCodeErrorMessage;
+  };
+
+  return {
+    verificationCodeError,
+    handleAuthCodeVerifyClick,
+    getFinalVerificationCodeErrorMessage,
+    shouldBlockSubmitByVerificationCode,
+  };
+};
+
+export default useVerificationCodeConfirm;
