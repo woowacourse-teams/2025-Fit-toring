6e79cc7842bb2080d485266d557b4005cacf5452
kimyou1102 <yoosk5485@naver.com>
2025-08-04 11:15:00 +0900

    feat: postAuthCode 함수 구현 #204

---
+++ frontend/src/pages/signup/apis/postAuthCode.ts
@@ -0,0 +1,9 @@
+import { apiClient } from '../../../common/apis/apiClient';
+import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
+
+export const postAuthCode = async (phone: string) => {
+  return await apiClient.post({
+    endpoint: API_ENDPOINTS.AUTH_CODE,
+    searchParams: { phone },
+  });
+};
