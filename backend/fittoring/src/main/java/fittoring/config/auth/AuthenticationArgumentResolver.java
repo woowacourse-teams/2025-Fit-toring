b3e7d2e94d24fda49f46f3acb55e331c5361d12d
Lee Dong Yeop <s01054830455@gmail.com>
2025-08-04 19:46:13 +0900

    feat: 매개변수의 어노테이션과 타입을 검증하고, LoginMember를 생성하여 반환하는 메서드 구현

--- backend/fittoring/src/main/java/fittoring/config/auth/AuthenticationArgumentResolver.java
+++ backend/fittoring/src/main/java/fittoring/config/auth/AuthenticationArgumentResolver.java
@@ -1,5 +1,6 @@
 package fittoring.config.auth;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -12,12 +13,16 @@ public class AuthenticationArgumentResolver implements HandlerMethodArgumentReso
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return false;
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasLoginMemberType = parameter.getParameterType().equals(LoginMember.class);
+        return hasLoginAnnotation && hasLoginMemberType;
     }
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        return null;
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        Object memberId = request.getAttribute("memberId");
+        return new LoginMember((Long) memberId);
     }
 }
