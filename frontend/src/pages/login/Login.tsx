b4fcdc29d288301333c18514d7a669942bb3847f
kimyou1102 <yoosk5485@naver.com>
2025-08-05 03:50:58 +0900

    feat: 로그인 페이지에 LoginIntro 컴포넌트 적용 #275

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import LoginHeader from './components/LoginHeader/LoginHeader';
+import LoginIntro from './components/LoginIntro/LoginIntro';
 
 function Login() {
   return (
     <>
       <LoginHeader />
+      <LoginIntro />
     </>
   );
 }
