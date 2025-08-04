1b91e5ce31cbadfcc5694f4b4e10e255b60aaabe
Lee Dong Yeop <s01054830455@gmail.com>
2025-08-04 19:09:43 +0900

    feat: WebConfig 설정 클래스에 AuthenticationArgumentResolver 추가

--- backend/fittoring/src/main/java/fittoring/config/auth/AuthenticationArgumentResolver.java
+++ backend/fittoring/src/main/java/fittoring/config/auth/AuthenticationArgumentResolver.java
@@ -1,11 +1,13 @@
 package fittoring.config.auth;
 
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Component
 public class AuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
