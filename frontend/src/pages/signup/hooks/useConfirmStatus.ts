f67d21c372a5b5b186422d07a060b9fe9172b801
kimyou1102 <yoosk5485@naver.com>
2025-08-04 16:16:11 +0900

    feat: useConfirmState 훅 생성 #204
    - 입력값과 마지막 확인값을 비교하여 중복 확인 여부를 관리하는 로직 분리
    - 아이디/인증코드 등 중복 확인이 필요한 필드에서 재사용 가능하도록 구현

---
+++ frontend/src/pages/signup/hooks/useConfirmStatus.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export const useConfirmState = <T>(value: T) => {
+  const [lastConfirmedValue, setLastConfirmedValue] = useState<T | null>(null);
+  const [isCheckNeeded, setIsCheckNeeded] = useState(true);
+
+  const confirm = () => {
+    setLastConfirmedValue(value);
+    setIsCheckNeeded(true);
+  };
+
+  const shouldBlockSubmit = () => {
+    if (!isMatchedValue) {
+      setIsCheckNeeded(false);
+      return true;
+    }
+    return false;
+  };
+
+  const isMatchedValue = value === lastConfirmedValue;
+
+  return {
+    confirm,
+    isCheckNeeded,
+    shouldBlockSubmit,
+  };
+};
