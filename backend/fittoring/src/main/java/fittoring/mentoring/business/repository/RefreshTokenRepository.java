9d9aae0aa80c9f12ed73eb948ce1951ed5575c8d
Lee Dong Yeop <s01054830455@gmail.com>
2025-08-04 17:09:54 +0900

    refactor: RefreshTokenRepository 상속 객체 변경 (JpaRepository -> ListCrudRepository)
    - 아직까지 JpaRepository의 기능을 사용하지 않음.
    - ListCrudRepository로 충분히 가능하기에 변경

--- backend/fittoring/src/main/java/fittoring/mentoring/business/repository/RefreshTokenRepository.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/repository/RefreshTokenRepository.java
@@ -2,11 +2,11 @@ package fittoring.mentoring.business.repository;
 
 import fittoring.mentoring.business.model.RefreshToken;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends ListCrudRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByTokenValue(String token);
 }
