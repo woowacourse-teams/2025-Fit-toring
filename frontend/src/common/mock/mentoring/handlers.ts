0fb6f60fe987803a75338619fcd9798ed7262473
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-04 20:58:09 +0900

    feat: 전문 분야 필터링하는 msw 핸들러 추가 #271

--- frontend/src/common/mock/mentoring/handlers.ts
+++ frontend/src/common/mock/mentoring/handlers.ts
@@ -13,17 +13,45 @@ export const testStateStore = {
 
 const BASE_URL = process.env.BASE_URL;
 const MENTORING_URL = `${BASE_URL}/mentorings`;
-const getMentoringItems = http.get(MENTORING_URL, () => {
-  const response = [...MENTORINGS];
+const getMentoringItems = http.get(MENTORING_URL, ({ request }) => {
+  const url = new URL(request.url);
+  const { searchParams } = url;
 
-  if (testStateStore.shouldFailCart) {
-    return new HttpResponse(null, {
-      status: 500,
-      statusText: 'mentorings fetch Failed',
-    });
+  const categoryTitle1 = searchParams.get('categoryTitle1');
+  const categoryTitle2 = searchParams.get('categoryTitle2');
+  const categoryTitle3 = searchParams.get('categoryTitle3');
+
+  const categoryValues = [
+    categoryTitle1,
+    categoryTitle2,
+    categoryTitle3,
+  ].filter((category) => category !== null) as string[];
+
+  if (categoryValues.length > 0) {
+    const response = MENTORINGS.filter((mentor) =>
+      categoryValues.every((category) => mentor.categories.includes(category)),
+    );
+
+    if (testStateStore.shouldFailCart) {
+      return new HttpResponse(null, {
+        status: 500,
+        statusText: 'filtered mentorings fetch Failed',
+      });
+    }
+
+    return HttpResponse.json(response);
+  } else {
+    const response = [...MENTORINGS];
+
+    if (testStateStore.shouldFailCart) {
+      return new HttpResponse(null, {
+        status: 500,
+        statusText: 'mentorings fetch Failed',
+      });
+    }
+
+    return HttpResponse.json(response);
   }
-
-  return HttpResponse.json(response);
 });
 
 export const mentoringHandler = [getMentoringItems];
