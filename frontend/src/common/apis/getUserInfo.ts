747654a0031a0f653cfc72094e9f645d6681a12e
kimyou1102 <yoosk5485@naver.com>
2025-08-05 21:39:31 +0900

    refactor: 타입 import시 type 붙여서 수정 #280

--- frontend/src/common/apis/getUserInfo.ts
+++ frontend/src/common/apis/getUserInfo.ts
@@ -2,7 +2,7 @@ import { API_ENDPOINTS } from '../constants/apiEndpoints';
 
 import { apiClient } from './apiClient';
 
-import { UserInfo } from '../types/userInfo';
+import type { UserInfo } from '../types/userInfo';
 
 export const getUserInfo = async () => {
   return await apiClient.get<UserInfo>({
