061b42512f3bbb33474ae1f9c7d2c10367a5a015
kimyou1102 <yoosk5485@naver.com>
2025-08-05 22:23:30 +0900

    fix: 올바르지 않은 인증번호 에러메시지 수정 #204
    - 인증번호 에러메시지가 아닌 비밀번호 에러메시지를 사용중이었음
    - 인증번호 에러메시지로 수정

--- frontend/src/pages/signup/hooks/useVerificationCodeInput.ts
+++ frontend/src/pages/signup/hooks/useVerificationCodeInput.ts
@@ -23,7 +23,7 @@ const useVerificationCodeInput = () => {
     }
 
     if (verificationCode.length < VERIFICATION_CODE_LENGTH) {
-      return ERROR_MESSAGE.INVALID_PASSWORD_LENGTH;
+      return ERROR_MESSAGE.INVALID_VERIFICATION_CODE;
     }
 
     return '';
