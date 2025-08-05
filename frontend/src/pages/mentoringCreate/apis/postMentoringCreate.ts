4654804849e69f7d728425bbb6dc64b19921862e
Beomtae <region9@naver.com>
2025-08-05 15:25:30 +0900

    feat: FormData post 요청 구현 #263

---
+++ frontend/src/pages/mentoringCreate/apis/postMentoringCreate.ts
@@ -0,0 +1,24 @@
+import { apiClient } from '../../../common/apis/apiClient';
+import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
+
+import type { mentoringCreateFormData } from '../components/types/mentoringCreateFormData';
+
+export const postMentoringCreate = async (
+  mentoringData: mentoringCreateFormData,
+  profileImageFile: File | null,
+  certificateImageFiles: File[],
+) => {
+  const formData = new FormData();
+  formData.append('data', JSON.stringify(mentoringData));
+  if (profileImageFile) {
+    formData.append('image', profileImageFile);
+  }
+  certificateImageFiles.forEach((file) =>
+    formData.append('certificateImages', file),
+  );
+
+  return await apiClient.post({
+    endpoint: API_ENDPOINTS.MENTORINGS,
+    searchParams: formData,
+  });
+};
