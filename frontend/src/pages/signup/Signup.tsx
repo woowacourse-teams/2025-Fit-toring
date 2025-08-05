209ed809490220c98089137b37c1d7447fae9d6e
kimyou1102 <yoosk5485@naver.com>
2025-08-05 14:48:27 +0900

    style: 회원가입 페이지 컴포넌트 최상단 요소에 스타일 추가 #234
    - padding-bottom과 배경색 추가

--- frontend/src/pages/signup/Signup.tsx
+++ frontend/src/pages/signup/Signup.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import AuthFooter from './components/AuthFooter/AuthFooter';
 import SignupForm from './components/SignupForm/SignupForm';
 import SignupHeader from './components/SignupHeader/SignupHeader';
@@ -5,13 +6,17 @@ import SignupIntro from './components/SignupIntro/SignupIntro';
 
 function Signup() {
   return (
-    <div>
+    <StyledContainer>
       <SignupHeader />
       <SignupIntro />
       <SignupForm />
       <AuthFooter currentPage="signup" />
-    </div>
+    </StyledContainer>
   );
 }
 
+const StyledContainer = styled.div`
+  background-color: ${({ theme }) => theme.BG.WHITE};
+  padding-bottom: 3rem;
+`;
 export default Signup;
