45383b751765792e72c4e052bd4ddde7b69acb18
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-06 22:17:37 +0900

    feat: 참여한 멘토링 페이지 url 경로 추가 및 라우터 설정 #193

--- frontend/src/common/constants/url.ts
+++ frontend/src/common/constants/url.ts
@@ -5,6 +5,7 @@ export const PAGE_URL = {
   MENTORING_CREATE: '/mentoring-create',
   MY_PAGE: '/my-page',
   CREATED_MENTORING: '/my-page/created-mentoring',
+  PARTICIPATED_MENTORING: '/my-page/participated-mentoring',
   SIGNUP: '/signup',
   LOGIN: '/login',
 } as const;
