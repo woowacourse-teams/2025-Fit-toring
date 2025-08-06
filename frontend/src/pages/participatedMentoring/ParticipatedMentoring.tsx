f34f562555faf855a7367456e800f0ca29a3bf00
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-06 22:36:55 +0900

    feat: 참여한 멘토링 목록 섹션 추가 및 설명 텍스트 추가 #193

--- frontend/src/pages/participatedMentoring/ParticipatedMentoring.tsx
+++ frontend/src/pages/participatedMentoring/ParticipatedMentoring.tsx
@@ -27,6 +27,18 @@ function ParticipatedMentoring() {
   return (
     <StyledContainer>
       <StyledTitle>참여한 멘토링</StyledTitle>
+      <StyledWrapper>
+        <StyledInfoWrapper>
+          <StyledSubTitle>
+            참여한 멘토링 목록 ({PARTICIPATED_MENTORING_LIST.length}건)
+          </StyledSubTitle>
+          <StyledDescription>
+            내가 신청한 멘토링 목록을 확인하고 완료된 멘토링에 대해 리뷰를
+            작성할 수 있습니다.
+          </StyledDescription>
+        </StyledInfoWrapper>
+        <StyledLine />
+      </StyledWrapper>
     </StyledContainer>
   );
 }
@@ -46,4 +58,45 @@ const StyledContainer = styled.section`
 const StyledTitle = styled.h2`
   color: ${({ theme }) => theme.FONT.B01};
   ${({ theme }) => theme.TYPOGRAPHY.LB3_R}
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100%;
+  border: 1px solid ${({ theme }) => theme.OUTLINE.REGULAR};
+  border-radius: 16px;
+  box-shadow: 0 0.4rem 1.6rem rgb(0 0 0 / 10%);
+
+  background-color: ${({ theme }) => theme.BG.WHITE};
+`;
+
+const StyledInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  padding: 2.5rem 2rem;
+`;
+
+const StyledSubTitle = styled.h3`
+  color: ${({ theme }) => theme.FONT.B01};
+  ${({ theme }) => theme.TYPOGRAPHY.LB4_R}
+`;
+
+const StyledDescription = styled.p`
+  word-break: keep-all;
+
+  color: ${({ theme }) => theme.FONT.B04};
+  ${({ theme }) => theme.TYPOGRAPHY.B1_R}
+`;
+
+const StyledLine = styled.hr`
+  width: 100%;
+  height: 1px;
+  margin: 0;
+  border: none;
+  border-top: 1px solid ${({ theme }) => theme.OUTLINE.REGULAR};
 `;
