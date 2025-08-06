3006dfc92715ceecaba8ba00fda3c997b796df2e
Beomtae <region9@naver.com>
2025-08-06 21:44:03 +0900

    refactor: memberSummary 타입 추가 #263

--- frontend/src/common/mock/members/data.ts
+++ frontend/src/common/mock/members/data.ts
@@ -1,4 +1,9 @@
-export const MEMBER_SUMMARY = {
+interface memberSummary {
+  name: string;
+  phoneNumber: string;
+}
+
+export const MEMBER_SUMMARY: memberSummary = {
   name: '홍길동',
   phoneNumber: '010-1234-9048',
 } as const;
