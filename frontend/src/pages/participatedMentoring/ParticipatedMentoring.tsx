f1860f8f4147766a7f4667f9e4c8ceae995d9e19
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-07 02:35:45 +0900

    test: 참여한 멘토링 더미 데이터 추가 및 MentoringItem 스토리 작성 #193

--- frontend/src/pages/participatedMentoring/ParticipatedMentoring.tsx
+++ frontend/src/pages/participatedMentoring/ParticipatedMentoring.tsx
@@ -5,7 +5,7 @@ import MentoringList from './MentoringList/MentoringList';
 
 import type { ParticipatedMentoringType } from './types/participatedMentoring';
 
-const PARTICIPATED_MENTORING_LIST: ParticipatedMentoringType[] = [
+export const PARTICIPATED_MENTORING_LIST: ParticipatedMentoringType[] = [
   {
     mentorName: '김트레이너',
     mentorProfileImage:
@@ -25,6 +25,16 @@ const PARTICIPATED_MENTORING_LIST: ParticipatedMentoringType[] = [
     categories: ['요가', '필라테스'],
     isReviewed: false,
     status: '승인됨',
+  },
+  {
+    mentorName: '이헬퍼',
+    mentorProfileImage:
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.crowdpic.net%2Fphoto%2F%25ED%2592%258D%25EA%25B2%25BD-%25EC%259E%2590%25EC%2597%25B0-%25EB%2593%25A4%25ED%258C%2590-%25EC%25B4%2588%25EC%259B%2590-%25EB%2582%2598%25EB%25AC%25B4-136857%3Fsrsltid%3DAfmBOopK3sXndcC9IZQXkHBG_NVOu_ZELeaPahtqFY0gMkGhB9zkxqBr&psig=AOvVaw2i3WaVGdyiSKf3VAcV-BK2&ust=1754548076306000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCODkj4rH9Y4DFQAAAAAdAAAAABAE',
+    price: '3000',
+    reservedAt: '2024-01-15',
+    categories: ['재활 운동', '스트레칭'],
+    isReviewed: false,
+    status: '승인대기',
   },
 ];
 
