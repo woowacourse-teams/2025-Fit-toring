d1daeace90cab86b301d5834551ad1b675c7a85e
kimyou1102 <yoosk5485@naver.com>
2025-08-06 14:13:54 +0900

    feat: postLogin api 함수 구현 #294

---
+++ frontend/src/pages/login/apis/postLogin.ts
@@ -0,0 +1,9 @@
+import { apiClient } from '../../../common/apis/apiClient';
+import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
+
+export const postLogin = async (loginId: string, password: string) => {
+  return await apiClient.post({
+    endpoint: API_ENDPOINTS.LOGIN,
+    body: { loginId, password },
+  });
+};
