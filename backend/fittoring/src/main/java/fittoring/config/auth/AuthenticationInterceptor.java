2cad310c340ec038e1bb7e81e17e57c516ce0171
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-08-06 13:26:19 +0900

    feat: 멘토링 조회 요청에 대한 쿠키 검증 제외
    - Post 요청 시만 쿠키를 검증하도록 수정

--- backend/fittoring/src/main/java/fittoring/config/auth/AuthenticationInterceptor.java
+++ backend/fittoring/src/main/java/fittoring/config/auth/AuthenticationInterceptor.java
@@ -21,12 +21,15 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+        if (!request.getMethod().equalsIgnoreCase("POST")) {
+            return true;
+        }
+
         Cookie[] cookies = request.getCookies();
         if (cookies == null || cookies.length == 0) {
             responseUnauthorized(response, BusinessErrorMessage.EMPTY_COOKIE.getMessage());
             return false;
         }
-
         try {
             String accessToken = jwtExtractor.extractTokenFromCookie("accessToken", cookies);
             jwtProvider.validateToken(accessToken);
@@ -37,7 +40,6 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
             responseUnauthorized(response, BusinessErrorMessage.INVALID_TOKEN.getMessage());
             return false;
         }
-
         return true;
     }
 
