a0bc92f97955e7065d5a642d60e35d06b6d720d0
kimyou1102 <yoosk5485@naver.com>
2025-08-08 01:08:12 +0900

    feat: 로그인 성공시 메인페이지로 이동 기능 구현 #301

--- frontend/src/pages/login/components/LoginForm/LoginForm.tsx
+++ frontend/src/pages/login/components/LoginForm/LoginForm.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
 
 import blind from '../../../../common/assets/images/blind.svg';
 import notBlind from '../../../../common/assets/images/notBlind.svg';
@@ -18,11 +19,14 @@ function LoginForm() {
   const { userId, handleUserIdChange } = useUserIdInput();
   const { password, handlePasswordChange } = usePasswordInput();
 
+  const navigate = useNavigate();
+
   const fetchLogin = async () => {
     try {
       const response = await postLogin(userId, password);
       if (response.status === 200) {
         alert('로그인에 성공했습니다.');
+        navigate('/');
       }
     } catch (error) {
       console.error('로그인 실패', error);
