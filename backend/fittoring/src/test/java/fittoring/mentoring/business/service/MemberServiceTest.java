2a08676094fdbb739a1a2b847e097ca8228e18cd
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-08-07 13:20:59 +0900

    feat: 내 요약 정보 조회 API 구현

--- backend/fittoring/src/test/java/fittoring/mentoring/business/service/MemberServiceTest.java
+++ backend/fittoring/src/test/java/fittoring/mentoring/business/service/MemberServiceTest.java
@@ -9,6 +9,7 @@ import fittoring.mentoring.business.model.Phone;
 import fittoring.mentoring.business.model.password.Password;
 import fittoring.mentoring.infra.S3Uploader;
 import fittoring.mentoring.presentation.dto.MyInfoResponse;
+import fittoring.mentoring.presentation.dto.MyInfoSummaryResponse;
 import fittoring.util.DbCleaner;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -152,6 +153,33 @@ class MemberServiceTest {
             softAssertions.assertThat(memberInfo.loginId()).isEqualTo(loginId);
             softAssertions.assertThat(memberInfo.name()).isEqualTo(name);
             softAssertions.assertThat(memberInfo.gender()).isEqualTo(gender);
+            softAssertions.assertThat(memberInfo.phoneNumber()).isEqualTo(phone.getNumber());
+        });
+    }
+
+    @DisplayName("멘티는 로그인 상태에서 내 요약 정보를 조회할 수 있다..")
+    @Test
+    void getMyInfoSummary() {
+        // given
+        String loginId = "loginId";
+        String name = "사용자";
+        String gender = "MALE";
+        Phone phone = new Phone("010-1234-5678");
+        Member member = new Member(
+                loginId,
+                gender,
+                name,
+                phone,
+                Password.from("password")
+        );
+        Member savedMember = em.persist(member);
+
+        // when
+        MyInfoSummaryResponse memberInfo = memberService.getMemberInfoSummary(savedMember.getId());
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(memberInfo.name()).isEqualTo(name);
             softAssertions.assertThat(memberInfo.phoneNumber()).isEqualTo(phone.getNumber());
         });
     }
