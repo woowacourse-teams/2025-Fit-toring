84397bff1ef33a21244efc61e8cb2f5a42ba3df2
Lee Dong Yeop <s01054830455@gmail.com>
2025-08-07 13:08:17 +0900

    feat: WebConfig 인터셉터 경로 추가(/reservations/**)

--- backend/fittoring/src/main/java/fittoring/config/WebConfig.java
+++ backend/fittoring/src/main/java/fittoring/config/WebConfig.java
@@ -33,7 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
-                .addPathPatterns("/mentorings/**");
+                .addPathPatterns("/mentorings/**", "/reservations/**");
     }
 
     @Override
