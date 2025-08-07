baf941f44de29ffba13ae8c49d041b72fc9c22bf
kimyou1102 <yoosk5485@naver.com>
2025-08-08 02:33:32 +0900

    feat: AuthProvider 적용 #301

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,29 +4,23 @@ import { ThemeProvider, Global } from '@emotion/react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import AuthProvider from './common/components/AuthProvider/AuthProvider';
 import { fonts } from './common/styles/fonts';
 import { resetCss } from './common/styles/reset';
 import { THEME } from './common/styles/theme';
 
 async function enableMocking() {
   return; // 사용 시 return 제거
-
-  const { worker } = await import('./common/mock/browser');
-
-  const isLocalHost = process.env.NODE_ENV === 'development';
-  if (!isLocalHost) {
-    return;
-  }
-
-  return worker.start();
 }
 
 enableMocking().then(() => {
   createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <ThemeProvider theme={THEME}>
-        <Global styles={[resetCss, fonts]} />
-        <App />
+        <AuthProvider>
+          <Global styles={[resetCss, fonts]} />
+          <App />
+        </AuthProvider>
       </ThemeProvider>
     </React.StrictMode>,
   );
