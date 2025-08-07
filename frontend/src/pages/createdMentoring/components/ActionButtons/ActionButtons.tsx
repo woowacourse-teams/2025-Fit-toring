cfb8b0470b64614a7da872fd4c2d977364faac70
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-07 20:53:15 +0900

    feat: 예약 상태 업데이트를 위한 클릭 핸들러 및 전화번호 전달 기능 추가 #284

--- frontend/src/pages/createdMentoring/components/ActionButtons/ActionButtons.tsx
+++ frontend/src/pages/createdMentoring/components/ActionButtons/ActionButtons.tsx
@@ -4,16 +4,56 @@ import {
   StatusTypeEnum,
   type StatusType,
 } from '../../../../common/types/statusType';
+import { patchReservationStatus } from '../../apis/patchReservationStatus';
+import { MENTORING_APPLICATION_STATUS_ENUM } from '../../types/mentoringApplicationStatus';
+
+import type { MENTORING_APPLICATION_STATUS } from '../../types/mentoringApplicationStatus';
 
 interface ActionButtonsProps {
+  reservationId: number;
   status: StatusType;
+  onClick: (status: StatusType, phoneNumber: string) => void;
 }
 
-function ActionButtons({ status }: ActionButtonsProps) {
+function ActionButtons({ reservationId, status, onClick }: ActionButtonsProps) {
+  const handleActionButtonClick = async (
+    newStatus: MENTORING_APPLICATION_STATUS,
+  ) => {
+    try {
+      const response = await patchReservationStatus(reservationId, {
+        status: newStatus,
+      });
+
+      if (response.status === 200) {
+        const updatedStatus =
+          newStatus === MENTORING_APPLICATION_STATUS_ENUM.APPROVE
+            ? StatusTypeEnum.approved
+            : StatusTypeEnum.rejected;
+
+        onClick(updatedStatus, '010-1111-2222');
+      }
+    } catch (error) {
+      console.error(`Error ${newStatus} reservation:`, error);
+      return;
+    }
+  };
+
   return status === StatusTypeEnum.pending ? (
     <StyledContainer>
-      <StyledPrimaryButton>승인</StyledPrimaryButton>
-      <StyledSecondaryButton>거절</StyledSecondaryButton>
+      <StyledPrimaryButton
+        onClick={() =>
+          handleActionButtonClick(MENTORING_APPLICATION_STATUS_ENUM.APPROVE)
+        }
+      >
+        승인
+      </StyledPrimaryButton>
+      <StyledSecondaryButton
+        onClick={() =>
+          handleActionButtonClick(MENTORING_APPLICATION_STATUS_ENUM.REJECT)
+        }
+      >
+        거절
+      </StyledSecondaryButton>
     </StyledContainer>
   ) : null;
 }
