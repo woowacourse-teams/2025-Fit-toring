5d635f9c2f5785c8144887ef15ae56dde4535095
Lee Dong Yeop <s01054830455@gmail.com>
2025-08-08 22:41:55 +0900

    refactor: import문 변경

--- backend/fittoring/src/main/java/fittoring/mentoring/business/service/ReservationService.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/service/ReservationService.java
@@ -3,15 +3,15 @@ package fittoring.mentoring.business.service;
 import fittoring.mentoring.business.exception.BusinessErrorMessage;
 import fittoring.mentoring.business.exception.MentoringNotFoundException;
 import fittoring.mentoring.business.exception.NotFoundMemberException;
+import fittoring.mentoring.business.exception.ReservationNotFoundException;
 import fittoring.mentoring.business.model.Image;
 import fittoring.mentoring.business.model.ImageType;
-import fittoring.mentoring.business.exception.ReservationNotFoundException;
 import fittoring.mentoring.business.model.Member;
 import fittoring.mentoring.business.model.Mentoring;
 import fittoring.mentoring.business.model.Reservation;
+import fittoring.mentoring.business.model.Status;
 import fittoring.mentoring.business.repository.CategoryMentoringRepository;
 import fittoring.mentoring.business.repository.ImageRepository;
-import fittoring.mentoring.business.model.Status;
 import fittoring.mentoring.business.repository.MemberRepository;
 import fittoring.mentoring.business.repository.MentoringRepository;
 import fittoring.mentoring.business.repository.ReservationRepository;
@@ -20,9 +20,9 @@ import fittoring.mentoring.business.service.dto.MentorMentoringReservationRespon
 import fittoring.mentoring.business.service.dto.PhoneNumberResponse;
 import fittoring.mentoring.business.service.dto.ReservationCreateDto;
 import fittoring.mentoring.presentation.dto.ParticipatedReservationResponse;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -114,8 +114,8 @@ public class ReservationService {
     public List<ParticipatedReservationResponse> findMemberReservations(Long memberId) {
         List<Reservation> memberReservations = reservationRepository.findAllByMenteeId(memberId);
         return memberReservations.stream()
-            .map(this::generateParticipatedReservationResponse)
-            .toList();
+                .map(this::generateParticipatedReservationResponse)
+                .toList();
     }
 
     private ParticipatedReservationResponse generateParticipatedReservationResponse(Reservation reservation) {
@@ -124,21 +124,21 @@ public class ReservationService {
         List<String> categoryTitles = categoryMentoringRepository.findTitleByMentoringId(mentoring.getId());
         boolean isReviewed = reviewRepository.existsByReservationId(reservation.getId());
         return new ParticipatedReservationResponse(
-            reservation.getId(),
-            mentoring.getId(),
-            mentoring.getMentor().getName(),
-            mentorProfileImage,
-            mentoring.getPrice(),
-            reservation.getCreatedAt().toLocalDate(),
-            categoryTitles,
-            isReviewed
+                reservation.getId(),
+                mentoring.getId(),
+                mentoring.getMentor().getName(),
+                mentorProfileImage,
+                mentoring.getPrice(),
+                reservation.getCreatedAt().toLocalDate(),
+                categoryTitles,
+                isReviewed
         );
     }
 
     private String findProfileImageUrl(Long relationId) {
         Optional<Image> image = imageRepository.findByImageTypeAndRelationId(
-            ImageType.MENTORING_PROFILE, relationId);
+                ImageType.MENTORING_PROFILE, relationId);
         return image.map(Image::getUrl)
-            .orElse(null);
+                .orElse(null);
     }
 }
