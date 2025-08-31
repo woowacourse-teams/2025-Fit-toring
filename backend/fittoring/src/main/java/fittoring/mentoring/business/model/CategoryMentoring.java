50b29a209172e74da5634e360b9c314286439b78
Lee Dong Yeop <s01054830455@gmail.com>
2025-08-31 23:50:18 +0900

    feat: 카테고리_멘토링 엔티티 삭제 상태, 삭제 시간 컬럼 추가와 flyway 스크립트 추가

--- backend/fittoring/src/main/java/fittoring/mentoring/business/model/CategoryMentoring.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/model/CategoryMentoring.java
@@ -1,5 +1,6 @@
 package fittoring.mentoring.business.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,8 +8,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -23,6 +26,13 @@ public class CategoryMentoring {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Getter
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+    @Getter
+    private LocalDateTime deletedAt;
+
     @ManyToOne
     @JoinColumn(nullable = false)
     private Category category;
@@ -33,7 +43,7 @@ public class CategoryMentoring {
     private Mentoring mentoring;
 
     public CategoryMentoring(Category category, Mentoring mentoring) {
-        this(null, category, mentoring);
+        this(null, false, null, category, mentoring);
     }
 
     public String getCategoryTitle() {
