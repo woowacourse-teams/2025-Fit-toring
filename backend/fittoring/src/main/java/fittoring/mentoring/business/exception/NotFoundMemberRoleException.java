4e1ae7fb2f838ebadce9d85fc4b3b3169212addc
ppparkta <pppppark2@gmail.com>
2025-09-01 02:34:04 +0900

    remove: 사용하지 않는 예외클래스 제거 #580

--- backend/fittoring/src/main/java/fittoring/mentoring/business/exception/NotFoundMemberRoleException.java
+++
@@ -1,8 +0,0 @@
-package fittoring.mentoring.business.exception;
-
-public class NotFoundMemberRoleException extends RuntimeException {
-
-    public NotFoundMemberRoleException(String message) {
-        super(message);
-    }
-}
