74de69bdb4f29784471224b848e83af672c51725
ppparkta <pppppark2@gmail.com>
2025-09-01 01:19:32 +0900

    feat: 관리자 리뷰 삭제 컨트롤러 구현 #582

--- backend/fittoring/src/main/java/fittoring/mentoring/presentation/api/admin/AdminReviewController.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/presentation/api/admin/AdminReviewController.java
@@ -7,6 +7,7 @@ import fittoring.mentoring.business.service.ReviewService;
 import fittoring.mentoring.presentation.dto.AdminReviewInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +29,15 @@ public class AdminReviewController {
                 mentoringId
         );
         return ResponseEntity.ok(response);
+    }
+
+    @AuthRequired
+    @DeleteMapping("/admin/reviews/{reviewId}")
+    public ResponseEntity<Void> deleteForAdmin(
+            @Login LoginInfo loginInfo,
+            @PathVariable("reviewId") Long reviewId
+    ) {
+        reviewService.deleteForAdmin(loginInfo.memberId(), reviewId);
+        return ResponseEntity.noContent().build();
     }
 }
