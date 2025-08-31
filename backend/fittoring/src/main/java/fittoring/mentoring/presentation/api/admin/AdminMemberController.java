bb5fa87f33c1e246e45df3dbe8cf4025df35b827
ppparkta <pppppark2@gmail.com>
2025-09-01 00:03:06 +0900

    refactor: 어노테이션 정렬 일관적으로 수정 #580

--- backend/fittoring/src/main/java/fittoring/mentoring/presentation/api/admin/AdminMemberController.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/presentation/api/admin/AdminMemberController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController
 @RequestMapping("/admin/members")
+@RestController
 public class AdminMemberController {
 
     private final MemberService memberService;
