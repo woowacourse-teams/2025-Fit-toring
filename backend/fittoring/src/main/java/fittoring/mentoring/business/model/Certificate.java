865b3111396073b580567c962109e8cfd56a7a37
Lee Dong Yeop <s01054830455@gmail.com>
2025-09-01 00:17:52 +0900

    feat: 자격증명 엔티티 soft delete 구현

--- backend/fittoring/src/main/java/fittoring/mentoring/business/model/Certificate.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/model/Certificate.java
@@ -20,6 +20,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -27,6 +29,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "UPDATE certificate SET is_deleted=true, deleted_at=now() WHERE id=?")
+@SQLRestriction("is_deleted = false")
 @Table(name = "certificate")
 @Entity
 public class Certificate {
