bce6e06252354d7818c0d6abffcb8a5c6ca8cb52
Beomtae <region9@naver.com>
2025-09-11 20:48:21 +0900

    feat: 헤더 테두리 제거 및 스크롤 시 테두리 생성 #643

--- frontend/src/common/components/Header/Header.tsx
+++ frontend/src/common/components/Header/Header.tsx
@@ -1,26 +1,36 @@
-import type { PropsWithChildren } from 'react';
-
+import { useEffect, useState, type PropsWithChildren } from 'react';
 import styled from '@emotion/styled';
 
 function Header({ children }: PropsWithChildren) {
-  return <StyledContainer>{children}</StyledContainer>;
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 0); // 스크롤이 0보다 크면 true
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <StyledContainer hasScrolled={hasScrolled}>{children}</StyledContainer>
+  );
 }
 
 export default Header;
 
-const StyledContainer = styled.header`
+const StyledContainer = styled.header<{ hasScrolled: boolean }>`
   position: sticky;
   top: 0;
   z-index: 100;
 
-  width: 48rem;
+  width: 100%;
   height: 5.7rem;
-  border-bottom: 1px solid ${({ theme }) => theme.OUTLINE.REGULAR};
-  box-shadow:
-    0 1px 3px 0 rgb(0 0 0 / 10%),
-    0 1px 2px -1px rgb(0 0 0 / 10%);
-
   background: ${({ theme }) => theme.BG.WHITE};
+
+  border-bottom: ${({ hasScrolled, theme }) =>
+    hasScrolled ? `1px solid ${theme.OUTLINE.REGULAR}` : 'none'};
 
   @media screen and (width <= 480px) {
     width: 100%;
