7c855244dbc7c6128c831b2c3f8e811b535eda08
Beomtae <region9@naver.com>
2025-09-11 17:49:19 +0900

    design: 모바일 레이아웃 배경 색상 변경 #643

--- frontend/src/common/components/MobileLayout/MobileLayout.tsx
+++ frontend/src/common/components/MobileLayout/MobileLayout.tsx
@@ -24,7 +24,8 @@ const StyledContainer = styled.main`
 const StyledContents = styled.section`
   width: 48rem;
 
-  background-color: ${({ theme }) => theme.BG.LIGHT};
+  background-color: ${({ theme }) => theme.BG.WHITE};
+  border: 1px solid ${({ theme }) => theme.SYSTEM.GRAY300};
 
   @media screen and (width <= 480px) {
     width: 100%;
