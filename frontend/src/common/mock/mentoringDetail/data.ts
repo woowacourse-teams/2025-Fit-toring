a5c94461d578cc77a8119b8605465a0701c3301a
kimyou1102 <yoosk5485@naver.com>
2025-09-11 11:59:19 +0900

    fix: 서버에서 오는 타입과 일치하게 타입 변경 #593

--- frontend/src/common/mock/mentoringDetail/data.ts
+++ frontend/src/common/mock/mentoringDetail/data.ts
@@ -2,7 +2,7 @@ import type { MentoringDetail } from '../../types/MentoringDetail';
 
 export const MENTORING_DETAIL: MentoringDetail = {
   id: 1,
-  mentorName: '김트레이너',
+  mentorName: "김트레이너",
   ratingAverage: ' 3.7',
   ratingCount: 10,
   categories: ['근력 증진', '다이어트', '체형 교정'],
@@ -13,39 +13,40 @@ export const MENTORING_DETAIL: MentoringDetail = {
     '5년차 전문 트레이너로 개인 맞춤 운동 및 식단 코칭을 제공합니다.',
   content:
     '김트레이너는 대단합니다. 저를 믿고 신청해주세요. 모든것을 도와드립니다. 여러분 저를 믿어주세요.',
+  chatUrl: '',
   certificates: [
     {
-      certificateId: '1',
+      certificateId: 1,
       title: '스포츠안마 자격증',
       type: 'LICENSE',
       imageUrl: '',
     },
     {
-      certificateId: '2',
+      certificateId: 2,
       title: '한국대학교 졸업증명서',
       type: 'EDUCATION',
       imageUrl: '',
     },
     {
-      certificateId: '3',
+      certificateId: 3,
       title: '헬스 트레이너 자격증',
       type: 'LICENSE',
       imageUrl: '',
     },
     {
-      certificateId: '4',
+      certificateId: 4,
       title: 'PT 자격증',
       type: 'LICENSE',
       imageUrl: '',
     },
     {
-      certificateId: '5',
+      certificateId: 5,
       title: '요가 자격증',
       type: 'LICENSE',
       imageUrl: '',
     },
     {
-      certificateId: '6',
+      certificateId: 6,
       title: '필라테스 자격증',
       type: 'LICENSE',
       imageUrl: '',
