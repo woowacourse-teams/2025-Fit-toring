1f64cf8d316841cfd9382d121935a0a820bf516a
kimyou1102 <yoosk5485@naver.com>
2025-09-12 18:06:40 +0900

    feat: 랜딩페이지 이미지로 구현 #653

--- frontend/src/pages/landing/Landing.tsx
+++ frontend/src/pages/landing/Landing.tsx
@@ -1,11 +1,75 @@
+import styled from '@emotion/styled';
+
+import landing1 from '../../common/assets/images/landing1.png';
+import landing2 from '../../common/assets/images/landing2.png';
+import landing3 from '../../common/assets/images/landing3.png';
+
 import Slogan from './components/Slogan';
 
 function Landing() {
   return (
     <div>
       <Slogan />
+      <StyledLandingBgWithColor>
+        <StyledImg src={landing1} alt="랜딩1" />
+      </StyledLandingBgWithColor>
+      <StyledLandingBg>
+        <StyledImg src={landing2} alt="랜딩2" />
+      </StyledLandingBg>
+      <StyledLandingBg>
+        <StyledImg src={landing3} alt="랜딩3" />
+      </StyledLandingBg>
+      <StyledButtonSection>
+        <StyledButton>시작하기</StyledButton>
+      </StyledButtonSection>
     </div>
   );
 }
 
 export default Landing;
+
+const StyledLandingBg = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  overflow: hidden;
+`;
+
+const StyledLandingBgWithColor = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  background: ${({ theme }) => `
+  linear-gradient(
+    180deg,
+    ${theme.SYSTEM.GRAY50} 0%,
+    #fff 100%
+  )
+`};
+  overflow: hidden;
+`;
+
+const StyledImg = styled.img`
+  width: 130%;
+`;
+
+const StyledButtonSection = styled.div`
+  height: 15rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledButton = styled.button`
+  width: 13rem;
+  border-radius: 7px;
+  padding: 1.5rem 2rem;
+  border: 2px solid #e3e3e3;
+  background: ${({ theme }) => theme.SYSTEM.GRAY900};
+  color: white;
+  border: none;
+  text-align: center;
+  font-size: 1.7rem;
+  font-weight: 700;
+  cursor: pointer;
+`;
