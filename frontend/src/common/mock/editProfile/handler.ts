de935a05276ff59aab27f59d05c9e884176b2ddc
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-09-20 18:16:37 +0900

    feat: 사용자 프로필 조회 MSW 핸들러 추가 #194

--- frontend/src/common/mock/editProfile/handler.ts
+++ frontend/src/common/mock/editProfile/handler.ts
@@ -2,6 +2,8 @@ import { http, HttpResponse } from 'msw';
 
 import { API_ENDPOINTS } from '../../constants/apiEndpoints';
 
+import { USER_PROFILE } from './data';
+
 import type { PartialUserProfileRequest } from '../../../pages/editProfile/types/userProfile';
 
 const BASE_URL = process.env.API_BASE_URL;
@@ -35,4 +37,15 @@ const patchMyProfile = http.patch(EDIT_PROFILE_URL, async ({ request }) => {
   );
 });
 
-export const editProfileHandlers = [patchMyProfile];
+const USER_INFO_URL = `${BASE_URL}${API_ENDPOINTS.MEMBERS_ME}`;
+const getUserInfo = http.get(`${USER_INFO_URL}`, () => {
+  const response = { ...USER_PROFILE };
+
+  if (testStateStore.shouldFail) {
+    return new HttpResponse({ message: '내 정보 조회 실패' }, { status: 500 });
+  }
+
+  return HttpResponse.json(response, { status: 200 });
+});
+
+export const editProfileHandlers = [patchMyProfile, getUserInfo];
