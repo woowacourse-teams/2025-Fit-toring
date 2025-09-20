82ee46dc83b9e76c3fe036b58b265b7d984a6e90
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-09-20 18:47:30 +0900

    feat: 사용자 정보 조회를 위한 useMyProfile 훅 추가 #194

--- frontend/src/pages/editProfile/EditProfile.tsx
+++ frontend/src/pages/editProfile/EditProfile.tsx
@@ -1,6 +1,14 @@
 import styled from '@emotion/styled';
 
+import useMyProfile from './hooks/useMyProfile';
+
 function EditProfile() {
+  const { myProfile } = useMyProfile();
+
+  if (!myProfile) {
+    return null;
+  }
+
   return <S_Container>회원 정보 수정 페이지</S_Container>;
 }
 
