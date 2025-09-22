9d19c3d178fe8cbaa788fd50c2f34c0e4b551ff5
ppparkta <pppppark2@gmail.com>
2025-09-22 21:02:30 +0900

    refactor: 마이페이지 내 멘토링 예약 목록 N+1 문제 해결

--- backend/fittoring/src/main/java/fittoring/mentoring/business/repository/ReservationRepository.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/repository/ReservationRepository.java
@@ -3,15 +3,23 @@ package fittoring.mentoring.business.repository;
 import fittoring.mentoring.business.model.Mentoring;
 import fittoring.mentoring.business.model.Reservation;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends ListCrudRepository<Reservation, Long> {
 
-    List<Reservation> findByMentoringId(Long mentoringId);
-
     List<Reservation> findAllByMentoringId(Long id);
+
+    @Query("""
+            SELECT r
+            FROM Reservation r
+            JOIN FETCH r.mentoring m
+            JOIN FETCH r.mentee mt
+            WHERE m.mentor.id = :mentorId
+            """)
+    List<Reservation> findAllByMentorId(Long mentorId);
 
     List<Reservation> findAllByMenteeId(Long menteeId);
 
