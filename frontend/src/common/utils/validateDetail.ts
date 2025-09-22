f4f1050bd7025a90caed70759b00aa07791fc714
Beomtae <region9@naver.com>
2025-09-22 14:08:49 +0900

    refactor: 타입 추론 되는 반환값 제거 #663

--- frontend/src/common/utils/validateDetail.ts
+++ frontend/src/common/utils/validateDetail.ts
@@ -1,6 +1,6 @@
 import { ERROR_MESSAGE } from '../constants/errorMessage';
 
-export const validateTextarea = (content: string): string => {
+export const validateTextarea = (content: string) => {
   if (content.length > 5000) {
     return ERROR_MESSAGE.CONTENT_LENGTH_TOO_LONG;
   }
