91f981bf2af7771de2ad2c960452ce9b44911bca
kimyou1102 <yoosk5485@naver.com>
2025-09-23 11:18:32 +0900

    build: pretendard semi bold 추가 #666
    Co-authored-by: Beomtae <Beomtae@users.noreply.github.com>
    Co-authored-by: dlsxjzld <dlsxjzld@users.noreply.github.com>

--- frontend/src/common/styles/fonts.ts
+++ frontend/src/common/styles/fonts.ts
@@ -2,12 +2,19 @@ import { css } from '@emotion/react';
 
 import PretendardBold from '../assets/fonts/Pretendard-Bold.subset.woff2';
 import PretendardRegular from '../assets/fonts/Pretendard-Regular.subset.woff2';
+import PretendardSemiBold from '../assets/fonts/Pretendard-SemiBold.subset.woff2';
 
 export const fonts = css`
   @font-face {
     font-family: Pretendard;
     font-weight: 400;
     src: url(${PretendardRegular}) format('woff2');
+  }
+
+  @font-face {
+    font-family: Pretendard;
+    font-weight: 600;
+    src: url(${PretendardSemiBold}) format('woff2');
   }
 
   @font-face {
