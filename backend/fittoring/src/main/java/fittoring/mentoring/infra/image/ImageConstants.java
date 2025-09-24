91de15cc19bd12f5e39e47a55c4753a7bbfa50dd
ppparkta <pppppark2@gmail.com>
2025-09-24 20:01:18 +0900

    refactor: 멘토링 썸네일 크기 조절

--- backend/fittoring/src/main/java/fittoring/mentoring/infra/image/ImageConstants.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/infra/image/ImageConstants.java
@@ -3,5 +3,5 @@ package fittoring.mentoring.infra.image;
 public class ImageConstants {
 
     public static final int DEFAULT_MAX_WIDTH = 500;
-    public static final int THUMBNAIL_MAX_WIDTH = 200;
+    public static final int THUMBNAIL_MAX_WIDTH = 300;
 }
