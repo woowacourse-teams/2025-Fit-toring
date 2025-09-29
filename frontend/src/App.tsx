08570fc72f39a6bddcb781d5b8dc71005ae0296a
kimyou1102 <yoosk5485@naver.com>
2025-09-30 02:20:34 +0900

    feat: 채팅방페이지 라우트 추가 #813

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { lazy } from 'react';
+
 import {
   createBrowserRouter,
   redirect,
@@ -6,9 +8,8 @@ import {
 
 import MobileLayout from './common/components/MobileLayout/MobileLayout';
 import { PAGE_URL } from './common/constants/url';
-import Landing from './pages/landing/Landing';
 import Home from './pages/home/Home';
-import { lazy } from 'react';
+import Landing from './pages/landing/Landing';
 
 const Login = lazy(() => import('./pages/login/Login'));
 const MentoringCreate = lazy(
@@ -28,6 +29,7 @@ const CreatedMentoring = lazy(
   () => import('./pages/createdMentoring/CreatedMentoring'),
 );
 const EditProfile = lazy(() => import('./pages/editProfile/EditProfile'));
+const ChatRoom = lazy(() => import('./pages/chatRoom/ChatRoom'));
 
 const router = createBrowserRouter([
   {
@@ -52,6 +54,7 @@ const router = createBrowserRouter([
     element: <MentoringUpdate />,
   },
   { path: PAGE_URL.LOGIN, element: <Login /> },
+  { path: `${PAGE_URL.CHAT_ROOM}/:roomId`, element: <ChatRoom /> },
   {
     path: `${PAGE_URL.MY_PAGE}`,
     element: <MyPage />,
---
+++ frontend/src/pages/chatRoom/ChatRoom.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ChatRoom() {
+  return <div>채팅방</div>;
+}
+
+export default ChatRoom;
