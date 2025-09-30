c771b0e8dc1829dd858733c3c7086030971464f3
kimyou1102 <yoosk5485@naver.com>
2025-09-30 16:00:21 +0900

    feat: ChatRoom 페이지 생성된 컴포넌트 적용 #813

--- frontend/src/pages/chatRoom/ChatRoom.tsx
+++ frontend/src/pages/chatRoom/ChatRoom.tsx
@@ -1,7 +1,168 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import styled from '@emotion/styled';
+
+import ChatContent from './components/ChatContent/ChatContent';
+import ChatRoomHeader from './components/ChatRoomHeader/ChatRoomHeader';
+import InputSection from './components/InputSection/InputSection';
+import MentoringActionPanel from './components/MentoringActionPanel/MentoringActionPanel';
+
+const DUMMY_MESSAGES = [
+  {
+    content:
+      '안녕하세요 회원님 멘토링 시작하겠습니다! 저는 멘토 김멘토입니다 어쩌구 저쩌구',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '어떤 내용이 궁금하실까요?',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '넵 안녕하세요 몇시쯤 어디서 만날까요?',
+    createdAt: '2026-09-10',
+    senderId: '2',
+  },
+  {
+    content:
+      '안녕하세요 회원님 멘토링 시작하겠습니다! 저는 멘토 김멘토입니다 어쩌구 저쩌구',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '어떤 내용이 궁금하실까요?',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '넵 안녕하세요 몇시쯤 어디서 만날까요?',
+    createdAt: '2026-09-10',
+    senderId: '2',
+  },
+  {
+    content:
+      '안녕하세요 회원님 멘토링 시작하겠습니다! 저는 멘토 김멘토입니다 어쩌구 저쩌구',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '어떤 내용이 궁금하실까요?',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '넵 안녕하세요 몇시쯤 어디서 만날까요?',
+    createdAt: '2026-09-10',
+    senderId: '2',
+  },
+  {
+    content:
+      '안녕하세요 회원님 멘토링 시작하겠습니다! 저는 멘토 김멘토입니다 어쩌구 저쩌구',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '어떤 내용이 궁금하실까요?',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '넵 안녕하세요 몇시쯤 어디서 만날까요?',
+    createdAt: '2026-09-10',
+    senderId: '2',
+  },
+  {
+    content:
+      '안녕하세요 회원님 멘토링 시작하겠습니다! 저는 멘토 김멘토입니다 어쩌구 저쩌구',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '어떤 내용이 궁금하실까요?',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '넵 안녕하세요 몇시쯤 어디서 만날까요?',
+    createdAt: '2026-09-10',
+    senderId: '2',
+  },
+  {
+    content:
+      '안녕하세요 회원님 멘토링 시작하겠습니다! 저는 멘토 김멘토입니다 어쩌구 저쩌구',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '어떤 내용이 궁금하실까요?',
+    createdAt: '2026-09-10',
+    senderId: '1',
+  },
+  {
+    content: '넵 안녕하세요 몇시쯤 어디서 만날까요?',
+    createdAt: '2026-09-10',
+    senderId: '2',
+  },
+];
 
 function ChatRoom() {
-  return <div>채팅방</div>;
+  const [value, setValue] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handlePaymentRequestClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {};
+
+  const handleReviewRequestClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {};
+
+  const handleEndClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {};
+
+  const handlePaymentClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {};
+
+  const handleReviewClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {};
+
+  return (
+    <S_Container>
+      <S_MentoringChatHeader>
+        <ChatRoomHeader name="김멘토" />
+        <MentoringActionPanel
+          mentorName="김멘토"
+          price={5000}
+          profileImageUrl="https://techcourse-project-2025.s3.amazonaws.com/fit-toring/profile-image/default/94a63bf8-4e70-40e2-a3fe-de2d7c7724c5.jpg"
+          mentorOwned={true}
+          onPaymentRequestClick={handlePaymentRequestClick}
+          onReviewRequestClick={handleReviewRequestClick}
+          onEndClick={handleEndClick}
+          onPaymentClick={handlePaymentClick}
+          onReviewClick={handleReviewClick}
+        />
+      </S_MentoringChatHeader>
+
+      <ChatContent messages={DUMMY_MESSAGES} />
+      <InputSection value={value} onChange={handleChange} />
+    </S_Container>
+  );
 }
 
 export default ChatRoom;
+
+const S_Container = styled.div`
+  position: relative;
+`;
+
+const S_MentoringChatHeader = styled.section`
+  position: sticky;
+  top: 0;
+`;
