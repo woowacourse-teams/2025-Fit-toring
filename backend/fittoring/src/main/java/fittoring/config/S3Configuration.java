f2199c00320f74a8dd8341549e9790a321722928
ppparkta <pppppark2@gmail.com>
2025-09-30 20:22:46 +0900

    fix: aws accessKey 제거

--- backend/fittoring/src/main/java/fittoring/config/S3Configuration.java
+++ backend/fittoring/src/main/java/fittoring/config/S3Configuration.java
@@ -3,7 +3,6 @@ package fittoring.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -18,7 +17,6 @@ public class S3Configuration {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.AP_NORTHEAST_2)
-                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
@@ -26,7 +24,6 @@ public class S3Configuration {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(Region.of(s3Properties.getRegion()))
-                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
--- backend/fittoring/src/main/java/fittoring/config/S3Properties.java
+++ backend/fittoring/src/main/java/fittoring/config/S3Properties.java
@@ -9,10 +9,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "aws.s3")
 public class S3Properties {
 
-    private final String accessKey;
-    private final String secretKey;
     private final String region;
     private final String bucketName;
-    private final String rootDirectory;
-    private final String tagging;
 }
