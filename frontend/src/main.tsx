03ddf1fe25f16727c1def95361e1484cac907407
Beomtae <region9@naver.com>
2025-09-30 19:29:32 +0900

    feat: tanstack 프로바이더 추가 #818

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,6 +10,7 @@ import AuthProvider from './common/components/AuthProvider/AuthProvider';
 import { fonts } from './common/styles/fonts';
 import { resetCss } from './common/styles/reset';
 import { THEME } from './common/styles/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
@@ -24,6 +25,8 @@ Sentry.init({
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
 });
+
+const queryClient = new QueryClient();
 
 async function enableMocking() {
   // 사용시 주석 제거
@@ -41,10 +44,12 @@ enableMocking().then(() => {
   createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <ThemeProvider theme={THEME}>
-        <AuthProvider>
-          <Global styles={[resetCss, fonts]} />
-          <App />
-        </AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <Global styles={[resetCss, fonts]} />
+            <App />
+          </AuthProvider>
+        </QueryClientProvider>
       </ThemeProvider>
     </React.StrictMode>,
   );
