abe47abbe7bf81ee211dff2151fc7b8f85531af4
ppparkta <pppppark2@gmail.com>
2025-10-01 13:24:54 +0900

    feat: 자격증명 이미지 저장 방식 변경

--- backend/fittoring/src/main/java/fittoring/mentoring/business/service/CertificateService.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/service/CertificateService.java
@@ -4,10 +4,8 @@ import fittoring.mentoring.business.exception.BusinessErrorMessage;
 import fittoring.mentoring.business.exception.CertificateNotFoundException;
 import fittoring.mentoring.business.exception.ForbiddenException;
 import fittoring.mentoring.business.exception.ImageNotFoundException;
-import fittoring.mentoring.business.exception.InvalidCertificateException;
 import fittoring.mentoring.business.exception.NotFoundMemberException;
 import fittoring.mentoring.business.model.Certificate;
-import fittoring.mentoring.business.model.CertificateType;
 import fittoring.mentoring.business.model.Image;
 import fittoring.mentoring.business.model.ImageType;
 import fittoring.mentoring.business.model.Member;
@@ -20,11 +18,11 @@ import fittoring.mentoring.business.service.dto.CertificateDeleteDto;
 import fittoring.mentoring.presentation.dto.CertificateDetailResponse;
 import fittoring.mentoring.presentation.dto.CertificateInfo;
 import fittoring.mentoring.presentation.dto.CertificateResponse;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Service
@@ -36,57 +34,36 @@ public class CertificateService {
 
     public void mapCertificatesToMentoring(
             List<CertificateInfo> certificateInfos,
-            List<MultipartFile> certificateImageFiles,
             Mentoring mentoring
     ) {
-        if (validateCertificateRequestData(certificateInfos, certificateImageFiles)) {
-            saveAllCertificates(certificateInfos, certificateImageFiles, mentoring);
-        }
-    }
-
-    private boolean validateCertificateRequestData(
-        List<CertificateInfo> certificateInfos,
-        List<MultipartFile> certificateImageFiles
-    ) {
-        if (certificateInfos == null || certificateImageFiles == null) {
-            return false;
-        }
-        if (certificateInfos.size() != certificateImageFiles.size()) {
-            throw new InvalidCertificateException(BusinessErrorMessage.CERTIFICATE_INFO_IMAGE_MISMATCH.getMessage());
-        }
-        for (CertificateInfo info : certificateInfos) {
-            if (info.title().isEmpty() || CertificateType.inValidCertificateType(info.type().name())) {
-                throw new InvalidCertificateException(
-                        BusinessErrorMessage.INVALID_CERTIFICATE_INFO.getMessage() + info.type().name() + " "
-                        + info.title());
-            }
-        }
-        return true;
+        // todo: S3 validate 추가
+        saveAllCertificates(certificateInfos, mentoring);
     }
 
     private void saveAllCertificates(
-        List<CertificateInfo> certificateInfos,
-        List<MultipartFile> certificateImageFiles,
-        Mentoring savedMentoring
+            List<CertificateInfo> certificateInfos,
+            Mentoring savedMentoring
     ) {
+        List<Image> certificateImages = new ArrayList<>();
         for (int i = 0; i < certificateInfos.size(); i++) {
             CertificateInfo certificateInfo = certificateInfos.get(i);
-            MultipartFile certificateImageFile = certificateImageFiles.get(i);
-
-            saveCertificate(certificateInfo, certificateImageFile, savedMentoring);
+            Long certificateId = saveCertificate(certificateInfo, savedMentoring);
+            certificateImages.add(new Image(
+                    certificateInfo.imageUrl(),
+                    ImageType.CERTIFICATE,
+                    certificateId
+            ));
         }
+        imageService.saveAll(certificateImages);
     }
 
-    private void saveCertificate(
-        CertificateInfo request,
-        MultipartFile certificateImageFile,
-        Mentoring mentoring
+    private Long saveCertificate(
+            CertificateInfo request,
+            Mentoring mentoring
     ) {
         final Certificate certificate = new Certificate(request.type(), request.title(), mentoring);
         final Certificate savedCertificate = certificateRepository.save(certificate);
-        Long certificateId = savedCertificate.getId();
-
-        imageService.uploadImageToS3(certificateImageFile, "certificate-image", ImageType.CERTIFICATE, certificateId);
+        return savedCertificate.getId();
     }
 
     public List<CertificateResponse> getAllCertificates(Long memberId, Status status) {
