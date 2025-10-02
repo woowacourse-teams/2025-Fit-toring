6502fa7b1ad59185005e24a2ebc71d8d2f60a9cc
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-02 17:05:55 +0900

    feat: WebSocket 인증 핸드셰이크 인터셉터 추가
    - AuthHandshakeInterceptor 클래스 생성 및 WebSocket 핸드셰이크 과정에서 인증 처리
    - 쿠키에서 액세스 토큰 추출 및 검증 로직 구현
    - 인증된 사용자 정보를 WebSocket 세션의 속성에 저장 (LoginInfo 객체 활용)
    - 토큰 미존재 또는 검증 실패 시 예외 처리 추가 (InvalidTokenException)

---
+++ backend/fittoring/src/main/java/fittoring/config/websocket/AuthHandshakeInterceptor.java
@@ -0,0 +1,62 @@
+package fittoring.config.websocket;
+
+import fittoring.config.auth.LoginInfo;
+import fittoring.mentoring.business.exception.BusinessErrorMessage;
+import fittoring.mentoring.business.exception.InvalidTokenException;
+import fittoring.mentoring.business.service.JwtExtractor;
+import fittoring.mentoring.business.service.JwtProvider;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+@RequiredArgsConstructor
+@Component
+public class AuthHandshakeInterceptor implements HandshakeInterceptor {
+
+    public static final String LOGIN_INFO_KEY = "loginInfo";
+    private static final String TOKEN_NAME = "accessToken";
+
+    private final JwtProvider jwtProvider;
+    private final JwtExtractor jwtExtractor;
+
+    @Override
+    public boolean beforeHandshake(
+            ServerHttpRequest request,
+            ServerHttpResponse response,
+            WebSocketHandler wsHandler,
+            Map<String, Object> attributes
+    ) {
+        if (request instanceof ServletServerHttpRequest servletRequest) {
+            HttpServletRequest httpServletRequest = servletRequest.getServletRequest();
+            Cookie[] cookies = httpServletRequest.getCookies();
+            validateCookie(cookies);
+            String token = jwtExtractor.extractTokenFromCookie(TOKEN_NAME, cookies);
+            jwtProvider.validateToken(token);
+            Long memberId = jwtProvider.getSubjectFromPayloadBy(token);
+            attributes.put(LOGIN_INFO_KEY, new LoginInfo(memberId));
+        }
+        return true;
+    }
+
+    private void validateCookie(final Cookie[] cookies) {
+        if (cookies == null || cookies.length == 0) {
+            throw new InvalidTokenException(BusinessErrorMessage.INVALID_TOKEN.getMessage());
+        }
+    }
+
+    @Override
+    public void afterHandshake(
+            ServerHttpRequest request,
+            ServerHttpResponse response,
+            WebSocketHandler wsHandler,
+            Exception exception
+    ) {
+    }
+}
