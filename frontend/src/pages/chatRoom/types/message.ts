aff72e83a2f6bcc3e1732be29ab22e1a917c0382
kimyou1102 <yoosk5485@naver.com>
2025-10-02 16:43:17 +0900

    refactor: Message 타입 변경 #825

---
+++ frontend/src/pages/chatRoom/types/message.ts
@@ -0,0 +1,14 @@
+export interface Message {
+  // 서버와 클라이언트 공통
+  content: string;
+  createdAt: string;
+  senderId: number;
+  chatRoomId: number;
+  tempId: number;
+
+  // 서버에서 받는 id
+  chatMessageId?: number;
+
+  // 클라이언트의 상태 관리를 위한 속성
+  status?: 'success' | 'fail';
+}
