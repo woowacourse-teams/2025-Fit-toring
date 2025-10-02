6aceb46c9303ad437c634ca438f74dfe154ab59b
Beomtae <region9@naver.com>
2025-10-02 16:52:18 +0900

    refactor: WAF 차단으로 인한 redirect url 제거 #800

--- frontend/src/pages/kakaoCallback/apis/postKakaoLogin.ts
+++ frontend/src/pages/kakaoCallback/apis/postKakaoLogin.ts
@@ -2,9 +2,8 @@ import { apiClient } from '../../../common/apis/apiClient';
 import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
 
 export const postKakaoLogin = async (authCode: string) => {
-  const redirectUri = encodeURIComponent(process.env.KAKAO_REDIRECT_URI!);
   return await apiClient.post({
-    endpoint: `${API_ENDPOINTS.KAKAO_LOGIN}?code=${encodeURIComponent(authCode)}&redirectUrl=${redirectUri}`,
+    endpoint: `${API_ENDPOINTS.KAKAO_LOGIN}?code=${encodeURIComponent(authCode)}`,
     withCredentials: true,
   });
 };
