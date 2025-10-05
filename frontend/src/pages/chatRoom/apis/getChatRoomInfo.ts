c6f940b18f78a69f0701901bf57825ab4bd34b47
kimyou1102 <yoosk5485@naver.com>
2025-10-05 15:35:56 +0900

    feat: getChatRoomInfo 함수 생성 #825

---
+++ frontend/src/pages/chatRoom/apis/getChatRoomInfo.ts
@@ -0,0 +1,11 @@
+import { apiClient } from '../../../common/apis/apiClient';
+import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
+
+import type { Message } from '../types/message';
+
+export const getChatRoomInfo = async (chatroomId: number) => {
+  return await apiClient.get<Message[]>({
+    endpoint: `${API_ENDPOINTS.CHATROOMS}/${chatroomId}`,
+    withCredentials: true,
+  });
+};
