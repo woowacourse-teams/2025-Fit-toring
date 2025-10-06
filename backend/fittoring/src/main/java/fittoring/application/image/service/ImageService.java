afa4aee1ca3c2b78df3d5a6df73d110b2eab2bc8
jye326 <jye326@naver.com>
2025-10-07 02:56:51 +0900

    feat: 멘토링 예약 응답에 프로필 이미지 조회 로직 추가
    - 멘토링 예약 응답에 `ImageService` 활용하여 프로필 이미지 매핑 로직 추가
    - `ParticipatedReservationDto` 대신 `ParticipatedReservationWithoutProfileImageDto`로 대체
    - `ReservationRepository`의 쿼리에서 이미지 직접 조회 제거
    - 새로운 이미지 조회 메서드 `findByImageTypeAndRelationIdIn` 추가
    - `findMentoringThumbnailMapByImageTypeAndRelationIds` 메서드로 이미지 매핑 로직 구현

--- backend/fittoring/src/main/java/fittoring/application/image/service/ImageService.java
+++ backend/fittoring/src/main/java/fittoring/application/image/service/ImageService.java
@@ -4,8 +4,13 @@ import fittoring.application.image.repository.ImageRepository;
 import fittoring.domain.model.Image;
 import fittoring.domain.model.ImageType;
 import fittoring.domain.model.ImageVariant;
+
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -51,6 +56,28 @@ public class ImageService {
                         .filter(img -> img.getImageVariant() == ImageVariant.DEFAULT)
                         .findFirst()
                 );
+    }
+
+    public Map<Long, String> findMentoringThumbnailMapByImageTypeAndRelationIds(
+            ImageType imageType, Collection<Long> relationIds
+    ) {
+        List<Image> images = imageRepository.findByImageTypeAndRelationIdIn(
+                imageType, relationIds
+        );
+
+        return images.stream()
+                .collect(Collectors.groupingBy(
+                        Image::getRelationId,
+                        Collectors.collectingAndThen(Collectors.toList(), list -> list.stream()
+                                .filter(img -> img.getImageVariant() == ImageVariant.THUMBNAIL)
+                                .findFirst()
+                                .or(() -> list.stream()
+                                        .filter(img -> img.getImageVariant() == ImageVariant.DEFAULT)
+                                        .findFirst()
+                                )
+                                .map(Image::getUrl)
+                                .orElse(null))
+                ));
     }
 
     public List<Image> findByRelationIdsAndImageType(List<Long> certificateIds, ImageType imageType) {
