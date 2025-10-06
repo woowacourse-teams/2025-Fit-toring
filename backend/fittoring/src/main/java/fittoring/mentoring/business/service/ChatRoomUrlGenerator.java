569e33bc62b2efb33dc7864fb082c347851439ab
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-06 18:17:41 +0900

    refactor: 상수 선언 방식 수정
    - BASE_URL의 선언 순서를 Java 컨벤션에 맞게 변경 (static final 순서)

--- backend/fittoring/src/main/java/fittoring/mentoring/business/service/ChatRoomUrlGenerator.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/service/ChatRoomUrlGenerator.java
@@ -2,7 +2,7 @@ package fittoring.mentoring.business.service;
 
 public class ChatRoomUrlGenerator {
 
-    private final static String BASE_URL = "https://www.fittoring.com/chat/room/";
+    private static final String BASE_URL = "https://www.fittoring.com/chat/room/";
 
     public static String generate(Long chatRoomId) {
         return BASE_URL + chatRoomId;
