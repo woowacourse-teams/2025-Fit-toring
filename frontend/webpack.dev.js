c96426520afa816f24e84558bd24cf8a3009dc55
Beomtae <region9@naver.com>
2025-10-08 16:12:02 +0900

    refactor: webpack.local 로 실행파일이 변경됨에 따른 불필요한 설정 제거 #800

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -7,18 +7,6 @@ const DotenvWebpackPlugin = require('dotenv-webpack');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-    client: {
-      overlay: true,
-    },
-  },
   plugins: [
     new DotenvWebpackPlugin({
       path: path.resolve(__dirname, '.env.dev'),
