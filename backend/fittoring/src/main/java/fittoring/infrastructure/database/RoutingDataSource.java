9fea72ccc7a7e65eadda291b65a486c10bf42118
ppparkta <pppppark2@gmail.com>
2025-10-08 22:18:50 +0900

    refactor: debug 단계로 조정

--- backend/fittoring/src/main/java/fittoring/infrastructure/database/RoutingDataSource.java
+++ backend/fittoring/src/main/java/fittoring/infrastructure/database/RoutingDataSource.java
@@ -19,11 +19,11 @@ public class RoutingDataSource extends AbstractRoutingDataSource {
     protected Object determineCurrentLookupKey() {
         boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
         if (readOnly) {
-            log.info("readOnly = true, request to replica");
+            log.debug("readOnly = true, request to replica");
             return DataSourceType.REPLICA;
         }
 
-        log.info("readOnly = false, request to source");
+        log.debug("readOnly = false, request to source");
         return DataSourceType.SOURCE;
     }
 }
