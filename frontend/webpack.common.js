3bf1ed57c74b0d63c1c8d17a4a129a3f09e919e3
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-09 20:07:52 +0900

    refactor: fonts.d.ts 파일 삭제 및 webpack 설정에서 폰트 관련 규칙 제거 #876

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -34,13 +34,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif|avif)$/i,
         type: 'asset',
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/fonts/[name][ext]',
-        },
-      },
     ],
   },
   resolve: {
