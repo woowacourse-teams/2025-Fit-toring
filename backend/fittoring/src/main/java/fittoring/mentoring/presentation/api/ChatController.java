f9ea7b8e8a9dc95c40f89db6c488a3d97b10e985
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-09 20:49:27 +0900

    feat: ChatMessageRequest 입력 유효성 검사 추가

--- backend/fittoring/src/main/java/fittoring/mentoring/presentation/api/ChatController.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/presentation/api/ChatController.java
@@ -5,6 +5,7 @@ import fittoring.config.websocket.AuthHandshakeInterceptor;
 import fittoring.mentoring.business.service.ChatMessageService;
 import fittoring.mentoring.presentation.dto.chat.request.ChatMessageRequest;
 import fittoring.mentoring.presentation.dto.chat.response.ChatMessageResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
@@ -22,7 +23,7 @@ public class ChatController {
     @MessageMapping("/chatroom/{chatRoomId}")
     public void chat(
             @DestinationVariable Long chatRoomId,
-            ChatMessageRequest request,
+            @Valid ChatMessageRequest request,
             @Header(AuthHandshakeInterceptor.LOGIN_INFO_KEY) LoginInfo loginInfo
     ) {
         ChatMessageResponse response = chatMessageService.registerMessage(chatRoomId, request, loginInfo.memberId());
