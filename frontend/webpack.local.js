5095065c5f2a00b41b418ac91076e17bde77575e
Beomtae <region9@naver.com>
2025-10-10 12:39:44 +0900

    fix: webpack.prod 경로 추가 설정

--- frontend/webpack.local.js
+++ frontend/webpack.local.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 module.exports = merge(common, {
