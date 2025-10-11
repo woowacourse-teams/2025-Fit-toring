1ccd0cadbc15067b5c28ba66ad8c7d72e17f701c
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-11 20:12:35 +0900

    feat: 관리자 멘토링 Service 클래스 추가
    - 관리자 별도로 멘토링에 관련된 service 로직을 수행할 수 있도록 관리자용 멘토링 service 구현

---
+++ backend/fittoring/src/main/java/fittoring/admin/service/AdminMentoringService.java
@@ -0,0 +1,35 @@
+package fittoring.admin.service;
+
+import fittoring.admin.presentation.dto.AdminMentoringResponse;
+import fittoring.admin.presentation.dto.PageResult;
+import fittoring.application.mentoring.repository.MentoringRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class AdminMentoringService {
+
+    private static final int PAGE_SIZE = 10;
+
+    private final MentoringRepository mentoringRepository;
+
+    @Transactional(readOnly = true)
+    public PageResult<AdminMentoringResponse> findAllForAdminPaged(Long memberId, int page) {
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE, Sort.by("id").descending());
+        Page<AdminMentoringResponse> allPagination = mentoringRepository.findAllWithPagination(pageable);
+
+        return new PageResult<>(
+                allPagination.getContent(),
+                allPagination.getNumber(),
+                PAGE_SIZE,
+                allPagination.getTotalElements(),
+                allPagination.getTotalPages()
+        );
+    }
+}
