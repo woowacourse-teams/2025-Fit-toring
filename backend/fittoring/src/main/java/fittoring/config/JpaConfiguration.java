d7ad285075bec1c2dc5bec485264c89676cde4eb
ppparkta <pppppark2@gmail.com>
2025-10-13 01:36:34 +0900

    refactor: DB 시간 정밀도 문제 해결 위한 설정 추가

--- backend/fittoring/src/main/java/fittoring/config/JpaConfiguration.java
+++ backend/fittoring/src/main/java/fittoring/config/JpaConfiguration.java
@@ -1,9 +1,27 @@
 package fittoring.config;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 @Configuration
 public class JpaConfiguration {
+
+    @Bean
+    public DateTimeProvider auditingDateTimeProvider(Clock clock) {
+        return () -> Optional.of(
+                Instant.now(clock).truncatedTo(ChronoUnit.MILLIS)
+        );
+    }
+
+    @Bean
+    public Clock systemClock() {
+        return Clock.systemUTC();
+    }
 }
