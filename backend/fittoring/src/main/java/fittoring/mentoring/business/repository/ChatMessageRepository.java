6214e5f010b3648c9f2b409a844cca404da8304c
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-12 18:21:05 +0900

    refactor: ChatMessageRepository가  CustomChatMessageRepository를 상속받도록 변경

--- backend/fittoring/src/main/java/fittoring/mentoring/business/repository/ChatMessageRepository.java
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/repository/ChatMessageRepository.java
@@ -1,12 +1,9 @@
 package fittoring.mentoring.business.repository;
 
 import fittoring.mentoring.business.model.ChatMessage;
-import java.util.List;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatMessageRepository extends ListCrudRepository<ChatMessage, Long> {
-
-    List<ChatMessage> findAllByChatRoomId(Long chatroomId);
+public interface ChatMessageRepository extends ListCrudRepository<ChatMessage, Long>, CustomChatMessageRepository {
 }
