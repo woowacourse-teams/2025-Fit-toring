6dfc423e01979d8848372a3e594f912260b4e719
sooyang <86940801+ppparkta@users.noreply.github.com>
2025-10-13 15:16:28 +0900

    [FEAT] 레플리카DB 라우팅 구현 (#875)
    * build: application 설정 환경에 따라 분리
    
    * feat: replica 라우팅 구현
    
    * feat: OSIV 옵션 비활성화
    
    * fix: 트랜잭션 범위 지정
    
    * refactor: debug 단계로 조정
    
    * refactor: 라우팅 초기화 호출 추가
    
    * fix: 프로필 범위 지정
    
    * fix: metrics 추가
    
    * refactor: 변수명 의미에 맞게 수정
    
    * refactor: 상수 import 제거

---
+++ backend/fittoring/src/main/java/fittoring/infrastructure/database/RoutingDataSource.java
@@ -0,0 +1,30 @@
+package fittoring.infrastructure.database;
+
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+    public static RoutingDataSource from(Map<Object, Object> dataSources) {
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+        routingDataSource.setDefaultTargetDataSource(dataSources.get(DataSourceType.SOURCE));
+        routingDataSource.setTargetDataSources(dataSources);
+        routingDataSource.afterPropertiesSet();
+        return routingDataSource;
+    }
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+        if (readOnly) {
+            log.debug("readOnly = true, request to replica");
+            return DataSourceType.REPLICA;
+        }
+
+        log.debug("readOnly = false, request to source");
+        return DataSourceType.SOURCE;
+    }
+}
