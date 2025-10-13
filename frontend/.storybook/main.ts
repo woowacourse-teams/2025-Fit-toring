fe8fdf4ee64bc74dad505605ef78f7c7c4780b50
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-13 19:04:56 +0900

    refactor: 주석 처리된 webpackFinal 설정 및 관련 코드 정리 #876

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,5 +1,3 @@
-// import webpack from 'webpack';
-
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
@@ -18,24 +16,5 @@ const config: StorybookConfig = {
     KAKAO_REST_API_KEY: process.env.KAKAO_REST_API_KEY || '',
     KAKAO_REDIRECT_URL: process.env.KAKAO_REDIRECT_URL || '',
   }),
-  // webpackFinal: async (config) => {
-  //   config.plugins = config.plugins || [];
-  //   config.plugins.push(
-  //     new webpack.DefinePlugin({
-  //       'process.env': {
-  //         NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-  //         SENTRY_DSN: JSON.stringify(process.env.SENTRY_DSN || ''),
-  //         API_BASE_URL: JSON.stringify(
-  //           process.env.API_BASE_URL || 'http://localhost:6006',
-  //         ),
-  //         GOOGLE_ANALYTICS_ID: JSON.stringify(
-  //           process.env.GOOGLE_ANALYTICS_ID || '',
-  //         ),
-  //       },
-  //     }),
-  //   );
-
-  //   return config;
-  // },
 };
 export default config;
