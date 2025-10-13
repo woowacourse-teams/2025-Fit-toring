da9ca6632663524a9d57ca91857c1960f1d235d3
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-13 19:11:58 +0900

    refactor: 예약 상태 검증 로직 이동(status -> reservation)

--- backend/fittoring/src/main/java/fittoring/domain/model/Reservation.java
+++ backend/fittoring/src/main/java/fittoring/domain/model/Reservation.java
@@ -1,5 +1,7 @@
 package fittoring.domain.model;
 
+import fittoring.application.exception.BusinessErrorMessage;
+import fittoring.application.exception.InvalidStatusException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -69,8 +71,17 @@ public class Reservation {
     }
 
     public void changeStatus(Status updateStatus) {
-        this.status.validateReservation(updateStatus);
+        validateReservation(updateStatus);
         this.status = updateStatus;
+    }
+
+    private void validateReservation(Status updateStatus) {
+        if (this.status.isReject() || this.status.isComplete()) {
+            throw new InvalidStatusException(BusinessErrorMessage.RESERVATION_STATUS_ALREADY_UPDATE.getMessage());
+        }
+        if (this.status.equals(updateStatus)) {
+            throw new InvalidStatusException(BusinessErrorMessage.RESERVATION_STATUS_ALREADY_EQUAL.getMessage());
+        }
     }
 
     public boolean isCreatedByMember(Long memberId) {
