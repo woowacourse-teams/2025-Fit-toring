2d866226c70bb15dacddab261cb9e91ead8c1b91
kimyou1102 <yoosk5485@naver.com>
2025-10-14 20:11:48 +0900

    refactor: 페이지네이션 방식에 맞게 getChatRoom API 함수 변경 #825

--- frontend/src/pages/chatRoom/apis/getChatRoom.ts
+++ frontend/src/pages/chatRoom/apis/getChatRoom.ts
@@ -3,9 +3,24 @@ import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
 
 import type { Message } from '../types/message';
 
-export const getChatRoom = async (chatRoomId: number) => {
+export const getChatRoom = async ({
+  chatRoomId,
+  sortKey,
+  cursorCode,
+}: {
+  chatRoomId: number;
+  sortKey: string;
+  cursorCode?: string;
+}) => {
+  const searchParams: Record<string, string> = { sortKey };
+
+  if (cursorCode) {
+    searchParams.cursorCode = cursorCode;
+  }
+
   return await apiClient.get<Message[]>({
     endpoint: `${API_ENDPOINTS.CHATROOMS}/${chatRoomId}/messages`,
     withCredentials: true,
+    searchParams,
   });
 };
