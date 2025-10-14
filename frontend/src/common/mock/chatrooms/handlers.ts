ee19991cbbd8d0d554059d7b0d7aca36e0a81ce6
kimyou1102 <yoosk5485@naver.com>
2025-10-14 20:11:22 +0900

    refactor: 페이지네이션 방식에 맞게 모킹데이터 및 msw 핸들러 함수 변경 #825

--- frontend/src/common/mock/chatrooms/handlers.ts
+++ frontend/src/common/mock/chatrooms/handlers.ts
@@ -2,15 +2,39 @@ import { http, HttpResponse } from 'msw';
 
 import { API_ENDPOINTS } from '../../constants/apiEndpoints';
 
-import { CHAT_MESSAGES, CHAT_ROOM_INFO } from './data';
+import {
+  CHAT_MESSAGES1,
+  CHAT_MESSAGES2,
+  CHAT_MESSAGES3,
+  CHAT_MESSAGES4,
+  CHAT_ROOM_INFO,
+} from './data';
 
-const getChatRoom = http.get(
+const mockPages = {
+  null: CHAT_MESSAGES1,
+  '1758712152|30': CHAT_MESSAGES2,
+  '1758712152|20': CHAT_MESSAGES3,
+  '1758712152|10': CHAT_MESSAGES4,
+};
+
+export const getChatRoom = http.get(
   `*${API_ENDPOINTS.CHATROOMS}/:chatroomId/messages`,
-  () => {
-    return HttpResponse.json(CHAT_MESSAGES);
+  ({ request }) => {
+    const url = new URL(request.url);
+    const cursorCode = url.searchParams.get('cursorCode');
+
+    const data = mockPages[cursorCode ?? 'null'];
+
+    if (!data) {
+      return HttpResponse.json(
+        { chatMessages: [], nextCursorCode: null, hasNext: false },
+        { status: 200 },
+      );
+    }
+
+    return HttpResponse.json(data, { status: 200 });
   },
 );
-
 const getChatRoomInfo = http.get(
   `*${API_ENDPOINTS.CHATROOMS}/:chatroomId`,
   () => {
