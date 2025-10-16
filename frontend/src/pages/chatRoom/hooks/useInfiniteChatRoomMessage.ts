04bcd9e41f54e72070cdf353a75a6cf4108974b9
kimyou1102 <yoosk5485@naver.com>
2025-10-16 21:59:55 +0900

    refactor: 채팅 메시지 페이지네이션 조회 로직을 useInfiniteChatRoomMessage 훅으로 분리 #825

---
+++ frontend/src/pages/chatRoom/hooks/useInfiniteChatRoomMessage.ts
@@ -0,0 +1,68 @@
+import {
+  useInfiniteQuery,
+  type InfiniteData,
+  type QueryKey,
+} from '@tanstack/react-query';
+
+import { getChatRoom } from '../apis/getChatRoom';
+
+import type { MessageResponse } from '../types/message';
+
+interface ChatRoomPageParam {
+  chatRoomId: number;
+  sortKey: string;
+  cursorCode?: string;
+}
+
+const useInfiniteChatRoomMessage = (chatRoomId: number) => {
+  const {
+    data: chatRoomMessage,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useInfiniteQuery<
+    MessageResponse,
+    Error,
+    InfiniteData<MessageResponse>,
+    QueryKey,
+    ChatRoomPageParam
+  >({
+    queryKey: ['chatRoom', chatRoomId],
+    queryFn: async ({ pageParam }) => {
+      return getChatRoom(pageParam);
+    },
+    initialPageParam: {
+      chatRoomId: Number(chatRoomId),
+      sortKey: 'CREATED_AT',
+    },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.hasNext) {
+        return {
+          chatRoomId: Number(chatRoomId),
+          cursorCode: lastPage.nextCursorCode,
+          sortKey: 'CREATED_AT',
+        };
+      }
+      return undefined;
+    },
+    select: (data) => ({
+      ...data,
+      pages: data.pages
+        .slice()
+        .reverse()
+        .map((page) => ({
+          ...page,
+          chatMessages: [...page.chatMessages].reverse(),
+        })),
+    }),
+  });
+
+  return {
+    chatRoomMessage,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  };
+};
+
+export default useInfiniteChatRoomMessage;
