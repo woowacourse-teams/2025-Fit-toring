5d27d21fc8f7f42d147e1150def326b75fdeafac
kimyou1102 <yoosk5485@naver.com>
2025-10-19 17:27:27 +0900

    refactor: 채팅방 메시지 조회 API 쿼리 파라미터 변경 반영 #825

--- frontend/src/pages/chatRoom/apis/getChatRoom.ts
+++ frontend/src/pages/chatRoom/apis/getChatRoom.ts
@@ -5,22 +5,14 @@ import type { MessageResponse } from '../types/message';
 
 export const getChatRoom = async ({
   chatRoomId,
-  sortKey,
   cursorCode,
 }: {
   chatRoomId: number;
-  sortKey: string;
   cursorCode?: string | null;
 }) => {
-  const searchParams: Record<string, string> = { sortKey };
-
-  if (cursorCode) {
-    searchParams.cursorCode = cursorCode;
-  }
-
   return await apiClient.get<MessageResponse>({
     endpoint: `${API_ENDPOINTS.CHATROOMS}/${chatRoomId}/messages`,
     withCredentials: true,
-    searchParams,
+    ...(cursorCode ? { searchParams: { cursorCode } } : {}),
   });
 };
