dc50a90576b6b1c796892c35a0fef4a8c8799bed
ppparkta <pppppark2@gmail.com>
2025-10-20 00:27:11 +0900

    refactor: 로컬 환경에서 SQS 리스너 동작하지 않도록 수정

--- backend/fittoring/src/main/java/fittoring/infrastructure/ImageSqsListener.java
+++ backend/fittoring/src/main/java/fittoring/infrastructure/ImageSqsListener.java
@@ -5,10 +5,12 @@ import fittoring.infrastructure.dto.ImageReadyMessageDto;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
+@Profile({"!local & !test"})
 @Component
 public class ImageSqsListener {
 
