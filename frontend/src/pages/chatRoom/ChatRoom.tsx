0d31bc1d2ac59465d7acec6ccf603f62ead46f5a
kimyou1102 <yoosk5485@naver.com>
2025-10-20 22:03:05 +0900

    refactor: localStorage의 memberId를 객체 전체 대신 내부 값으로 저장하도록 변경 #957

--- frontend/src/pages/chatRoom/ChatRoom.tsx
+++ frontend/src/pages/chatRoom/ChatRoom.tsx
@@ -31,7 +31,8 @@ function ChatRoom() {
   const { chatRoomId } = useParams();
 
   const storedData = localStorage.getItem('memberId');
-  const memberId = storedData ? JSON.parse(storedData) : null;
+  const parsedData = storedData ? JSON.parse(storedData) : null;
+  const memberId = parsedData ? parsedData.memberId : null;
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
