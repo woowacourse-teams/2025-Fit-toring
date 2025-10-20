fd6d1e24a6d1ee47cea6996d965d6c122988fea1
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-20 16:14:40 +0900

    [FEAT] 멘토링 정렬 기능 추가 (#930)
    * feat: 정렬을 위한 useSortKey 훅 추가 #917
    
    * feat: Home 페이지에 멘토링 정렬 기능 추가 #917
    
    * feat: 무한 스크롤 멘토 목록 요청에 정렬 키 추가 #917
    
    * feat: 전문 분야 필터링된 멘토 목록 요청에 정렬 키 추가 #917
    
    * feat: 정렬 버튼에 다양한 정렬 옵션 추가 #917
    
    * feat: 정렬 버튼에 현재 정렬 키 전달 및 드롭다운 메뉴 추가 #917
    
    * feat: SortDropDown으로 네이밍 변경 #917
    
    * feat: SortDropDown에서 외부 클릭 시 드롭다운 닫기 기능 추가 #917
    
    * refactor: SortDropDown에서 핸들러 이름을 변경 #917
    
    * feat: 정렬 버튼 클릭 시 드롭다운 닫기 기능 추가 #917

--- frontend/src/pages/home/components/SortDropDown/SortDropDown.stories.tsx
+++ frontend/src/pages/home/components/SortDropDown/SortDropDown.stories.tsx
@@ -1,13 +1,13 @@
-import SortButton from './SortButton';
+import SortDropDown from './SortDropDown';
 
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
 
 const meta = {
-  title: 'Home/SortButton',
-  component: SortButton,
+  title: 'Home/SortDropDown',
+  component: SortDropDown,
 
   decorators: [(Story) => <Story />],
-} satisfies Meta<typeof SortButton>;
+} satisfies Meta<typeof SortDropDown>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -15,11 +15,13 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     handleSortButtonClick: () => {},
+    currentSortKey: 'CREATED_AT',
   },
   parameters: {
     docs: {
       description: {
-        story: 'SortButton 컴포넌트는 멘토 리스트를 정렬하는 버튼입니다.',
+        story:
+          'SortDropDown 컴포넌트는 멘토 리스트를 정렬하는 버튼들이 보이는 드롭다운입니다.',
       },
     },
   },
