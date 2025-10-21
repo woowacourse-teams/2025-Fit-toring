a11bc304392316f26462e904c825dfaa1ae71c43
ppparkta <pppppark2@gmail.com>
2025-10-22 02:50:46 +0900

    fix: delete 시 강제로 DB에 반영한다

--- backend/fittoring/src/main/java/fittoring/application/image/repository/ImageRepository.java
+++ backend/fittoring/src/main/java/fittoring/application/image/repository/ImageRepository.java
@@ -74,5 +74,13 @@ public interface ImageRepository extends ListCrudRepository<Image, Long> {
             @Param("baseName") String baseName
     );
 
-    void deleteByImageTypeAndRelationId(ImageType imageType, Long relationId);
+    @Modifying(flushAutomatically = true)
+    @Query("""
+            DELETE FROM Image i
+            WHERE i.imageType = :type and i.relationId = :relationId
+            """)
+    void deleteByImageTypeAndRelationId(
+            @Param("type") ImageType imageType,
+            @Param("relationId") Long relationId
+    );
 }
