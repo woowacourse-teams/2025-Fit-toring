9783a69665af06ee99212b29a3f5d350817dddd4
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-31 16:13:40 +0900

    refactor: useMyProfile 훅에서 쿼리 키를 상수로 정의하고 memberId를 기반으로 쿼리 키 설정 #1047

--- frontend/src/pages/editProfile/hooks/useMyProfile.ts
+++ frontend/src/pages/editProfile/hooks/useMyProfile.ts
@@ -5,6 +5,10 @@ import { getUserInfo } from '../../../common/apis/getUserInfo';
 import type { UserInfo } from '../../../common/types/userInfo';
 import type { UserProfileResponse } from '../types/userProfile';
 
+export const MY_PROFILE_QUERY_KEY = {
+  myProfile: (key: string | null) => ['myProfile', key],
+} as const;
+
 const convertResponse = (response: UserInfo): UserProfileResponse => {
   const { name, gender, phoneNumber, image } = response;
   return {
@@ -16,8 +20,12 @@ const convertResponse = (response: UserInfo): UserProfileResponse => {
 };
 
 const useMyProfile = () => {
+  const storedData = localStorage.getItem('memberId');
+  const parsedData = storedData ? JSON.parse(storedData) : null;
+  const memberId = parsedData ? parsedData.memberId : null;
+
   const { data: myProfile } = useQuery({
-    queryKey: ['myProfile'],
+    queryKey: MY_PROFILE_QUERY_KEY.myProfile(memberId),
     queryFn: async () => {
       const response = await getUserInfo();
       return convertResponse(response);
