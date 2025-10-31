0153127e581e0476606bfc038041426bc53b0186
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-31 17:50:49 +0900

    test: 회원 정보 수정 API 테스트 추가

--- backend/fittoring/src/test/java/fittoring/integration/MemberIntegrationTest.java
+++ backend/fittoring/src/test/java/fittoring/integration/MemberIntegrationTest.java
@@ -2,11 +2,13 @@ package fittoring.integration;
 
 import fittoring.AbstractApiDocumentationTest;
 import fittoring.application.auth.service.JwtProvider;
+import fittoring.application.member.presentation.dto.request.MemberInfoUpdateRequest;
 import fittoring.application.member.repository.MemberRepository;
 import fittoring.domain.model.Member;
 import fittoring.domain.model.Phone;
 import fittoring.domain.model.password.Password;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,7 +37,7 @@ class MemberIntegrationTest extends AbstractApiDocumentationTest {
                 .accept("application/json")
                 .filter(documentWithTag("member/get-members-me-success"))
                 .cookie("accessToken", accessToken)
-                .log().all().then()
+                .log().all()
                 .when()
                 .get("/members/me")
                 .then()
@@ -82,5 +84,101 @@ class MemberIntegrationTest extends AbstractApiDocumentationTest {
                 .get("/members/me")
                 .then()
                 .statusCode(401);
+    }
+
+    @DisplayName("회원(멘토, 멘티)은 자신의 회원 정보인 이름, 성별, 비밀번호, 전화번호를 수정할 수 있다. 수정에 성공하면 204 상태코드를 응답한다.")
+    @Test
+    void updateInfo() {
+        // given
+        String rawName = "이름";
+        String rawGender = "MALE";
+        String rawPhoneNumber = "010-1234-5678";
+        Password rawPassword = Password.from("password");
+        Member member = memberRepository.save(
+                new Member(
+                        "menteeId",
+                        rawGender,
+                        rawName,
+                        new Phone(rawPhoneNumber),
+                        rawPassword
+                )
+        );
+
+        String accessToken = jwtProvider.createAccessToken(member.getId());
+
+        String newName = "newName";
+        String newGender = "newGender";
+        String newPassword = "newPassword";
+        String newPhoneNumber = "010-5678-9123";
+
+        MemberInfoUpdateRequest request = new MemberInfoUpdateRequest(
+                newName,
+                newGender,
+                newPassword,
+                newPhoneNumber
+        );
+
+        // when
+        // then
+        RestAssured
+                .given(spec)
+                .accept("application/json")
+                .contentType(ContentType.JSON)
+                .cookie("accessToken", accessToken)
+                .filter(documentWithTag("member/patch-memberInfo-success-partial"))
+                .log().all()
+                .body(request)
+                .when()
+                .patch("/members/me")
+                .then()
+                .log().all()
+                .statusCode(204);
+    }
+
+    @DisplayName("회원(멘토, 멘티)은 자신의 이름, 성별, 비밀번호, 전화번호 중 일부를 선택적으로 수정할 수 있다.")
+    @Test
+    void updateInfo2() {
+        // given
+        String rawName = "이름";
+        String rawGender = "MALE";
+        String rawPhoneNumber = "010-1234-5678";
+        Password rawPassword = Password.from("password");
+        Member member = memberRepository.save(
+                new Member(
+                        "menteeId",
+                        rawGender,
+                        rawName,
+                        new Phone(rawPhoneNumber),
+                        rawPassword
+                )
+        );
+
+        String accessToken = jwtProvider.createAccessToken(member.getId());
+
+        String newName = "newName";
+        String newPhoneNumber = "010-5678-9123";
+
+        MemberInfoUpdateRequest request = new MemberInfoUpdateRequest(
+                newName,
+                null,
+                null,
+                newPhoneNumber
+        );
+
+        // when
+        // then
+        RestAssured
+                .given(spec)
+                .accept("application/json")
+                .contentType(ContentType.JSON)
+                .cookie("accessToken", accessToken)
+                .filter(documentWithTag("member/patch-memberInfo-success-optional"))
+                .log().all()
+                .body(request)
+                .when()
+                .patch("/members/me")
+                .then()
+                .log().all()
+                .statusCode(204);
     }
 }
