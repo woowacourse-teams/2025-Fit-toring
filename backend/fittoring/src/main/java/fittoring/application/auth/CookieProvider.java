83ff6797a6ff001b13d73506ef3a1bec9a665bd6
Lee Dong Yeop <s01054830455@gmail.com>
2025-12-12 20:14:47 +0900

    refactor: CookieProvider가 sameSite 설정정보를 사용하도록 변경
    - 스프링 설정 정보를 사용해야 하기 때문에 빈으로 등록하게 되었음

--- backend/fittoring/src/main/java/fittoring/application/auth/CookieProvider.java
+++ backend/fittoring/src/main/java/fittoring/application/auth/CookieProvider.java
@@ -1,23 +1,32 @@
 package fittoring.application.auth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CookieProvider {
 
-    private static ResponseCookie.ResponseCookieBuilder baseBuilder(String name, String value) {
+    private final String sameSite;
+
+    public CookieProvider(@Value("${cookie.same-site}") String sameSite) {
+        this.sameSite = sameSite;
+    }
+
+    private ResponseCookie.ResponseCookieBuilder baseBuilder(final String name, final String value) {
         return ResponseCookie.from(name, value)
                 .httpOnly(true)
                 .secure(true)
                 .path("/")
-                .sameSite("Strict");
+                .sameSite(sameSite);
     }
 
-    public static ResponseCookie createCookie(final String name, final String value) {
+    public ResponseCookie createCookie(final String name, final String value) {
         return baseBuilder(name, value)
                 .build();
     }
 
-    public static ResponseCookie clearCookie(final String name) {
+    public ResponseCookie clearCookie(final String name) {
         final int maxAgeSeconds = 0;
         return baseBuilder(name, "")
                 .maxAge(maxAgeSeconds)
