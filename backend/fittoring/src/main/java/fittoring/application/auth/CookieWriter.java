aa8d6c1863efdc5d11916a6b1dd853886e6d4383
Lee Dong Yeop <s01054830455@gmail.com>
2025-12-15 18:18:26 +0900

    [REFACTOR] Cookie same-site 프로필별 설정 분리 (#1143)
    * build: 프로필별 same-site 쿠키 설정 추가
    
    - test.yml 경우 테스트를 위해서 None으로 고정
    
    * refactor: CookieProvider가 sameSite 설정정보를 사용하도록 변경
    
    - 스프링 설정 정보를 사용해야 하기 때문에 빈으로 등록하게 되었음
    
    * refactor: CookieWriter가 CookieProvider를 의존하도록 변경
    
    - CookieProvider가 빈으로 등록되면서 기존의 정적 호출이 아닌 의존성 주입을 통해서 사용하도록 변경되어야 했음
    
    * refactor: OauthSignUpToken을 쿠키에 쓰는 책임을 CookieWriter로 이동
    
    - cookieWriter 의존성 주입 방식으로 변경
    - CookieWriter.writeOauthSignUpToken으로 OAuth 쿠키 생성 로직 통합하여 컨트롤러에서 직접 쿠키를 쓰는 책임을 외부로 분리
    
    * test: 테스트 검증문 수정

--- backend/fittoring/src/main/java/fittoring/application/auth/CookieWriter.java
+++ backend/fittoring/src/main/java/fittoring/application/auth/CookieWriter.java
@@ -2,30 +2,36 @@ package fittoring.application.auth;
 
 import fittoring.application.auth.service.dto.AuthTokenDto;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+@Component
 public class CookieWriter {
 
-    public static void write(HttpServletResponse response, AuthTokenDto tokens) {
-        ResponseCookie accessToken = CookieProvider.createCookie("accessToken", tokens.accessToken());
-        ResponseCookie refreshToken = CookieProvider.createCookie("refreshToken", tokens.refreshToken());
+    private final CookieProvider cookieProvider;
+
+    public void write(HttpServletResponse response, AuthTokenDto tokens) {
+        ResponseCookie accessToken = cookieProvider.createCookie("accessToken", tokens.accessToken());
+        ResponseCookie refreshToken = cookieProvider.createCookie("refreshToken", tokens.refreshToken());
         response.addHeader(HttpHeaders.SET_COOKIE, accessToken.toString());
         response.addHeader(HttpHeaders.SET_COOKIE, refreshToken.toString());
     }
 
-    public static void clearCookies(HttpServletResponse response) {
-        ResponseCookie accessToken = CookieProvider.clearCookie("accessToken");
-        ResponseCookie refreshToken = CookieProvider.clearCookie("refreshToken");
-        ResponseCookie oauthSignUpToken = CookieProvider.clearCookie("oauthSignUpToken");
+    public void writeOauthSignUpToken(HttpServletResponse response, String oauthSignUpToken) {
+        ResponseCookie cookie = cookieProvider.createCookie("oauthSignUpToken", oauthSignUpToken);
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+    }
+
+    public void clearCookies(HttpServletResponse response) {
+        ResponseCookie accessToken = cookieProvider.clearCookie("accessToken");
+        ResponseCookie refreshToken = cookieProvider.clearCookie("refreshToken");
+        ResponseCookie oauthSignUpToken = cookieProvider.clearCookie("oauthSignUpToken");
 
         response.addHeader(HttpHeaders.SET_COOKIE, accessToken.toString());
         response.addHeader(HttpHeaders.SET_COOKIE, refreshToken.toString());
         response.addHeader(HttpHeaders.SET_COOKIE, oauthSignUpToken.toString());
     }
 }
-
-
