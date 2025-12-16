4be1f7bde3e91c56cf8360233ce89403587ac870
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-12-16 20:24:33 +0900

    [REFACTOR] API 응답, 요청의 Gender 타입 변경 (#1148)
    * refactor: common/gender.ts 파일 추가 및 Gender 필드 타입을 GenderClient로 변경 #1147
    
    - UserInfo 타입
    - SignupInfo 타입
    - IdentityVerificationInfo 타입
    - useGender 커스텀 훅
    - SignupForm 컴포넌트
    - IdentityVerificationForm 컴포넌트
    
    * feat: GenderServer 타입 추가 #1147
    
    * feat: genderConverter 유틸리티 함수 추가 #1147
    
    - convertGenderClientToServer: 클라이언트 -> 서버 변환
    - convertGenderServerToClient: 서버 -> 클라이언트 변환
    
    * refactor: UserInfo 인터페이스의 gender 필드를 제네릭으로 변경 및 GenderServer 타입 추가 #1147
    
    * refactor: UserInfo 관련 타입을 UserInfoClient로 변경 및 getUserInfo 함수 수정 #1147
    
    * refactor: postIdentityVerification에서 gender 변환 로직 추가 #1147
    
    * refactor: postSignup에서 gender 변환 로직 추가 #1147
    
    * refactor: patchMyProfile에서 gender 변환 로직 추가 #1147

--- frontend/src/pages/editProfile/apis/patchMyProfile.ts
+++ frontend/src/pages/editProfile/apis/patchMyProfile.ts
@@ -1,12 +1,16 @@
 import { apiClient } from '../../../common/apis/apiClient';
 import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
+import { convertGenderClientToServer } from '../../../common/utils/genderConverter';
 
 import type { PartialUserProfileRequest } from '../types/userProfile';
 
 export const patchMyProfile = async (body: PartialUserProfileRequest) => {
   return await apiClient.patch({
     endpoint: API_ENDPOINTS.MEMBERS_ME,
-    body,
+    body: {
+      ...body,
+      gender: body.gender && convertGenderClientToServer(body.gender),
+    },
     withCredentials: true,
   });
 };
