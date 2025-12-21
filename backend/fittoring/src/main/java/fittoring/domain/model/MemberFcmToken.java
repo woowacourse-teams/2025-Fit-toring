f5e9e55adfe5953dd0d0ada084435079333dd357
kimdev <kim__dev@hufs.ac.kr>
2025-12-21 15:27:07 +0900

    feat: 회원 FCM 토큰 테이블 및 엔티티 추가
    - member_fcm_token 테이블 생성(migration 스크립트 추가)
      - id(PK), member_id(FK), token, updated_at 컬럼 구성
      - token에 유니크 제약 조건 추가
      - member_id에 인덱스 추가
    - MemberFcmToken 엔티티 추가
      - Auditing을 통한 updatedAt 자동 업데이트 설정
      - 데이터 엔티티의 일관성과 보안을 위해 Lombok 접근 제어자 활용

---
+++ backend/fittoring/src/main/java/fittoring/domain/model/MemberFcmToken.java
@@ -0,0 +1,37 @@
+package fittoring.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "member_fcm_token")
+@Entity
+public class MemberFcmToken {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(name = "member_id", nullable = false, unique = true)
+    private Long memberId;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
---
+++ backend/fittoring/src/main/resources/db/migration/V202512211520_add_member_fcm_token.sql
@@ -0,0 +1,10 @@
+CREATE TABLE member_fcm_token(
+    id          BIGINT  AUTO_INCREMENT  PRIMARY KEY,
+    member_id   BIGINT  NOT NULL,
+    token       VARCHAR(255) NOT NULL,
+    updated_at  DATETIME NOT NULL,
+
+    UNIQUE KEY uk_member_id(member_id),
+    UNIQUE KEY uk_token(token),
+    INDEX idx_member_id(member_id)
+);
