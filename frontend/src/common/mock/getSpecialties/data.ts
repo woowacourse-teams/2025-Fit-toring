52789e24afc5d7c253c0f0e0bbcc2d794cecd4b8
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-12-22 22:52:44 +0900

    refactor: Specialty 데이터 구조 변경 및 id 타입 수정 #1165

--- frontend/src/common/mock/getSpecialties/data.ts
+++ frontend/src/common/mock/getSpecialties/data.ts
@@ -1,10 +1,44 @@
 import type { Specialty } from '../../types/Specialty';
 
 export const SPECIALTIES: Specialty[] = [
-  { id: '1', title: '근력 운동' },
-  { id: '2', title: '유산소 운동' },
-  { id: '3', title: '다이어트' },
-  { id: '4', title: '체형 교정' },
-  { id: '5', title: '영양 상담' },
-  { id: '6', title: '재활 운동' },
+  {
+    id: 5,
+    title: '근력 강화',
+  },
+  {
+    id: 2,
+    title: '다이어트',
+  },
+  {
+    id: 3,
+    title: '벌크업',
+  },
+  {
+    id: 8,
+    title: '부상 상담',
+  },
+  {
+    id: 4,
+    title: '식단 관리',
+  },
+  {
+    id: 7,
+    title: '유연성·스트레칭',
+  },
+  {
+    id: 10,
+    title: '자세 교정',
+  },
+  {
+    id: 9,
+    title: '재활 운동',
+  },
+  {
+    id: 1,
+    title: '체형 교정',
+  },
+  {
+    id: 6,
+    title: '홈 트레이닝',
+  },
 ] as const;
