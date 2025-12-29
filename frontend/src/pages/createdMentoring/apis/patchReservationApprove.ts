743dfbddca4c05e6287f8fc6addacda213715e9b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-12-29 16:25:50 +0900

    refactor: 예약 상태 관련 API 추가 (승인, 거부, 완료) #1171

---
+++ frontend/src/pages/createdMentoring/apis/patchReservationApprove.ts
@@ -0,0 +1,10 @@
+import { apiClient } from '../../../common/apis/apiClient';
+import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
+
+export const patchReservationApprove = async (reservationId: number) => {
+  return await apiClient.patch({
+    endpoint: `${API_ENDPOINTS.RESERVATIONS}/${reservationId}${API_ENDPOINTS.PATCH_MENTORING_APPROVE}`,
+    body: {},
+    withCredentials: true,
+  });
+};
