61673b5ea2a1d5dd662a964d1d65f109351ee1c6
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2026-01-05 20:08:05 +0900

    refactor: 카카오 로그인 버튼 클릭 핸들러에서 API 엔드포인트 상수화 #1144

--- frontend/src/pages/login/components/LoginForm/LoginForm.tsx
+++ frontend/src/pages/login/components/LoginForm/LoginForm.tsx
@@ -12,6 +12,7 @@ import { useAuth } from '../../../../common/components/AuthProvider/AuthProvider
 import Button from '../../../../common/components/Button/Button';
 import FormField from '../../../../common/components/FormField/FormField';
 import Input from '../../../../common/components/Input/Input';
+import { API_ENDPOINTS } from '../../../../common/constants/apiEndpoints';
 import { PAGE_URL } from '../../../../common/constants/url';
 import usePasswordInput from '../../../../common/hooks/usePasswordInput';
 import useUserIdInput from '../../../../common/hooks/useUserIdInput';
@@ -66,8 +67,8 @@ function LoginForm() {
     loginMutate({ loginId: userId, password });
   };
 
-  const handleSocialLoginButtonClick = () => {
-    window.location.href = `${process.env.API_BASE_URL}/kakao/login`;
+  const handleSocialLoginButtonClick = async () => {
+    window.location.href = `${process.env.API_BASE_URL}${API_ENDPOINTS.KAKAO_LOGIN}`;
   };
 
   const loginFormValidated = userId !== '' && password !== '';
