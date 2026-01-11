d34daa4197c448c3599a645698327ec04232e6d6
Lee Dong Yeop <s01054830455@gmail.com>
2026-01-12 00:57:48 +0900

    test: 관리자 멘토링 e2e 테스트 구조 변경

--- backend/fittoring/src/test/java/fittoring/integration/admin/AdminMemberIntegrationTest.java
+++ backend/fittoring/src/test/java/fittoring/integration/admin/AdminMemberIntegrationTest.java
@@ -3,19 +3,14 @@ package fittoring.integration.admin;
 import fittoring.AbstractApiDocumentationTest;
 import fittoring.admin.presentation.dto.AdminMemberResponse;
 import fittoring.admin.presentation.dto.PageResult;
+import fittoring.application.FixtureUtil;
 import fittoring.application.auth.service.JwtProvider;
 import fittoring.application.member.repository.MemberRepository;
-import fittoring.domain.model.Gender;
-import fittoring.domain.model.Member;
 import fittoring.domain.model.MemberRole;
-import fittoring.domain.model.Phone;
-import fittoring.domain.model.password.Password;
 import io.restassured.RestAssured;
 import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
-import java.util.List;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -23,94 +18,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class AdminMemberIntegrationTest extends AbstractApiDocumentationTest {
 
-    private Member admin;
-    private Member user;
-    private String adminAccessToken;
-    private String userAccessToken;
-
     @Autowired
     private MemberRepository memberRepository;
 
     @Autowired
     private JwtProvider jwtProvider;
 
-    @BeforeEach
-    void setUp() {
-        admin = memberRepository.save(new Member(
-                "adminId",
-                Gender.FEMALE,
-                "관리자",
-                new Phone("010-0000-0000"),
-                Password.from("pw"),
-                MemberRole.ADMIN
-        ));
-        adminAccessToken = jwtProvider.createAccessToken(admin.getId(), admin.getRole());
-        user = memberRepository.save(new Member(
-                "userId",
-                Gender.MALE,
-                "멘티",
-                new Phone("010-1111-1111"),
-                Password.from("pw")
-        ));
-        userAccessToken = jwtProvider.createAccessToken(user.getId(), user.getRole());
-    }
-
-    @DisplayName("관리자 사용자 목록 조회")
+    @DisplayName("관리자 사용자 목록 페이징 조회")
     @Nested
     class MembersForAdmin {
-
-        @DisplayName("관리자 권한이 없다면 멤버 목록 조회에 실패한다")
-        @Test
-        void failFindMembersForAdminWithoutAdmin() {
-            // given
-            // when
-            // then
-            RestAssured
-                    .given()
-                    .log().all().contentType(ContentType.JSON)
-                    .cookie("accessToken", userAccessToken)
-                    .when()
-                    .get("/admin/members")
-                    .then().log().all()
-                    .statusCode(403);
-        }
-
         @DisplayName("관리자 권한이 있다면 멤버 목록 조회에 성공한다")
         @Test
         void successFindMembersForAdminWithAdmin() {
             // given
+            for (int i = 1; i <= 21; i++) {
+                memberRepository.save(FixtureUtil.getTestMentee(i));
+            }
+
+            String adminAccessToken = jwtProvider.createAccessToken(1L, MemberRole.ADMIN);
+
             // when
-            var actual = RestAssured
-                    .given()
+            PageResult<AdminMemberResponse> actual = RestAssured
+                    .given(spec)
                     .log().all().contentType(ContentType.JSON)
                     .cookie("accessToken", adminAccessToken)
+                    .filter(documentWithTag("admin/members/get-members-success"))
                     .when()
                     .get("/admin/members")
                     .then().log().all()
                     .statusCode(200)
                     .extract()
-                    .as(new TypeRef<PageResult<AdminMemberResponse>>() {
+                    .as(new TypeRef<>() {
                     });
-            List<AdminMemberResponse> content = actual.content();
+
             // then
-            var expected = List.of(
-                    new AdminMemberResponse(
-                            admin.getName(),
-                            admin.getLoginId(),
-                            admin.getGender(),
-                            admin.getPhoneNumber(),
-                            admin.getRole()
-                    ),
-                    new AdminMemberResponse(
-                            user.getName(),
-                            user.getLoginId(),
-                            user.getGender(),
-                            user.getPhoneNumber(),
-                            user.getRole()
-                    )
-            );
-            Assertions.assertThat(content)
-                    .containsExactlyInAnyOrderElementsOf(expected);
+            SoftAssertions.assertSoftly(softAssertions -> {
+                softAssertions.assertThat(actual.total()).isEqualTo(21);
+                softAssertions.assertThat(actual.content()).hasSize(20);
+                softAssertions.assertThat(actual.totalPages()).isEqualTo(2);
+            });
+        }
+
+        @DisplayName("관리자 권한이 없다면 멤버 목록 조회에 실패한다")
+        @Test
+        void failFindMembersForAdminWithoutAdmin() {
+            // given
+            String userAccessToken = jwtProvider.createAccessToken(1L, MemberRole.MENTEE);
+
+            // when // then
+            RestAssured
+                    .given(spec)
+                    .log().all().contentType(ContentType.JSON)
+                    .cookie("accessToken", userAccessToken)
+                    .filter(documentWithTag("admin/members/get-members-forbidden"))
+                    .when()
+                    .get("/admin/members")
+                    .then().log().all()
+                    .statusCode(403);
         }
     }
 }
