75eb4e31f425330f25496b8de89c8f64db7e07ee
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2026-01-11 19:44:21 +0900

    feat: Firebase 메시징 기능을 위한 초기화 #1185

---
+++ frontend/src/pwa/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+const firebaseConfig = {
+  apiKey: 'AIzaSyCrUhONRlAeig6WSRTrvwZBPYjIeTiC5sA',
+  authDomain: 'fittoring-dev.firebaseapp.com',
+  projectId: 'fittoring-dev',
+  storageBucket: 'fittoring-dev.firebasestorage.app',
+  messagingSenderId: '263030224988',
+  appId: '1:263030224988:web:0d8da83df7e01155b0ce99',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const messaging = getMessaging(app);
