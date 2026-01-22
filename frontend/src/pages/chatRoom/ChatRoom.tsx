784fc37fd7bed79a562e03952d6356de892d7e06
kimyou1102 <yoosk5485@naver.com>
2026-01-23 03:51:38 +0900

    feat: 채팅방 접근 권한 없음 상태 UI 컴포넌트 추가 #1238

--- frontend/src/pages/chatRoom/ChatRoom.tsx
+++ frontend/src/pages/chatRoom/ChatRoom.tsx
@@ -12,8 +12,11 @@ import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import SockJS from 'sockjs-client';
 
+import ApiError from '../../common/apis/ApiError';
+
 import { getChatRoomInfo } from './apis/getChatRoomInfo';
 import ChatContent from './components/ChatContent/ChatContent';
+import ChatRoomForbidden from './components/ChatRoomForbidden/ChatRoomForbidden';
 import ChatRoomHeader from './components/ChatRoomHeader/ChatRoomHeader';
 import InputSection from './components/InputSection/InputSection';
 import MentoringActionPanel from './components/MentoringActionPanel/MentoringActionPanel';
@@ -167,11 +170,21 @@ function ChatRoom() {
     }
   }, [chatRoomMessage]);
 
-  const { data: chatRoomInfoData, isPending: chatRoomInfoIsPending } =
-    useQuery<ChatRoomInfo>({
-      queryKey: ['chatRoomInfo', chatRoomId],
-      queryFn: () => getChatRoomInfo(Number(chatRoomId!)),
-    });
+  const {
+    data: chatRoomInfoData,
+    isPending: chatRoomInfoIsPending,
+    error,
+  } = useQuery<ChatRoomInfo, ApiError>({
+    queryKey: ['chatRoomInfo', chatRoomId],
+    queryFn: () => getChatRoomInfo(Number(chatRoomId!)),
+    retry: (failureCount, error) => {
+      if (error instanceof ApiError && error.status === 403) {
+        return false;
+      }
+
+      return failureCount < 1;
+    },
+  });
 
   const stompClientRef = useRef<Client | null>(null);
 
@@ -231,7 +244,11 @@ function ChatRoom() {
     };
   }, [capturePrevScroll, chatRoomId]);
 
-  if (!memberId) {
+  if (error?.status === 403) {
+    return <ChatRoomForbidden />;
+  }
+
+  if (error?.status === 401) {
     return <div>로그인 후 이용 가능합니다.</div>;
   }
 
