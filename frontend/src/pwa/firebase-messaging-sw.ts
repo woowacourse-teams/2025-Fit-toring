610646bf52463f16a203e30b9ec598d2da581375
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2026-01-28 13:43:23 +0900

    fix: 백그라운드 및 포그라운드 알림 메시지 수정 #1273

--- frontend/src/pwa/firebase-messaging-sw.ts
+++ frontend/src/pwa/firebase-messaging-sw.ts
@@ -33,7 +33,7 @@ onBackgroundMessage(messaging, (payload) => {
 
   const notificationTitle = data.title || '제목 없음';
   const notificationOptions = {
-    body: data.body || '내용 없음',
+    body: '백그라운드 알림: ' + data.body || '내용 없음',
     icon: iconPath,
     badge: iconPath,
     data: {
