e2a0800866556f4f335ba488d572ad3c7075493e
kimyou1102 <yoosk5485@naver.com>
2026-02-10 19:36:39 +0900

    feat: getChatRooms API 함수 생성 #1304

---
+++ frontend/src/pages/chatRooms/apis/getChatRooms.ts
@@ -0,0 +1,10 @@
+import { apiClient } from '../../../common/apis/apiClient';
+import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
+
+import type { ChatRooms } from '../types/chatRooms';
+
+export const getChatRooms = async () => {
+  return await apiClient.get<ChatRooms[]>({
+    endpoint: `${API_ENDPOINTS.CHATROOMS}`,
+  });
+};
