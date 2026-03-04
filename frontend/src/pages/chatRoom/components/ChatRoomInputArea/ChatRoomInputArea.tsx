3e8853c2e87d69b2c3488dad1fbce6a2680aa498
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2026-03-04 12:54:49 +0900

    feat: 채팅 입력 영역에 이미지 업로드 UI 추가 #1364

--- frontend/src/pages/chatRoom/components/ChatRoomInputArea/ChatRoomInputArea.tsx
+++ frontend/src/pages/chatRoom/components/ChatRoomInputArea/ChatRoomInputArea.tsx
@@ -29,11 +29,74 @@ function ChatRoomInputArea({
         <MenuToggleButton opened={menuOpened} onClick={toggleMenu} />
         <InputSection value={value} onChange={onChange} onSubmit={onSubmit} />
       </S_InputWrapper>
+      {menuOpened && (
+        <S_MenuPanel>
+          <S_MenuItemWrapper>
+            <S_MenuItemLabel>
+              <S_HiddenInput type="file" accept="image/*" />
+              <S_AlbumIcon src={albumIcon} alt="앨범" />
+            </S_MenuItemLabel>
+            <S_MenuItemDescription>앨범</S_MenuItemDescription>
+          </S_MenuItemWrapper>
+        </S_MenuPanel>
+      )}
     </>
   );
 }
+
 export default ChatRoomInputArea;
 
 const S_InputWrapper = styled.div`
   display: flex;
+`;
+
+const S_MenuPanel = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 100%;
+  height: auto;
+  padding: 1.6rem;
+  border-top: 1px solid ${({ theme }) => theme.OUTLINE.REGULAR};
+
+  background-color: ${({ theme }) => theme.BG.WHITE};
+`;
+
+const S_MenuItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.4rem;
+`;
+
+const S_MenuItemLabel = styled.label`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 3.6rem;
+  height: 3.6rem;
+  padding: 0;
+  border: none;
+  border-radius: 50%;
+
+  cursor: pointer;
+
+  background-color: ${({ theme }) => theme.SYSTEM.GRAY400};
+  aspect-ratio: 1 / 1;
+`;
+
+const S_HiddenInput = styled.input`
+  display: none;
+`;
+
+const S_MenuItemDescription = styled.span`
+  ${({ theme }) => theme.TYPOGRAPHY.B3_R};
+  color: ${({ theme }) => theme.FONT.B01};
+`;
+
+const S_AlbumIcon = styled.img`
+  width: 1.6rem;
+  height: 1.6rem;
 `;
