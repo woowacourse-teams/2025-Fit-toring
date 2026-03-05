46a884feba3aae0374a0f19a1a96a3cf0a3bfdb9
kimyou1102 <yoosk5485@naver.com>
2026-03-05 23:25:38 +0900

    refactor: SPA 라우트 변경 감지를 위해 ChannelTalkProvider를 router 루트에 배치 #1367

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 
 import BottomTabLayout from './common/components/BottomTabLayout/BottomTabLayout';
+import ChannelTalkProvider from './common/components/ChannelTalkProvider/ChannelTalkProvider';
 import MobileLayout from './common/components/MobileLayout/MobileLayout';
 import { PAGE_URL } from './common/constants/url';
 import ChatRooms from './pages/chatRooms/ChatRooms';
@@ -38,65 +39,74 @@ const IdentityVerification = lazy(
 
 const router = createBrowserRouter([
   {
-    element: <MobileLayout />,
+    element: <ChannelTalkProvider />,
     children: [
       {
-        element: <BottomTabLayout />,
+        element: (
+          <>
+            <MobileLayout />
+          </>
+        ),
         children: [
           {
-            path: PAGE_URL.HOME,
-            element: <Home />,
-            loader: () => {
-              const firstVisited = !sessionStorage.getItem('hasVisited');
-
-              if (firstVisited) {
-                return redirect(PAGE_URL.LANDING);
-              }
-              return null;
-            },
-          },
-          { path: `${PAGE_URL.CHAT_ROOMS}`, element: <ChatRooms /> },
-          {
-            path: `${PAGE_URL.MY_PAGE}`,
-            element: <MyPage />,
+            element: <BottomTabLayout />,
             children: [
               {
-                index: true,
-                element: <CreatedMentoring />,
+                path: PAGE_URL.HOME,
+                element: <Home />,
+                loader: () => {
+                  const firstVisited = !sessionStorage.getItem('hasVisited');
+
+                  if (firstVisited) {
+                    return redirect(PAGE_URL.LANDING);
+                  }
+                  return null;
+                },
               },
+              { path: `${PAGE_URL.CHAT_ROOMS}`, element: <ChatRooms /> },
               {
-                path: PAGE_URL.CREATED_MENTORING,
-                element: <CreatedMentoring />,
-              },
-              {
-                path: PAGE_URL.PARTICIPATED_MENTORING,
-                element: <ParticipatedMentoring />,
-              },
-              {
-                path: PAGE_URL.EDIT_PROFILE,
-                element: <EditProfile />,
+                path: `${PAGE_URL.MY_PAGE}`,
+                element: <MyPage />,
+                children: [
+                  {
+                    index: true,
+                    element: <CreatedMentoring />,
+                  },
+                  {
+                    path: PAGE_URL.CREATED_MENTORING,
+                    element: <CreatedMentoring />,
+                  },
+                  {
+                    path: PAGE_URL.PARTICIPATED_MENTORING,
+                    element: <ParticipatedMentoring />,
+                  },
+                  {
+                    path: PAGE_URL.EDIT_PROFILE,
+                    element: <EditProfile />,
+                  },
+                ],
               },
             ],
           },
+          { path: PAGE_URL.LANDING, element: <Landing /> },
+          { path: `${PAGE_URL.DETAIL}/:mentoringId`, element: <Detail /> },
+          { path: `${PAGE_URL.BOOKING}/:mentoringId`, element: <Booking /> },
+          { path: PAGE_URL.SIGNUP, element: <Signup /> },
+          { path: PAGE_URL.MENTORING_CREATE, element: <MentoringCreate /> },
+          {
+            path: `${PAGE_URL.MENTORING_UPDATE}/:mentoringId`,
+            element: <MentoringUpdate />,
+          },
+          { path: PAGE_URL.LOGIN, element: <Login /> },
+          {
+            path: `${PAGE_URL.CHAT_ROOM}/:chatRoomId`,
+            element: <ChatRoom />,
+          },
+          {
+            path: PAGE_URL.IDENTITY_VERIFICATION,
+            element: <IdentityVerification />,
+          },
         ],
-      },
-      { path: PAGE_URL.LANDING, element: <Landing /> },
-      { path: `${PAGE_URL.DETAIL}/:mentoringId`, element: <Detail /> },
-      { path: `${PAGE_URL.BOOKING}/:mentoringId`, element: <Booking /> },
-      { path: PAGE_URL.SIGNUP, element: <Signup /> },
-      { path: PAGE_URL.MENTORING_CREATE, element: <MentoringCreate /> },
-      {
-        path: `${PAGE_URL.MENTORING_UPDATE}/:mentoringId`,
-        element: <MentoringUpdate />,
-      },
-      { path: PAGE_URL.LOGIN, element: <Login /> },
-      {
-        path: `${PAGE_URL.CHAT_ROOM}/:chatRoomId`,
-        element: <ChatRoom />,
-      },
-      {
-        path: PAGE_URL.IDENTITY_VERIFICATION,
-        element: <IdentityVerification />,
       },
     ],
   },
