669cb7630cfc06db8866a9414aba0d34ec6906ff
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2026-03-05 15:41:06 +0900

    refactor: Message 인터페이스를 MessageDTO로 변경하고, 이미지 및 텍스트 메시지 타입 정의 추가 #1364

--- frontend/src/pages/chatRoom/types/message.ts
+++ frontend/src/pages/chatRoom/types/message.ts
@@ -1,18 +1,36 @@
 import type { MESSAGE_TYPE } from '../constants/message';
 
-export interface Message {
+interface MessageDTO {
   // 서버와 클라이언트 공통
-  content: string;
+  content: string | null;
   createdAt: string;
   senderId: number;
   chatRoomId: number;
   tempId: number;
   messageType: MessageType;
+  thumbnailUrl: string | null;
+  originalImageUrl: string | null;
   // 서버에서 받는 id
   chatMessageId?: number;
 
   // 클라이언트의 상태 관리를 위한 속성
   status?: 'success' | 'fail' | 'pending';
+}
+
+export type Message = ImageMessage | TextMessage;
+
+export interface ImageMessage extends MessageDTO {
+  content: null;
+  messageType: 'IMAGE';
+  thumbnailUrl: string;
+  originalImageUrl: string;
+}
+
+export interface TextMessage extends MessageDTO {
+  content: string;
+  messageType: 'TEXT';
+  thumbnailUrl: null;
+  originalImageUrl: null;
 }
 
 export interface MessageResponse {
