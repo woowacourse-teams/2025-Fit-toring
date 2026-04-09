1e130637aa212f282df48a74ee2f9b97c8aa51f6
kimyou1102 <yoosk5485@naver.com>
2026-04-10 01:34:30 +0900

    feat: getCommunityPosts API 함수 생성 #1478

--- frontend/src/pages/community/apis/getCommunityPosts.ts
+++ frontend/src/pages/community/apis/getCommunityPosts.ts
@@ -3,9 +3,16 @@ import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
 
 import type { CommunityPostResponse } from '../types/posts';
 
-export const getCommunityPosts = async () => {
+interface GetCommunityPostsParams {
+  cursorCode?: string | null;
+}
+
+export const getCommunityPosts = async ({
+  cursorCode,
+}: GetCommunityPostsParams = {}) => {
   return await apiClient.get<CommunityPostResponse>({
     endpoint: `${API_ENDPOINTS.POSTS}`,
     withCredentials: true,
+    ...(cursorCode ? { searchParams: { cursorCode } } : {}),
   });
 };
