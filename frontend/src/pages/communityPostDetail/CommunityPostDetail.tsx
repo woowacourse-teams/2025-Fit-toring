11391c666d27b16ab980ac344e81dd4179c2cce3
kimyou1102 <yoosk5485@naver.com>
2026-04-10 06:37:23 +0900

    feat: 커뮤니티 게시글 상세 API 연동 #1480

--- frontend/src/pages/communityPostDetail/CommunityPostDetail.tsx
+++ frontend/src/pages/communityPostDetail/CommunityPostDetail.tsx
@@ -1,6 +1,10 @@
 import styled from '@emotion/styled';
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 
+import LoadingSpinner from '../../common/components/LoadingSpinner/LoadingSpinner';
+
+import { getCommunityPostDetail } from './apis/getCommunityPostDetail';
 import CommunityPostDetailHeader from './components/CommunityPostDetailHeader/CommunityPostDetailHeader';
 import InputSection from './components/InputSection/InputSection';
 import PostCommentSection from './components/PostCommentSection/PostCommentSection';
@@ -9,32 +13,26 @@ import PostHeader from './components/PostHeader/PostHeader';
 
 function CommunityPostDetail() {
   const { postId } = useParams();
+  const { data: postData, isPending } = useQuery({
+    queryKey: ['communityPostDetail', postId],
+    queryFn: () => getCommunityPostDetail(postId!),
+    enabled: Boolean(postId),
+  });
 
-  const post = {
-    id: 13,
-    title: '주말 오전 같이 러닝하실 분 있나요?',
-    nickname: '작성자E',
-    profileImageUrl: null,
-    isAnonymous: false,
-    createdAt: '2026-04-02T09:10:00',
-    commentCount: 2,
-    viewCount: 87,
-    likeCount: 5,
-    content:
-      '토요일 아침에 가볍게 5km 정도 뛰고 브런치 먹을 분 구합니다. 초보도 괜찮아요.',
-  };
+  if (isPending || !postData) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <S_Container>
       <CommunityPostDetailHeader />
       <S_Content>
         <PostHeader
-          viewCount={post.viewCount}
-          createdAt={post.createdAt}
-          nickname={post.nickname}
-          profileImageUrl={post.profileImageUrl}
+          createdAt={postData.createdAt}
+          nickname={postData.isAnonymous ? '익명' : postData.nickname}
+          viewCount={postData.viewCount}
         />
-        <PostContent title={post.title} content={post.content} />
+        <PostContent title={postData.title} content={postData.content} />
         <PostCommentSection postId={postId ?? ''} />
       </S_Content>
       <InputSection />
