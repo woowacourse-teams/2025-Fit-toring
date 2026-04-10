1daf82b752de02a179506823be2cd28d49dac88b
kimyou1102 <yoosk5485@naver.com>
2026-04-10 13:38:10 +0900

    feat: getPostComments API 함수 생성 #1480

--- frontend/src/pages/communityPostDetail/msw/handlers.ts
+++ frontend/src/pages/communityPostDetail/msw/handlers.ts
@@ -1,9 +1,10 @@
 import { http, HttpResponse } from 'msw';
 
-import { COMMUNITY_POST_DETAIL } from './data';
+import { COMMUNITY_POST_DETAIL, POST_COMMENTS } from './data';
 
 const BASE_URL = process.env.API_BASE_URL;
 const COMMUNITY_POST_DETAIL_URL = `${BASE_URL}/posts/:postId`;
+const POST_COMMENTS_URL = `${BASE_URL}/posts/:postId/comments`;
 
 const getCommunityPostDetail = http.get(
   COMMUNITY_POST_DETAIL_URL,
@@ -17,4 +18,11 @@ const getCommunityPostDetail = http.get(
   },
 );
 
-export const communityPostDetailHandler = [getCommunityPostDetail];
+const getPostComments = http.get(POST_COMMENTS_URL, async () => {
+  return HttpResponse.json(POST_COMMENTS);
+});
+
+export const communityPostDetailHandler = [
+  getCommunityPostDetail,
+  getPostComments,
+];
--- frontend/src/pages/communityPostDetail/types/postComment.ts
+++ frontend/src/pages/communityPostDetail/types/postComment.ts
@@ -2,5 +2,10 @@ export interface PostComment {
   id: number;
   nickname: string;
   content: string;
+  isAnonymous: boolean;
+  isGuestComment: boolean;
+  rootId: number | null;
+  parentId: number | null;
+  isDeleted: boolean;
   createdAt: string;
 }
