240b3b82b7c524141e35dc296a744464a124e5ff
kimyou1102 <yoosk5485@naver.com>
2026-05-20 04:35:55 +0900

    feat: 마이페이지 구현 #1572

--- frontend/src/pages/myPage/MyPage.tsx
+++ frontend/src/pages/myPage/MyPage.tsx
@@ -1,20 +1,73 @@
 import styled from '@emotion/styled';
-import { Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import MyPageHeader from './components/MyPageHeader/MyPageHeader';
+import { PAGE_URL } from '../../common/constants/url';
+import useMyProfile from '../editProfile/hooks/useMyProfile';
+
+import MyPageProfileSummary from './components/MyPageProfileSummary/MyPageProfileSummary';
+import MyPageSection from './components/MyPageSection/MyPageSection';
+
+import type { MyPageSectionItem } from './components/MyPageSection/MyPageSection';
 
 function MyPage() {
+  const navigate = useNavigate();
+  const { myProfile } = useMyProfile();
+
+  const displayName = myProfile?.name?.trim() || '회원';
+
+  const handleEditProfileClick = () => {
+    window.alert('준비중입니다.');
+  };
+
+  const handlePreparingClick = () => {
+    window.alert('준비중입니다.');
+  };
+
+  const mentoringItems: MyPageSectionItem[] = [
+    {
+      label: '내가 개설한 멘토링',
+      onClick: () => navigate(PAGE_URL.CREATED_MENTORING),
+    },
+    {
+      label: '내가 참여한 멘토링',
+      onClick: () => navigate(PAGE_URL.PARTICIPATED_MENTORING),
+    },
+  ];
+
+  const activityItems: MyPageSectionItem[] = [
+    {
+      label: '내가 작성한 글',
+      onClick: handlePreparingClick,
+    },
+  ];
+
+  const settingItems: MyPageSectionItem[] = [
+    {
+      label: '설정',
+      onClick: handleEditProfileClick,
+    },
+  ];
+
   return (
     <S_Container>
-      <MyPageHeader />
-      <Outlet />
+      <MyPageProfileSummary
+        profileImg={myProfile?.image}
+        name={displayName}
+        onClick={handleEditProfileClick}
+      />
+      <MyPageSection items={mentoringItems} title="멘토링" />
+      <MyPageSection items={activityItems} title="커뮤니티" />
+      <MyPageSection items={settingItems} title="설정" />
     </S_Container>
   );
 }
 
 export default MyPage;
 
-const S_Container = styled.div`
+const S_Container = styled.main`
   display: flex;
   flex-direction: column;
+
+  width: 100%;
+  padding: 2rem 1.8rem 2.4rem;
 `;
