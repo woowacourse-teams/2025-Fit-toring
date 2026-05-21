e12d7f0c34ca072daf7e59c213a495d6363f2da5
kimyou1102 <yoosk5485@naver.com>
2026-05-22 07:01:52 +0900

    feat: 보호 라우트 로딩 스피너 지연 표시 #1579
    - 짧은 로딩시 깜빡이는 불편함을 방지하기 위해 지연 로딩 적용

--- frontend/src/common/components/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/common/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,18 +1,22 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
+import useDelayedVisibility from '../../../pages/chatRoom/hooks/useDelayedVisibility';
 import { PAGE_URL } from '../../constants/url';
 import useAuthCheck from '../../hooks/useAuthCheck';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
+const LOADING_SPINNER_DELAY_MS = 1000;
+
 function ProtectedRoute() {
   const location = useLocation();
+  const showLoadingSpinner = useDelayedVisibility(LOADING_SPINNER_DELAY_MS);
   const { data, isError, isFetchedAfterMount, isFetching, isPending } =
     useAuthCheck();
 
   const checkingAuth = isPending || (isFetching && !isFetchedAfterMount);
 
   if (checkingAuth) {
-    return <LoadingSpinner />;
+    return showLoadingSpinner ? <LoadingSpinner /> : null;
   }
 
   if (isError || !data?.memberId) {
