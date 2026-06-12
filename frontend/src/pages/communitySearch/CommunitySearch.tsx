6fcf1e883d686cab995ef34402d209586c797ae1
Lee Dong Yeop <s01054830455@gmail.com>
2026-06-12 15:30:36 +0900

    feat: 최근 검색어 섹션 추가
    - 검색 화면에 최근 검색어 칩 목록 추가
    
    - 최근 검색어 클릭 시 검색 결과 화면으로 이동
    
    - 전체삭제 확인창과 삭제 테스트 추가

--- frontend/src/pages/communitySearch/CommunitySearch.tsx
+++ frontend/src/pages/communitySearch/CommunitySearch.tsx
@@ -1,11 +1,42 @@
 import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
+
+import { PAGE_URL } from '../../common/constants/url';
 
 import CommunitySearchHeader from './components/CommunitySearchHeader/CommunitySearchHeader';
+import RecentSearchSection from './components/RecentSearchSection/RecentSearchSection';
+import useRecentSearchKeywords from './hooks/useRecentSearchKeywords';
 
 function CommunitySearch() {
+  const navigate = useNavigate();
+  const {
+    recentSearchKeywords,
+    addRecentSearchKeyword,
+    deleteRecentSearchKeyword,
+    deleteAllRecentSearchKeywords,
+  } = useRecentSearchKeywords();
+
+  const handleRecentSearchKeywordClick = (keyword: string) => {
+    addRecentSearchKeyword(keyword);
+    navigate(
+      `${PAGE_URL.COMMUNITY_SEARCH_RESULT}?keyword=${encodeURIComponent(
+        keyword,
+      )}`,
+    );
+  };
+
   return (
     <S_Container>
-      <CommunitySearchHeader autoFocus />
+      <CommunitySearchHeader
+        autoFocus
+        onSearch={addRecentSearchKeyword}
+      />
+      <RecentSearchSection
+        keywords={recentSearchKeywords}
+        onKeywordClick={handleRecentSearchKeywordClick}
+        onKeywordRemove={deleteRecentSearchKeyword}
+        onClear={deleteAllRecentSearchKeywords}
+      />
     </S_Container>
   );
 }
